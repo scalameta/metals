dca5a4a8638d79486dd3b0b8b784dc535d0539b9
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-11-12 01:29:31 +0100

    Remove unnecessary code.
    Indexing the full classpath in scalatest and flexmark takes a while,
    which slows down the editor startup time.

--- test-workspace/a/src/test/scala/example/UserTest.scala
+++ test-workspace/a/src/test/scala/example/UserTest.scala
@@ -1,14 +1,3 @@
 package example
 
-import scala.collection.immutable.AbstractMap
-import scala.runtime.CharRef
-import com.vladsch.flexmark.html.HtmlRenderer
-
-class UserTest extends org.scalatest.FunSuite {
-  test("") {
-    val x = List(1, 2).map(i => i + 2)
-    new CharRef('a').toString
-    CharRef.create('a')
-    java.io.File.pathSeparator
-  }
-}
+class UserTest {}
--- test-workspace/build.sbt
+++ test-workspace/build.sbt
@@ -4,10 +4,6 @@ inThisBuild(
     addCompilerPlugin(
       "org.scalameta" % "semanticdb-scalac" % "2.1.1" cross CrossVersion.full
     ),
-    libraryDependencies ++= List(
-      "com.vladsch.flexmark" % "flexmark-all" % "0.26.4",
-      ("org.scalatest" %% "scalatest" % "3.0.3" % Test).withSources()
-    ),
     scalacOptions += "-Yrangepos"
   )
 )
