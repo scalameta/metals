4fa58ce538d099bf617883e5e0bb57b1c38278b4
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-11-23 21:11:47 +0100

    Properly keep track of argument count in signature help.
    Previously, we found the first open paren but now we keep a stack of how
    many open parens are in between.
    This change makes it possible to return the correct active parameter,
    as well as filtering out overloads that have too few parameters.
    This also means we no longer return bogus results when outside of
    an argument list.

--- metaserver/src/main/scala/scala/meta/languageserver/compiler/SignatureHelpProvider.scala
+++ metaserver/src/main/scala/scala/meta/languageserver/compiler/SignatureHelpProvider.scala
@@ -1,7 +1,9 @@
 package scala.meta.languageserver.compiler
 
+import scala.annotation.tailrec
 import scala.tools.nsc.interactive.Global
 import scala.reflect.internal.util.Position
+import scala.reflect.internal.util.SourceFile
 import langserver.types.ParameterInformation
 import langserver.types.SignatureHelp
 import langserver.types.SignatureInformation
@@ -14,19 +16,17 @@ object SignatureHelpProvider {
     // in cases like `foo(bar(), <cursor>)` since we will find the
     // symbol of `bar` when we want to match against `foo`.
     // Related https://github.com/scalameta/language-server/issues/52
-    val lastParenOffset =
-      position.source.content.lastIndexOf('(', position.point)
-    if (lastParenOffset < 0) {
-      // bail if we can't find open paren to prevent false positives.
-      empty
-    } else {
-      val lastParenPosition = position.withPoint(lastParenOffset)
+    findEnclosingCallSite(position).fold(empty) { callSite =>
+      val lastParenPosition = position.withPoint(callSite.openParenOffset)
       val signatureInformations = for {
         member <- compiler
           .completionsAt(lastParenPosition)
           .matchingResults()
           .distinct
         if member.sym.isMethod
+        if member.sym.asMethod.paramLists.headOption.exists { paramList =>
+          paramList.length > callSite.activeArgument
+        }
       } yield {
         val sym = member.sym.asMethod
         val parameterInfos = sym.paramLists.headOption.map { params =>
@@ -48,8 +48,32 @@ object SignatureHelpProvider {
         // TODO(olafur) populate activeSignature and activeParameter fields, see
         // https://github.com/scalameta/language-server/issues/52
         activeSignature = None,
-        activeParameter = None
+        activeParameter = Some(callSite.activeArgument)
       )
     }
+  }
+  case class CallSite(openParenOffset: Int, activeArgument: Int)
+  private def findEnclosingCallSite(
+      caret: Position
+  ): Option[CallSite] = {
+    @tailrec
+    def loop(i: Int, openParens: Int, commas: Int): Option[CallSite] = {
+      if (i < 0) None
+      else {
+        val char = caret.source.content(i)
+        char match {
+          case '(' =>
+            if (openParens == 0) Some(CallSite(i, commas))
+            else loop(i - 1, openParens - 1, commas)
+          case ')' =>
+            loop(i - 1, openParens + 1, commas)
+          case ',' if openParens == 0 =>
+            loop(i - 1, openParens, commas + 1)
+          case _ =>
+            loop(i - 1, openParens, commas)
+        }
+      }
+    }
+    loop(caret.point, 0, 0)
   }
 }
--- metaserver/src/test/scala/tests/compiler/SignatureHelpTest.scala
+++ metaserver/src/test/scala/tests/compiler/SignatureHelpTest.scala
@@ -2,6 +2,7 @@ package tests.compiler
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.meta.languageserver.compiler.SignatureHelpProvider
+import langserver.types.SignatureHelp
 import play.api.libs.json.Json
 
 object SignatureHelpTest extends CompilerSuite {
@@ -9,16 +10,26 @@ object SignatureHelpTest extends CompilerSuite {
   def check(
       filename: String,
       code: String,
-      expected: String
+      fn: SignatureHelp => Unit
   ): Unit = {
     targeted(
       filename,
       code, { pos =>
-        val result = SignatureHelpProvider.signatureHelp(compiler, pos)
-        val obtained = Json.prettyPrint(Json.toJson(result))
-        assertNoDiff(obtained, expected)
+        val obtained = SignatureHelpProvider.signatureHelp(compiler, pos)
+        fn(obtained)
       }
     )
+  }
+
+  def check(
+      filename: String,
+      code: String,
+      expected: String
+  ): Unit = {
+    check(filename, code, { result =>
+      val obtained = Json.prettyPrint(Json.toJson(result))
+      assertNoDiff(obtained, expected)
+    })
   }
 
   check(
@@ -42,7 +53,8 @@ object SignatureHelpTest extends CompilerSuite {
       |    "parameters" : [ {
       |      "label" : "assertion: Boolean"
       |    } ]
-      |  } ]
+      |  } ],
+      |  "activeParameter" : 0
       |}""".stripMargin
   )
 
@@ -54,7 +66,45 @@ object SignatureHelpTest extends CompilerSuite {
       |}
     """.stripMargin,
     """
-      |
+      |{
+      |  "signatures" : [ {
+      |    "label" : "assert(assertion: Boolean, message: => Any)Unit",
+      |    "parameters" : [ {
+      |      "label" : "assertion: Boolean"
+      |    }, {
+      |      "label" : "message: => Any"
+      |    } ]
+      |  } ],
+      |  "activeParameter" : 1
+      |}
     """.stripMargin
+  )
+
+  check(
+    "no-result",
+    """
+      |object c {
+      |  assert(true)
+      |  <<caret>>
+      |}
+    """.stripMargin, { obtained =>
+      assert(obtained.signatures.isEmpty)
+    }
+  )
+
+  check(
+    "tricky-comma",
+    """
+      |object b {
+      |  Predef.assert(","<<caret>>
+      |}
+    """.stripMargin, { obtained =>
+      val activeParameter = obtained.activeParameter
+      assert(activeParameter.nonEmpty)
+      // TODO(olafur) should be 0 since the comma is quoted
+      // we can fix this if we use the tokenizer, but then we have to handle
+      // other tricky cases like unclosed string literals.
+      assert(activeParameter.get == 1)
+    }
   )
 }
