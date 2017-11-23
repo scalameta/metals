a2f684fdffee2c80ff1d5fd64673580b9d6f32c6
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-11-23 09:56:24 +0100

    Prevent false positive signature helps by locating (
    Previously we blindly used the offset provided by the client even if
    that offset didn't point to a trigger character. Now we locate
    the relevant open paren.

--- metaserver/src/main/scala/scala/meta/languageserver/Compiler.scala
+++ metaserver/src/main/scala/scala/meta/languageserver/Compiler.scala
@@ -62,26 +62,48 @@ class Compiler(
     getCompiler(path, line, column - 1).fold(noSignatureHelp) {
       case (compiler, position) =>
         logger.info(s"Signature help at $path:$line:$column")
-        val signatureInformations = for {
-          member <- compiler.completionsAt(position).matchingResults().distinct
-          if member.sym.isMethod
-        } yield {
-          val sym = member.sym.asMethod
-          val parameterInfos = sym.paramLists.headOption.map { params =>
-            params.map { param =>
-              ParameterInformation(
-                label = s"${param.nameString}: ${param.info.toLongString}",
-                documentation = None
-              )
+        // Find the last leading open paren to get the method symbol
+        // of this argument list. Note, this may still cause false positives
+        // in cases like `foo(bar(), <cursor>)` since we will find the
+        // symbol of `bar` when we want to match against `foo`.
+        // Related https://github.com/scalameta/language-server/issues/52
+        val lastParenOffset =
+          position.source.content.lastIndexOf('(', position.point)
+        if (lastParenOffset < 0) {
+          // bail if we can't find open paren to prevent false positives.
+          noSignatureHelp
+        } else {
+          val lastParenPosition = position.withPoint(lastParenOffset)
+          val signatureInformations = for {
+            member <- compiler
+              .completionsAt(lastParenPosition)
+              .matchingResults()
+              .distinct
+            if member.sym.isMethod
+          } yield {
+            val sym = member.sym.asMethod
+            val parameterInfos = sym.paramLists.headOption.map { params =>
+              params.map { param =>
+                ParameterInformation(
+                  label = s"${param.nameString}: ${param.info.toLongString}",
+                  documentation = None
+                )
+              }
             }
+            SignatureInformation(
+              label = s"${sym.nameString}${sym.info.toLongString}",
+              documentation = None,
+              parameters = parameterInfos.getOrElse(Nil)
+            )
           }
-          SignatureInformation(
-            label = s"${sym.nameString}${sym.info.toLongString}",
-            documentation = None,
-            parameters = parameterInfos.getOrElse(Nil)
+          SignatureHelp(
+            signatures = signatureInformations,
+            // TODO(olafur) populate activeSignature and activeParameter fields, see
+            // https://github.com/scalameta/language-server/issues/52
+            activeSignature = None,
+            activeParameter = None
           )
         }
-        SignatureHelp(signatureInformations, None, None)
     }
   }
 
