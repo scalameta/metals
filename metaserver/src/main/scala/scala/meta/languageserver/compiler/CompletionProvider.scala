0248711127286daaf28e73340df42f8dddbe6151
Gabriele Petronella <gabriele@buildo.io>
2017-11-29 00:19:04 +0900

    Sort completion results by relevance

--- metaserver/src/main/scala/scala/meta/languageserver/compiler/CompletionProvider.scala
+++ metaserver/src/main/scala/scala/meta/languageserver/compiler/CompletionProvider.scala
@@ -17,7 +17,7 @@ object CompletionProvider extends LazyLogging {
       compiler: Global,
       cursor: Cursor
   ): CompletionList = {
-    import compiler.CompletionResult
+    import compiler._
 
     def completionItemKind(r: CompletionResult#M): CompletionItemKind = {
       if (r.sym.isPackage) CompletionItemKind.Module
@@ -32,6 +32,22 @@ object CompletionProvider extends LazyLogging {
       else CompletionItemKind.Value
     }
 
+    def computeRelevance(
+        sym: Symbol,
+        viaView: Symbol,
+        inherited: Boolean
+    ): Int = {
+      var relevance = 0
+      if (sym.isLocalToBlock) relevance += 10
+      if (sym.hasGetter) relevance += 5
+      if (!inherited) relevance += 10
+      if (viaView == NoSymbol) relevance += 20
+      if (!sym.hasPackageFlag) relevance += 30
+      if (sym.owner != definitions.AnyClass && sym.owner != definitions.AnyRefClass && sym.owner != definitions.ObjectClass)
+        relevance += 40
+      relevance
+    }
+
     val unit = ScalacProvider.addCompilationUnit(
       global = compiler,
       code = cursor.contents,
@@ -41,17 +57,26 @@ object CompletionProvider extends LazyLogging {
     val position = unit.position(cursor.offset)
     val isUsedLabel = mutable.Set.empty[String]
     val items = List.newBuilder[CompletionItem]
-    safeCompletionsAt(compiler, position).foreach { r =>
-      val label = r.symNameDropLocal.decoded
-      if (!isUsedLabel(label)) {
-        isUsedLabel += label
-        items += CompletionItem(
-          label = label,
-          detail = Some(r.sym.signatureString),
-          kind = Some(completionItemKind(r))
-        )
+    safeCompletionsAt(compiler, position)
+      .sortBy {
+        case TypeMember(sym, _, true, inherited, viaView) =>
+          // logger.debug(s"Relevance of ${sym.name}: ${computeRelevance(sym, viaView, inherited)}")
+          -computeRelevance(sym, viaView, inherited)
+        case ScopeMember(sym, _, true, _) =>
+          -computeRelevance(sym, NoSymbol, false)
+        case _ => 0
       }
-    }
+      .foreach { r =>
+        val label = r.symNameDropLocal.decoded
+        if (!isUsedLabel(label)) {
+          isUsedLabel += label
+          items += CompletionItem(
+            label = label,
+            detail = Some(r.sym.signatureString),
+            kind = Some(completionItemKind(r))
+          )
+        }
+      }
     CompletionList(isIncomplete = false, items = items.result())
   }
 
