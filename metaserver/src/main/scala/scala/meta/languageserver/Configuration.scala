e4fee53d524b6b2ef8bcc79da80bace874e50c86
Gabriele Petronella <gabriele@buildo.io>
2017-12-23 11:24:59 +0100

    Format

--- metaserver/src/main/scala/scala/meta/languageserver/Configuration.scala
+++ metaserver/src/main/scala/scala/meta/languageserver/Configuration.scala
@@ -3,9 +3,9 @@ package scala.meta.languageserver
 import play.api.libs.json._
 
 case class Configuration(
-  scalafmtConfPath: String,
-  scalafixConfPath: String,
-  enableCompletions: Boolean,
+    scalafmtConfPath: String,
+    scalafixConfPath: String,
+    enableCompletions: Boolean,
 )
 object Configuration {
   implicit val format: OFormat[Configuration] = Json.format[Configuration]
