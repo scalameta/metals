cb4d1e2c5f0b3475ec23740f04f0a0357a552dd8
Alexey Alekhin <laughedelic@gmail.com>
2018-04-05 01:02:10 +0200

    Add release shortcut command

--- bin/release.sh
+++ bin/release.sh
@@ -3,7 +3,7 @@ set -ev
 
 # release only on merge commits or tags
 if [[ "$(git rev-list --merges HEAD^..HEAD)" || "$TRAVIS_TAG" ]]; then
-    sbt '+releaseEarly' 'project sbt-metals' '^releaseEarly'
+    sbt release
 else
     echo 'skipping release'
 fi
