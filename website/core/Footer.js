aa423636101e9d779b960be3ee7c42385d4680f3
Gabriele Petronella <gabriele@buildo.io>
2018-04-25 15:07:48 +0200

    Add links in footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -1,11 +1,53 @@
 const React = require('react');
 
+const siteConfig = require(process.cwd() + '/siteConfig.js');
+
 class Footer extends React.Component {
   render() {
     const currentYear = new Date().getFullYear();
     const { copyright, colors: { secondaryColor } } = this.props.config;
     return (
       <footer className="nav-footer" id="footer" style={{ backgroundColor: secondaryColor }}>
+      <section className="sitemap">
+          {this.props.config.footerIcon && (
+            <a href={this.props.config.baseUrl} className="nav-home">
+              <img
+                src={`${this.props.config.baseUrl}${this.props.config
+                  .footerIcon}`}
+                alt={this.props.config.title}
+                width="66"
+                height="58"
+              />
+            </a>
+          )}
+          <div>
+            <h5>Docs</h5>
+            <a
+              href={`
+                ${this.props.config.baseUrl}docs/overview.html`}
+            >
+              Overview
+            </a>
+            <a
+              href={`
+                ${this.props.config.baseUrl}docs/installation-contributors.html`}
+            >
+              Contributors
+            </a>
+          </div>
+          <div>
+            <h5>Community</h5>
+            <a href="https://gitter.im/scalameta/metals" target="_blank">
+              Chat on Gitter
+            </a>
+          </div>
+          <div>
+            <h5>More</h5>
+            <a href={siteConfig.repoUrl} target="_blank">
+              GitHub
+            </a>
+          </div>
+        </section>
         <section className="copyright">{copyright}</section>
       </footer>
     );
