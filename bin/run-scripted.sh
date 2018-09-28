6bed4da984a1ad9a6cb477663596c4b32e810f20
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-09-28 15:36:38 +0200

    Run scripted tests in both sbt 1 and 0.13

--- bin/run-scripted.sh
+++ bin/run-scripted.sh
@@ -3,7 +3,7 @@ set -eux
 
 if [[ $(git diff master --name-only | grep sbt-metals) ]]; then
   echo "Diff detected in sbt-metals, running scripted tests"
-  sbt ^sbt-metals/scripted
+  sbt "+sbt-metals/scripted"
 else
   echo "No diff detected in sbt-metals, skipping scripted tests"
 fi
