b84fa4479e614afa4cb6a5ce5b52873c42d1d1ed
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-09-28 16:33:14 +0200

    Remove unstable parts
    This commit removes unstable features that nobody is working on at the
    moment:
    
    - completions and signature help with the presentation compiler.
    - diagnostics as you type with the presentation compiler.
    - hover with an old scalafix type printer. This will be replaced
      with one of the newer type printers that is based on the new
      symbol tables.
    - linting with Scalafix, the integration uses an old internal API.
      Scalafix now has a public API for build tools that we should hook into
      instead. Refactoring to use the new API is equivalent to a full
      rewrite so we may as well remove the current integration.
    
    This commit also updates the project overview to reflect the current
    status.

--- metals/src/main/scala/scala/meta/metals/Semanticdbs.scala
+++ metals/src/main/scala/scala/meta/metals/Semanticdbs.scala
@@ -1,17 +1,6 @@
 package scala.meta.metals
 
 import java.nio.file.Files
-import scala.meta.interactive.InteractiveSemanticdb
-import scala.meta.metals.compiler.ScalacProvider
-import scala.meta.metals.compiler.CompilerEnrichments._
-import scala.meta.parsers.ParseException
-import scala.meta.semanticdb
-import scala.meta.tokenizers.TokenizeException
-import scala.tools.nsc.interactive.Global
-import scala.tools.nsc.reporters.StoreReporter
-import scala.util.control.NonFatal
-import scala.{meta => m}
-import org.langmeta.inputs.Input
 import org.langmeta.internal.io.PathIO
 import org.langmeta.internal.semanticdb.schema.Database
 import org.langmeta.io.AbsolutePath
@@ -22,59 +11,6 @@ object Semanticdbs {
   object File {
     def unapply(path: RelativePath): Boolean =
       PathIO.extension(path.toNIO) == "semanticdb"
-  }
-
-  def toSemanticdb(
-      input: Input.VirtualFile,
-      scalacProvider: ScalacProvider
-  ): Option[semanticdb.Database] =
-    for {
-      compiler <- scalacProvider.getCompiler(input)
-    } yield toSemanticdb(input, compiler)
-
-  def toSemanticdb(
-      input: Input.VirtualFile,
-      compiler: Global,
-  ): semanticdb.Database = {
-    val doc = try {
-      InteractiveSemanticdb.toDocument(
-        compiler = compiler,
-        code = input.value,
-        filename = input.path,
-        timeout = 10000
-      )
-    } catch {
-      case NonFatal(err) =>
-        err match {
-          case _: ParseException | _: TokenizeException =>
-          // ignore, expected.
-          case _ =>
-            scribe.error(s"Failed to emit semanticdb for ${input.path}", err)
-        }
-        toMessageOnlySemanticdb(input, compiler)
-    }
-    semanticdb.Database(doc.copy(language = "Scala212") :: Nil)
-  }
-
-  def toMessageOnlySemanticdb(
-      input: Input,
-      compiler: Global
-  ): m.Document = {
-    val messages = compiler.reporter match {
-      case r: StoreReporter =>
-        r.infos.collect {
-          case r.Info(pos, msg, severity) =>
-            val mpos = pos.toMeta(input)
-            val msev = severity match {
-              case r.INFO => m.Severity.Info
-              case r.WARNING => m.Severity.Warning
-              case _ => m.Severity.Error
-            }
-            m.Message(mpos, msev, msg)
-        }.toList
-      case _ => Nil
-    }
-    semanticdb.Document(input, "", Nil, messages, Nil, Nil)
   }
 
   def loadFromFile(
