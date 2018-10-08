30e011c3748c36f4314bbbb5d23acbb205d6ccf6
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-10-08 20:42:15 +0200

    Refactor the build
    - upgrade scalameta
    - remove unnecessary projects
    - use resource generators to feed input project
      classpath/sourceDirectories/sourceJars into test suite.

---
+++ project/InputProperties.scala
@@ -0,0 +1,59 @@
+import java.io.File
+import sbt.Keys._
+import sbt._
+
+object InputProperties extends AutoPlugin {
+  var file: Option[File] = None
+  def resourceGenerator(input: Reference): Def.Initialize[Task[Seq[File]]] =
+    Def.taskDyn {
+      file.synchronized {
+        file match {
+          case Some(value) =>
+            Def.task(List(value))
+          case None =>
+            resourceGeneratorImpl(input)
+        }
+      }
+    }
+  def resourceGeneratorImpl(input: Reference): Def.Initialize[Task[Seq[File]]] =
+    Def.task {
+      val out = managedResourceDirectories
+        .in(Compile)
+        .value
+        .head / "metals-input.properties"
+      val props = new java.util.Properties()
+      props.put(
+        "sourceroot",
+        baseDirectory.in(ThisBuild).value.toString
+      )
+      val sourceJars = for {
+        configurationReport <- updateClassifiers.in(input).value.configurations
+        moduleReport <- configurationReport.modules
+        (artifact, file) <- moduleReport.artifacts
+        if artifact.classifier.contains("sources")
+      } yield file
+      props.put(
+        "dependencySources",
+        sourceJars.map(_.toPath).distinct.mkString(File.pathSeparator)
+      )
+      props.put(
+        "sourceDirectories",
+        List(
+          unmanagedSourceDirectories.in(input, Compile).value,
+          unmanagedSourceDirectories.in(input, Test).value
+        ).flatten.mkString(File.pathSeparator)
+      )
+      props.put(
+        "classpath",
+        fullClasspath
+          .in(input, Test)
+          .value
+          .map(_.data)
+          .mkString(File.pathSeparator)
+      )
+      IO.write(props, "input", out)
+      file = Some(out)
+      List(out)
+    }
+
+}
--- sbt-metals/src/main/scala/scala/meta/sbt/MetalsPlugin.scala
+++ sbt-metals/src/main/scala/scala/meta/sbt/MetalsPlugin.scala
@@ -36,11 +36,11 @@ package scala.meta.sbt {
 
     def scala211 = "2.11.12"
 
-    def scala212 = "2.12.4"
+    def scala212 = "2.12.7"
 
     def supportedScalaVersions = List(scala212, scala211)
 
-    def semanticdbVersion = "2.1.7"
+    def semanticdbVersion = "4.0.0"
 
     val metalsBuildInfo = taskKey[Map[String, String]](
       "List of key/value pairs for build information such as classpath/sourceDirectories"
