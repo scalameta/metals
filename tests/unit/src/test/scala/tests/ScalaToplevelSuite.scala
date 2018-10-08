41d291ed1716eac3fdfed797e4f79606b7f62638
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-10-08 22:17:48 +0200

    Add ScalaToplevelSuite to assert that it's a subset of Mtags.

---
+++ tests/unit/src/test/scala/tests/ScalaToplevelSuite.scala
@@ -0,0 +1,29 @@
+package tests
+
+import scala.meta.internal.io.FileIO
+import scala.meta.internal.mtags.Enrichments._
+import scala.meta.internal.mtags.Mtags
+
+/**
+ * Assert the symbols emitted by ScalaToplevelMtags is a subset of ScalaMtags
+ */
+class ScalaToplevelSuite extends BaseSuite {
+  val testClasspath = Libraries.suite.flatMap(_.sources().entries)
+  testClasspath.foreach { entry =>
+    test(entry.toNIO.getFileName.toString) {
+      FileIO.withJarFileSystem(entry, create = false) { root =>
+        FileIO.listAllFilesRecursively(root).foreach { file =>
+          if (file.toNIO.getFileName.toString.endsWith(".scala")) {
+            val input = file.toInput
+            val scalaMtags = Mtags.toplevels(Mtags.index(input))
+            val scalaToplevelMtags = Mtags.toplevels(input)
+            val obtained = scalaToplevelMtags.mkString("\n")
+            val expected = scalaMtags.mkString("\n")
+            assertNoDiff(obtained, expected, input.text)
+          }
+        }
+      }
+    }
+  }
+
+}
