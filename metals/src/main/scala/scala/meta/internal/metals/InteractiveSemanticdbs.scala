ff43aafa8ffb26d9b9aed46b22b7577be9c61291
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-11-26 11:45:38 +0100

    Fix deadlock in ConcurrentHashMap
    Interestingly, `ConcurrentHashMap` freezes in one of the tests while
    `Collections.synchronizedMap` works fine. My guess at why this happens
    is that `ConcurrentHashMap` should only be used when the "if absent" function
    is super fast and simple, whereas in InteractiveSemanticdbs the method
    takes at least 100-200ms to construct a compiler instance.

--- metals/src/main/scala/scala/meta/internal/metals/InteractiveSemanticdbs.scala
+++ metals/src/main/scala/scala/meta/internal/metals/InteractiveSemanticdbs.scala
@@ -7,7 +7,6 @@ import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.Collections
-import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicReference
@@ -48,10 +47,14 @@ final class InteractiveSemanticdbs(
     with Semanticdbs {
   import messages._
   private val activeDocument = new AtomicReference[Option[String]](None)
-  private val globalCache =
-    new ConcurrentHashMap[BuildTargetIdentifier, Global]()
-  private val textDocumentCache =
-    new ConcurrentHashMap[AbsolutePath, s.TextDocument]()
+  // Not ConcurrentHashMap since it can hit on deadlocks inside computeIfAbsent
+  // when the "if absent" function is slow.
+  private val globalCache = Collections.synchronizedMap(
+    new java.util.HashMap[BuildTargetIdentifier, Global]()
+  )
+  private val textDocumentCache = Collections.synchronizedMap(
+    new java.util.HashMap[AbsolutePath, s.TextDocument]()
+  )
 
   def reset(): Unit = {
     textDocumentCache.clear()
