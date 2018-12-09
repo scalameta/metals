4502bb05e4d6adb6a22492cd21ab51d4f37ee9ed
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-12-09 13:23:30 +0100

    Update vim installation instructions
    - document all `~/.vimrc` config in one step instead of two steps.
    - use stable release by default, but show snapshot version in table.

--- metals-docs/src/main/scala/docs/BootstrapModifier.scala
+++ metals-docs/src/main/scala/docs/BootstrapModifier.scala
@@ -6,7 +6,6 @@ import scala.meta.inputs.Input
 
 class BootstrapModifier extends StringModifier {
   override val name: String = "bootstrap"
-  val snapshot = Snapshot.latest()
   override def process(
       info: String,
       code: Input,
@@ -15,8 +14,13 @@ class BootstrapModifier extends StringModifier {
     info.split(" ") match {
       case Array(binary, client) =>
         s"""
-           |Next, build a `$binary` binary using the
+           |Next, build a `$binary` binary for the latest Metals release using the
            |[Coursier](https://github.com/coursier/coursier) command-line interface.
+           |
+           || Version                   | Published             | Resolver                |
+           || ---                       | ---                   | ---                     |
+           ||  ${Docs.release.version}  | ${Docs.release.date}  | `-r sonatype:releases`  |
+           ||  ${Docs.snapshot.version} | ${Docs.snapshot.date} | `-r sonatype:snapshots` |
            |
            |```sh
            |# Make sure to use coursier v1.1.0-M9 or newer.
@@ -29,19 +33,12 @@ class BootstrapModifier extends StringModifier {
            |  --java-opt -Xms1G \\
            |  --java-opt -Xmx4G  \\
            |  --java-opt -Dmetals.client=$client \\
-           |  org.scalameta:metals_2.12:${snapshot.version} \\
+           |  org.scalameta:metals_2.12:${Docs.release.version} \\
            |  -r bintray:scalacenter/releases \\
            |  -r sonatype:snapshots \\
-           |  -o $binary -f
+           |  -o /usr/local/bin/$binary -f
            |```
-           |
-           |(optional) Feel free to place this binary anywhere on your `$$PATH`, for example
-           |adapt the `-o` flag like this:
-           |
-           |```diff
-           |-  -o $binary -f
-           |+  -o /usr/local/bin/$binary -f
-           |```
+           |Make sure the generated `$binary` binary is available on your `$$PATH`.
            |""".stripMargin
       case _ =>
         reporter.error(s"Invalid info '$info'. Expected '<binary> <client>'")
