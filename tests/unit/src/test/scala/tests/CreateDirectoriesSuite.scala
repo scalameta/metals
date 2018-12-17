52a0ed17df003b3483f8f2eea5033d872eb954cf
Gabriele Petronella <gabriele@buildo.io>
2018-12-17 14:25:03 +0100

    Add test to CreateDirectoriesSuite

--- tests/unit/src/test/scala/tests/CreateDirectoriesSuite.scala
+++ tests/unit/src/test/scala/tests/CreateDirectoriesSuite.scala
@@ -1,12 +1,13 @@
 package tests
 
 import java.nio.file.Files
+import java.nio.file.FileAlreadyExistsException
 import scala.meta.io.AbsolutePath
 import scala.meta.internal.metals.MetalsEnrichments._
 
 object CreateDirectoriesSuite extends BaseSuite {
 
-  test("symlinks") {
+  test("symlink-parent") {
 
     /**
      * Starting from this layout, create /.sbt/1.0/plugins/foo/bar/
@@ -31,6 +32,31 @@ object CreateDirectoriesSuite extends BaseSuite {
     assert(!directFooBarPath.isDirectory)
     symlinkFooBarPath.createDirectories()
     assert(directFooBarPath.isDirectory)
+  }
+
+  test("symlink-target") {
+
+    /**
+     * Starting from this layout, try to create /.sbt/1.0/plugins
+     * and check that no error is thrown
+     *
+     *  /
+     *  ├── plugins
+     *  └── .sbt
+     *      └── 1.0
+     *          └── plugins -> ../../../plugins
+     */
+    val root = Files.createTempDirectory("scalameta")
+    val plugins = Files.createDirectory(root.resolve("plugins"))
+    val sbt1 = Files.createDirectories(root.resolve(".sbt").resolve("1.0"))
+    val symlinkPluginsPath = sbt1.resolve("plugins")
+    Files.createSymbolicLink(symlinkPluginsPath, plugins)
+
+    // check that we get an exception using the default nio method
+    intercept[FileAlreadyExistsException] {
+      Files.createDirectories(symlinkPluginsPath)
+    }
+    AbsolutePath(symlinkPluginsPath).createDirectories()
   }
 
 }
