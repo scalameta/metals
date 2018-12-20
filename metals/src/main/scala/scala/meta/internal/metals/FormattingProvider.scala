9607b1f4b537785871291a09db49dde73729a8f1
Gabriele Petronella <gabriele@buildo.io>
2018-12-20 12:10:09 +0100

    Address review comments
    Notable changes:
    - remove scalafmtVersion and scalafmtRequireConfigFile options
    - read scalafmtVersion from .scalafmt.conf
    - classload Scalafmt instances lazily
    - keep a cache with all classloaded Scalafmt instance

--- metals/src/main/scala/scala/meta/internal/metals/FormattingProvider.scala
+++ metals/src/main/scala/scala/meta/internal/metals/FormattingProvider.scala
@@ -7,6 +7,8 @@ import scala.language.reflectiveCalls
 import java.util
 import java.nio.file.Files
 import scala.util.Try
+import scala.meta.internal.tokenizers.PlatformTokenizerCache
+import com.typesafe.config.ConfigFactory
 
 /**
  * Implement text formatting using Scalafmt
@@ -21,47 +23,42 @@ final class FormattingProvider(
   def format(path: AbsolutePath): util.List[l.TextEdit] = {
     val input = path.toInputFromBuffers(buffers)
     val fullDocumentRange = Position.Range(input, 0, input.chars.length).toLSP
+    val defaultScalafmtVersion = "1.5.1"
     (for {
-      scalafmt <- classloadedScalafmt
-      formatted <- scalafmtConfigPath match {
+      scalafmt <- scalafmtConfigPath match {
         case Some(configPath) =>
-          scalafmt
-            .format(input.text, configPath.toString, input.path)
-            .fold(e => {
-              scribe.error("Error while running Scalafmt", e)
-              None
-            }, Some(_))
-        case None if !userConfig().scalafmtRequireConfigFile =>
-          scalafmt
-            .format(input.text, input.path)
-            .fold(e => {
-              scribe.error("Error while running Scalafmt", e)
-              None
-            }, Some(_))
+          val scalafmtConf = ConfigFactory.parseFile(configPath.toNIO.toFile)
+          val scalafmtVersion = Try(scalafmtConf.getString("version")).toOption
+            .getOrElse(defaultScalafmtVersion)
+          classloadScalafmt(scalafmtVersion)
         case _ =>
           scribe.info(
             "Skipping formatting request because no Scalafmt config file is present"
           )
           None
       }
+      configPath <- scalafmtConfigPath
+      formatted <- scalafmt
+        .format(input.text, configPath.toString, input.path)
+        .fold(e => {
+          scribe.error(s"Scalafmt error: ${e.getMessage}")
+          None
+        }, Some(_))
     } yield {
       List(new l.TextEdit(fullDocumentRange, formatted))
     }).getOrElse(Nil).asJava
   }
 
   private def scalafmtConfigPath: Option[AbsolutePath] = {
-    val relativePath =
-      userConfig().scalafmtConfigPath.getOrElse(".scalafmt.conf")
-    Some(workspace.resolve(relativePath))
+    Some(workspace.resolve(userConfig().scalafmtConfigPath))
       .filter(path => Files.isRegularFile(path.toNIO))
   }
 
-  private val classloadedScalafmt: Option[Scalafmt] = {
-    embedded.scalafmtJars match {
+  private def classloadScalafmt(version: String): Option[Scalafmt] = {
+    embedded.scalafmtJars(version) match {
       case Some(classloader) =>
         type Scalafmt210 = {
           def format(code: String, configFile: String, filename: String): String
-          def format(code: String, filename: String): String
         }
         val scalafmt210 = classloader
           .loadClass("org.scalafmt.cli.Scalafmt210")
@@ -72,14 +69,16 @@ final class FormattingProvider(
               code: String,
               configFile: String,
               filename: String
-          ): Try[String] =
-            Try(scalafmt210.format(code, configFile, filename))
-          def format(code: String, filename: String): Try[String] =
-            Try(scalafmt210.format(code, filename))
+          ): Try[String] = {
+            val r = Try(scalafmt210.format(code, configFile, filename))
+            // See https://github.com/scalameta/scalameta/issues/1068
+            PlatformTokenizerCache.megaCache.clear()
+            r
+          }
         })
       case None =>
         scribe.error(
-          "not found: scalafmt classloader. Make sure you have specified an existing version of Scalafmt in the settings"
+          "not found: Scalafmt classloader. Make sure you have specified an existing version of Scalafmt in the settings"
         )
         None
     }
@@ -89,5 +88,4 @@ final class FormattingProvider(
 
 private trait Scalafmt {
   def format(code: String, configFile: String, filename: String): Try[String]
-  def format(code: String, filename: String): Try[String]
 }
--- metals/src/main/scala/scala/meta/internal/metals/UserConfiguration.scala
+++ metals/src/main/scala/scala/meta/internal/metals/UserConfiguration.scala
@@ -1,5 +1,6 @@
 package scala.meta.internal.metals
 
+import scala.meta.RelativePath
 import com.google.gson.JsonObject
 import com.google.gson.JsonPrimitive
 import com.google.gson.JsonElement
@@ -15,12 +16,15 @@ import scala.util.Try
 case class UserConfiguration(
     javaHome: Option[String] = None,
     sbtScript: Option[String] = None,
-    scalafmtVersion: Option[String] = None,
-    scalafmtRequireConfigFile: Boolean = true,
-    scalafmtConfigPath: Option[String] = None
+    scalafmtConfigPath: RelativePath =
+      UserConfiguration.default.scalafmtConfigPath
 )
 
 object UserConfiguration {
+  object default {
+    val scalafmtConfigPath = RelativePath(".scalafmt.conf")
+  }
+
   def options: List[UserConfigurationOption] = List(
     UserConfigurationOption(
       "java-home",
@@ -41,27 +45,9 @@ object UserConfiguration {
         |""".stripMargin
     ),
     UserConfigurationOption(
-      "scalafmt-version",
-      "1.5.1",
-      "1.3.0",
-      "Scalafmt version",
-      """Optional Scalafmt version"""
-    ),
-    UserConfigurationOption(
-      "scalafmt-require-config-file",
-      "true",
-      "false",
-      "Scalafmt require config file",
-      """|Whether a Scalafmt config file is required for formatting to work.
-         |By default, Metals ignores textFormatting/requests if no Scalafmt config file is found.
-         |Update this setting to `true` if you prefer to run Scalafmt with its default configuration
-         |when no Scalafmt config file is present.
-      """.stripMargin
-    ),
-    UserConfigurationOption(
-      "scalafmt-require-config-path",
-      ".scalafmt.conf",
-      "path/to/my/scalafmt.conf",
+      "scalafmt-config-path",
+      default.scalafmtConfigPath.toString,
+      "project/.scalafmt.conf",
       "Scalafmt config path",
       "Optional Scalafmt config file path, relative to the workspace root"
     )
@@ -100,34 +86,20 @@ object UserConfiguration {
             .filter(_.nonEmpty)
       )
 
-    def getBooleanKey(key: String): Option[Boolean] =
-      getKey(
-        key,
-        value =>
-          Try(value.getAsBoolean).fold(_ => {
-            errors += s"json error: key '$key' should have value of type boolean but obtained $value"
-            None
-          }, Some(_))
-      )
-
     val javaHome =
       getStringKey("java-home")
     val sbtScript =
       getStringKey("sbt-script")
-    val scalafmtVersion =
-      getStringKey("scalafmt-version")
-    val scalafmtRequireConfigFile =
-      getBooleanKey("scalafmt-require-config-file").getOrElse(true)
     val scalafmtConfigPath =
       getStringKey("scalafmt-config-path")
+        .map(RelativePath(_))
+        .getOrElse(default.scalafmtConfigPath)
 
     if (errors.isEmpty) {
       Right(
         UserConfiguration(
           javaHome,
           sbtScript,
-          scalafmtVersion,
-          scalafmtRequireConfigFile,
           scalafmtConfigPath
         )
       )
