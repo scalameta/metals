23ed32e5bcdcdb9fbbf7d9b97c8e924622960fa1
Gabriele Petronella <gabriele@buildo.io>
2018-12-21 17:06:39 +0100

    Don't attempt build import if sbt version is < 0.13.7

--- metals/src/main/scala/scala/meta/internal/metals/Messages.scala
+++ metals/src/main/scala/scala/meta/internal/metals/Messages.scala
@@ -116,7 +116,7 @@ class Messages(icons: Icons) {
       command = ServerCommands.OpenBrowser(learnMoreUrl)
     )
     def toFixMessage =
-      "To fix this problem, upgrade to sbt v1.2.1+ or manually import the build."
+      "To fix this problem, upgrade to sbt v0.13.17+ or v1.2.1+ or manually import the build."
     def dismissForever: MessageActionItem =
       new MessageActionItem("Don't show again")
     def learnMore: MessageActionItem =
