f1b83d16b49cda8d455fb13cc5f399baf637aaad
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-02-18 10:27:50 +0100

    Finish incomplete comment.

--- metals/src/main/scala/scala/meta/internal/metals/PresentationCompilerClassLoader.scala
+++ metals/src/main/scala/scala/meta/internal/metals/PresentationCompilerClassLoader.scala
@@ -4,7 +4,9 @@ package scala.meta.internal.metals
  * ClassLoader that is used to reflectively invoke presentation compiler APIs.
  *
  * The presentation compiler APIs are compiled against exact Scala versions of the compiler
- * while Metals only runs in a single Scala version. In order to communicate with
+ * while Metals only runs in a single Scala version. In order to communicate between Metals and the
+ * reflectively loaded compiler, this classloader shares a subset of Java classes that appear in
+ * method signatures of the `PresentationCompiler` class.
  */
 class PresentationCompilerClassLoader(parent: ClassLoader)
     extends ClassLoader(null) {
