22cc14694d3f4c40cd50d3599d28b19b0a071118
Marek Żarnowski <mzarnowski@virtuslab.com>
2019-04-04 11:26:13 +0200

    remove unnecessary explicit Traverser

--- metals/src/main/scala/scala/meta/internal/metals/FoldingRangeProvider.scala
+++ metals/src/main/scala/scala/meta/internal/metals/FoldingRangeProvider.scala
@@ -3,15 +3,13 @@ package scala.meta.internal.metals
 import java.util
 import org.eclipse.lsp4j.FoldingRange
 import org.eclipse.lsp4j.FoldingRangeCapabilities
-import scala.meta.Term.Block
+import scala.meta.Template
+import scala.meta.Term
+import scala.meta.Tree
+import scala.meta._
 import scala.meta.internal.metals.FoldingMode._
-import scala.meta.internal.metals.FoldingRangeKind.Factory
 import scala.meta.internal.metals.FoldingRangeKind.Region
 import scala.meta.io.AbsolutePath
-import scala.meta.transversers.SimpleTraverser
-import scala.meta.Position
-import scala.meta.Template
-import scala.meta.Tree
 
 final class FoldingRangeProvider(val trees: Trees, mode: FoldingMode) {
   def getRangedFor(path: AbsolutePath): util.List[FoldingRange] = {
@@ -22,34 +20,14 @@ final class FoldingRangeProvider(val trees: Trees, mode: FoldingMode) {
   }
 
   private def findFoldingRanges(tree: Tree): util.List[FoldingRange] = {
-    val ranges = new FoldingRanges
-    new RangeFinder(ranges).apply(tree)
+    val ranges = new FoldingRanges(mode)
+
+    tree traverse {
+      case block: Term.Block => ranges.add(Region, block.pos)
+      case template: Template => ranges.add(Region, template.pos)
+    }
+
     ranges.get
-  }
-
-  private class RangeFinder(ranges: FoldingRanges) extends SimpleTraverser {
-
-    private def fold(tree: Tree): Unit = tree match {
-      case _: Block => createRange(Region, tree.pos)
-      case _: Template => createRange(Region, tree.pos)
-      case _ =>
-    }
-
-    override def apply(tree: Tree): Unit = {
-      fold(tree)
-      super.apply(tree)
-    }
-
-    private def createRange(factory: Factory, pos: Position): Unit = {
-      val range = factory.create(
-        pos.startLine,
-        pos.startColumn,
-        pos.endLine,
-        pos.endColumn
-      )(mode)
-
-      ranges.add(range)
-    }
   }
 }
 
--- metals/src/main/scala/scala/meta/internal/metals/FoldingRanges.scala
+++ metals/src/main/scala/scala/meta/internal/metals/FoldingRanges.scala
@@ -3,12 +3,25 @@ package scala.meta.internal.metals
 import java.util
 import java.util.Collections
 import org.eclipse.lsp4j.FoldingRange
+import scala.meta.inputs.Position
+import scala.meta.internal.metals.FoldingRangeKind.Factory
 import scala.meta.internal.metals.FoldingRanges.foldingThreshold
 
-final class FoldingRanges {
+final class FoldingRanges(mode: FoldingMode) {
   private val allRanges = new util.ArrayList[FoldingRange]()
 
   def get: util.List[FoldingRange] = Collections.unmodifiableList(allRanges)
+
+  def add(factory: Factory, pos: Position): Unit = {
+    val range = factory.create(
+      pos.startLine,
+      pos.startColumn,
+      pos.endLine,
+      pos.endColumn
+    )(mode)
+
+    add(range)
+  }
 
   def add(range: FoldingRange): Unit = {
     if (isNotCollapsed(range)) {
