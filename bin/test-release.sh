0bee64b1441de26d76372d0ed8782f62c110fbc8
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-04-26 23:48:11 +0200

    Update test release script to include full cross-build

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -3,7 +3,18 @@ set -eux
 
 version=$1
 
-coursier fetch org.scalameta:metals_2.12:$version -r sonatype:public
+coursier fetch \
+  org.scalameta:metals_2.12:$version \
+  org.scalameta:mtags_2.12.8:$version \
+  org.scalameta:mtags_2.12.7:$version \
+  org.scalameta:mtags_2.12.6:$version \
+  org.scalameta:mtags_2.12.5:$version \
+  org.scalameta:mtags_2.12.4:$version \
+  org.scalameta:mtags_2.11.12:$version \
+  org.scalameta:mtags_2.11.11:$version \
+  org.scalameta:mtags_2.11.10:$version \
+  org.scalameta:mtags_2.11.9:$version \
+  -r sonatype:public
 coursier fetch \
     "org.scalameta:sbt-metals;sbtVersion=1.0;scalaVersion=2.12:$version" \
     "org.scalameta:sbt-metals;sbtVersion=0.13;scalaVersion=2.10:$version" \
