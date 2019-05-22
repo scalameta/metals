olafurpg/language-server

run code using debug adapter

metals/src/main/scala/scala/meta/internal/debug/JvmDebugServer.scala
@@ -1,0 +1,56 @@
+package scala.meta.internal.debug
+import java.net.ServerSocket
+import java.net.Socket
+import java.util.concurrent.ExecutorService
+import org.eclipse.lsp4j.debug.services.IDebugProtocolClient
+import org.eclipse.lsp4j.jsonrpc.Launcher
+import org.eclipse.lsp4j.jsonrpc.debug.DebugLauncher
+import scala.concurrent.ExecutionContextExecutorService
+import scala.concurrent.Future
+
+object JvmDebugServer {
+  def launch(
+      adapter: DebugAdapter
+  )(implicit ec: ExecutionContextExecutorService): Int = {
+    val server = createSocket()
+    startListening(server, adapter)
+    server.getLocalPort
+  }
+
+  private def createSocket(): ServerSocket = {
+    val serverSocket = new ServerSocket(0)
+    Runtime.getRuntime.addShutdownHook(new Thread(() => {
+      serverSocket.close()
+    }))
+    serverSocket
+  }
+
+  private def startListening(server: ServerSocket, adapter: DebugAdapter)(
+      implicit ec: ExecutionContextExecutorService
+  ): Unit = {
+    for {
+      client <- Future { server.accept() }
+    } launch(client, adapter)
+  }
+
+  private def launch(clientSocket: Socket, adapter: DebugAdapter)(
+      implicit ec: ExecutionContextExecutorService
+  ): Future[Unit] = {
+    val launcher = createLauncher(adapter, clientSocket)
+    adapter.setClient(launcher.getRemoteProxy)
+
+    Future { launcher.startListening().get() }.map(_ => ())
+  }
+
+  private def createLauncher(
+      adapter: AnyRef,
+      clientSocket: Socket
+  )(implicit executorService: ExecutorService): Launcher[IDebugProtocolClient] =
+    new DebugLauncher.Builder[IDebugProtocolClient]()
+      .setLocalService(adapter)
+      .setInput(clientSocket.getInputStream)
+      .setOutput(clientSocket.getOutputStream)
+      .setRemoteInterface(classOf[IDebugProtocolClient])
+      .setExecutorService(executorService)
+      .create()
+}

metals/src/main/scala/scala/meta/internal/metals/MetalsEnrichments.scala
@@ -77,7 +77,8 @@ object MetalsEnrichments
   }
 
   implicit class XtensionCompileResult(result: b.CompileResult) {
-    def isSuccessful: Boolean = result.getStatusCode == b.StatusCode.OK
+    def succeeded: Boolean = result.getStatusCode == b.StatusCode.OK
+    def failed: Boolean = result.getStatusCode == b.StatusCode.ERROR
   }
 
   implicit class XtensionEditDistance(result: Either[EmptyResult, m.Position]) {
@@ -510,5 +511,10 @@ object MetalsEnrichments
 
     def findFirstTrailing(predicate: Token => Boolean): Option[Token] =
       trailingTokens.find(predicate)
+  }
+
+  implicit class XtensionURI(uri: URI) {
+    def toAbsolutePath: AbsolutePath =
+      AbsolutePath(Paths.get(uri))
   }
 }

scalameta/metals

metals/src/main/scala/scala/meta/internal/eval/JvmDebugServer.scala
@@ -1,0 +1,58 @@
+package scala.meta.internal.eval
+import java.net.ServerSocket
+import java.net.Socket
+import java.util.concurrent.ExecutorService
+import org.eclipse.lsp4j.debug.services.IDebugProtocolClient
+import org.eclipse.lsp4j.jsonrpc.Launcher
+import org.eclipse.lsp4j.jsonrpc.debug.DebugLauncher
+import scala.concurrent.ExecutionContextExecutorService
+import scala.concurrent.Future
+import scala.meta.internal.metals.GlobalTrace
+
+object JvmDebugServer {
+  def launch(
+      adapter: DebugAdapter
+  )(implicit ec: ExecutionContextExecutorService): Int = {
+    val server = createSocket()
+    startListening(server, adapter)
+    server.getLocalPort
+  }
+
+  private def createSocket(): ServerSocket = {
+    val serverSocket = new ServerSocket(0)
+    Runtime.getRuntime.addShutdownHook(new Thread(() => {
+      serverSocket.close()
+    }))
+    serverSocket
+  }
+
+  private def startListening(server: ServerSocket, adapter: DebugAdapter)(
+      implicit ec: ExecutionContextExecutorService
+  ): Unit = {
+    for {
+      client <- Future { server.accept() }
+    } launch(client, adapter)
+  }
+
+  private def launch(clientSocket: Socket, adapter: DebugAdapter)(
+      implicit ec: ExecutionContextExecutorService
+  ): Future[Unit] = {
+    val launcher = createLauncher(adapter, clientSocket)
+    adapter.setClient(launcher.getRemoteProxy)
+
+    Future { launcher.startListening().get() }.map(_ => ())
+  }
+
+  private def createLauncher(
+      adapter: AnyRef,
+      clientSocket: Socket
+  )(implicit executorService: ExecutorService): Launcher[IDebugProtocolClient] =
+    new DebugLauncher.Builder[IDebugProtocolClient]()
+      .traceMessages(GlobalTrace.setupTracePrinter("dap"))
+      .setLocalService(adapter)
+      .setInput(clientSocket.getInputStream)
+      .setOutput(clientSocket.getOutputStream)
+      .setRemoteInterface(classOf[IDebugProtocolClient])
+      .setExecutorService(executorService)
+      .create()
+}

metals/src/main/scala/scala/meta/internal/metals/MetalsEnrichments.scala
@@ -77,7 +77,8 @@ object MetalsEnrichments
   }
 
   implicit class XtensionCompileResult(result: b.CompileResult) {
-    def isSuccessful: Boolean = result.getStatusCode == b.StatusCode.OK
+    def isOK: Boolean = result.getStatusCode == b.StatusCode.OK
+    def isError: Boolean = result.getStatusCode == b.StatusCode.ERROR
   }
 
   implicit class XtensionEditDistance(result: Either[EmptyResult, m.Position]) {
@@ -510,5 +511,10 @@ object MetalsEnrichments
 
     def findFirstTrailing(predicate: Token => Boolean): Option[Token] =
       trailingTokens.find(predicate)
+  }
+
+  implicit class XtensionURI(uri: URI) {
+    def toAbsolutePath: AbsolutePath =
+      AbsolutePath(Paths.get(uri))
   }
 }