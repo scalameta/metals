7ca8cfbfa5aa357ab8e7f4ac892b3a1bdb4ce4a8
Wojciech Urbanski <wojciech.urbanski@idemia.com>
2019-06-16 23:26:30 +0200

    Check only sbt files at workspace level

--- metals/src/main/scala/scala/meta/internal/builds/SbtDigest.scala
+++ metals/src/main/scala/scala/meta/internal/builds/SbtDigest.scala
@@ -1,17 +1,46 @@
 package scala.meta.internal.builds
 
-import scala.meta.io.AbsolutePath
+import java.nio.file.{Files, Path}
 import java.security.MessageDigest
 
+import scala.collection.JavaConverters._
+import scala.meta.internal.builds.Digest.digestScala
+import scala.meta.internal.io.PathIO
+import scala.meta.io.AbsolutePath
+
 object SbtDigest extends Digestable {
+  val sbtExtension = "sbt"
+
   override protected def digestWorkspace(
       workspace: AbsolutePath,
       digest: MessageDigest
   ): Boolean = {
     val project = workspace.resolve("project")
-    Digest.digestDirectory(workspace, digest) &&
+    digestSbtFiles(workspace, digest) &&
     Digest.digestFileBytes(project.resolve("build.properties"), digest) &&
     Digest.digestDirectory(project, digest) &&
     Digest.digestDirectory(project.resolve("project"), digest)
   }
+
+  def digestSbtFiles(
+      path: AbsolutePath,
+      digest: MessageDigest
+  ): Boolean = {
+    if (!path.isDirectory) {
+      true
+    } else {
+      Files.list(path.toNIO).iterator().asScala.forall(digestSbtFile(digest))
+    }
+  }
+
+  private def digestSbtFile(digest: MessageDigest)(filePath: Path) = {
+    val path = AbsolutePath(filePath)
+    def ext = PathIO.extension(path.toNIO)
+    if (path.isFile && ext == sbtExtension) {
+      digestScala(path, digest)
+    } else {
+      true
+    }
+  }
+
 }
