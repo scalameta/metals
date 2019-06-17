1f01efe6725e02016ec20094f0a3d63551854c0d
Gabriele Petronella <gabriele@buildo.io>
2019-06-17 22:11:30 +0200

    Fix CompletionKeywordSuite remaining tests
    In some instances, keywords completions fallback to parsing the
    diagnostics reported by the parser to infer the cursor position.
    
    In 2.13 there have been some changes to the concrete implementations of
    the Reporter class. Namely, the concrete class in 2.13 is
    Reporter.AdaptedReporter, which delegates to a StoreReporter (whereas
    in 2.12 we get a StoreReporter directly)
    
    The storeReporter method provides a cross-compatible API for accessing
    the StoreReporter instance

--- mtags/src/main/scala-2.11/scala/meta/internal/pc/Compat.scala
+++ mtags/src/main/scala-2.11/scala/meta/internal/pc/Compat.scala
@@ -1,9 +1,17 @@
 package scala.meta.internal.pc
 
+import scala.tools.nsc.reporters.Reporter
+import scala.tools.nsc.reporters.StoreReporter
+
 trait Compat { this: MetalsGlobal =>
   def metalsFunctionArgTypes(tpe: Type): List[Type] = {
     val dealiased = tpe.dealiasWiden
     if (definitions.isFunctionTypeDirect(dealiased)) dealiased.typeArgs.init
     else Nil
+  }
+
+  def storeReporter(r: Reporter): Option[StoreReporter] = r match {
+    case s: StoreReporter => Some(s)
+    case _ => None
   }
 }
