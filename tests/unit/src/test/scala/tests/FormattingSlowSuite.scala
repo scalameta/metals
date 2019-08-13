1f7379a8db70cf8568303bf110b4a899d587a55b
tanishiking24 <rikiriki1238@gmail.com>
2019-08-13 10:38:24 +0900

    Update scalafmt to 2.0.1

--- tests/unit/src/test/scala/tests/FormattingSlowSuite.scala
+++ tests/unit/src/test/scala/tests/FormattingSlowSuite.scala
@@ -316,7 +316,7 @@ object FormattingSlowSuite extends BaseSlowSuite("formatting") {
         )
         assertNoDiff(
           client.workspaceDiagnostics,
-          """|.scalafmt.conf:1:1: error: missing setting 'version'. To fix this problem, add the following line to .scalafmt.conf: 'version=2.0.0'.
+          """|.scalafmt.conf:1:1: error: missing setting 'version'. To fix this problem, add the following line to .scalafmt.conf: 'version=2.0.1'.
              |maxColumn=40
              |^^^^^^^^^^^^
              |""".stripMargin
