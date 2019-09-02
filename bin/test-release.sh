75f7b60954f098e6bc5b18dc9dfa124e4aef9b2d
Olafur Pall Geirsson <lgeirsson@twitter.com>
2019-09-02 15:15:05 +0100

    Add 2.12.9 to test-release.sh

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -8,6 +8,7 @@ suffix=${argumentsRest:-}
 coursier fetch \
   org.scalameta:metals_2.12:$version \
   org.scalameta:mtags_2.13.0:$version \
+  org.scalameta:mtags_2.12.9:$version \
   org.scalameta:mtags_2.12.8:$version \
   org.scalameta:mtags_2.12.7:$version \
   org.scalameta:mtags_2.11.12:$version $suffix
