aa8d6d099560b892728deff54035e6b2fdae8b09
Olafur Pall Geirsson <lgeirsson@twitter.com>
2019-10-10 22:20:11 +0200

    Add link to new Slack channel in the website footer.
    The Gitter UX is quite bad to the point where I don't enjoy using it
    much anymore. I suspect GitHub will sooner or later release a community
    chat solution judging by
    https://spectrum.chat/spectrum/general/spectrum-is-joining-github~1d3eb8ee-4c99-46c0-8daf-ca35a96be6ce/
    However, until then I think it's worth a try to keep the conversation
    going on Slack under the #scalameta-metals channel in the new
    "scala-lang" organization.

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -20,9 +20,7 @@ class Footer extends React.Component {
           {this.props.config.footerIcon && (
             <a href={this.props.config.baseUrl} className="nav-home">
               <img
-                src={`${this.props.config.baseUrl}${
-                  this.props.config.footerIcon
-                }`}
+                src={`${this.props.config.baseUrl}${this.props.config.footerIcon}`}
                 alt={this.props.config.title}
                 width="66"
                 height="58"
@@ -53,6 +51,13 @@ class Footer extends React.Component {
             <h5>Social</h5>
             <a href="https://github.com/scalameta/metals" target="_blank">
               <img src="https://img.shields.io/github/stars/scalameta/metals.svg?color=%23087e8b&label=stars&logo=github&style=social" />
+            </a>
+            <a href="https://scala-lang-slack.herokuapp.com/" target="_blank">
+              <script
+                async
+                defer
+                src="https://scala-lang-slack.herokuapp.com/slackin.js"
+              ></script>
             </a>
             <a href="https://gitter.im/scalameta/metals" target="_blank">
               <img src="https://img.shields.io/gitter/room/scalameta/metals.svg?logo=gitter&style=social" />
