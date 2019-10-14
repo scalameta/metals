e077c70017d6d8885ae57a581f19941ac91dc4d4
Olafur Pall Geirsson <lgeirsson@twitter.com>
2019-10-14 09:13:12 +0100

    Use AdoptOpenJDK on Azure

---
+++ bin/azure.sh
@@ -0,0 +1,9 @@
+set -eux
+curl -sLO https://github.com/shyiko/jabba/raw/master/install.sh
+chmod +x install.sh
+./install.sh
+source ~/.jabba/jabba.sh
+jabba install adopt@1.8.0-222
+jabba use adopt@1.8.0-222
+java -version
+./sbt unit/test
