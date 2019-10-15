52ebb6c5bacfa5e11a0965ce00af43e2b598d412
tgodzik <tomek.godzik@gmail.com>
2019-10-15 21:41:11 +0200

    Install Bloop while running on Azure and Github Actions

--- bin/azure.sh
+++ bin/azure.sh
@@ -6,4 +6,12 @@ source ~/.jabba/jabba.sh
 jabba install adopt@1.8.0-222
 jabba use adopt@1.8.0-222
 java -version
+
+function bloop_version {
+  cat build.sbt | grep "val bloop" | sed 's|[^0-9.]||g'
+}
+
+wget -O bin/coursier https://git.io/coursier-cli && chmod +x bin/coursier \
+&& bin/coursier launch ch.epfl.scala:bloopgun-core_2.12:$(bloop_version) -- about
+
 ./sbt unit/test
