8652ff13fe6d8ea2ab49f13eabedc57bfbc61e64
tgodzik <tomek.godzik@gmail.com>
2019-10-24 19:02:58 +0200

    Move more tests into github actions

--- bin/test.sh
+++ bin/test.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-bloopDir="bloop-server"
-
 function bloop_version {
   grep "val bloop" build.sbt | sed 's|[^0-9.]||g'
 }
 
-[ -d "$bloopDir" ] && rm -rf "$bloopDir"
-mkdir "$bloopDir"
-
-wget -O $bloopDir/install.py https://github.com/scalacenter/bloop/releases/download/v$(bloop_version)/install.py \
-&& python $bloopDir/install.py --dest $bloopDir
-
-$bloopDir/bloop ng-stop
-$bloopDir/bloop server &>/dev/null &
+coursier launch ch.epfl.scala:bloopgun-core_2.12:$(bloop_version) -- about
 
 sbt "$1"
 # sbt must be the last command - its exit code signals if tests passed or not
