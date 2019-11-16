054d3a74319c6f6b07a1e0f40ffcdc7e54ea9f12
Olafur Pall Geirsson <lgeirsson@twitter.com>
2019-11-15 22:48:56 -0800

    Add support for Scala 2.11/2.12 in worksheets
    Previously, worksheets only worked for Scala 2.12.10. Now, worksheets
    support all Scala versions where completions are supported.
    
    Another nice improvements in this change is that we no longer poke into
    internal mdoc APIs. The worksheet evaluation logic has been added to
    public mdoc APIs so that we have a clear separation of concerns. This
    means that improvements to rendering of values in worksheets need to be
    contributed to the scalameta/mdoc repository instead of
    scalameta/metals.

--- metals/src/main/scala/scala/meta/internal/metals/Embedded.scala
+++ metals/src/main/scala/scala/meta/internal/metals/Embedded.scala
@@ -14,6 +14,8 @@ import scala.meta.internal.metals.MetalsEnrichments._
 import scala.meta.internal.pc.ScalaPresentationCompiler
 import scala.meta.pc.PresentationCompiler
 import com.geirsson.coursiersmall.CoursierSmall
+import scala.meta.internal.worksheets.MdocClassLoader
+import mdoc.interfaces.Mdoc
 
 /**
  * Wrapper around software that is embedded with Metals.
@@ -32,6 +34,22 @@ final class Embedded(
     presentationCompilers.clear()
   }
 
+  private val mdocs: TrieMap[String, URLClassLoader] =
+    TrieMap.empty
+  def mdoc(info: ScalaBuildTarget): Mdoc = {
+    val classloader = mdocs.getOrElseUpdate(
+      info.getScalaBinaryVersion(),
+      statusBar.trackSlowTask("Preparing worksheets") {
+        Embedded.newMdocClassLoader(info)
+      }
+    )
+    serviceLoader(
+      classOf[Mdoc],
+      "mdoc.internal.worksheets.Mdoc",
+      classloader
+    )
+  }
+
   private val presentationCompilers: TrieMap[String, URLClassLoader] =
     TrieMap.empty
   def presentationCompiler(
@@ -44,19 +62,30 @@ final class Embedded(
         Embedded.newPresentationCompilerClassLoader(info, scalac)
       }
     )
-    val services =
-      ServiceLoader.load(classOf[PresentationCompiler], classloader).iterator()
+    serviceLoader(
+      classOf[PresentationCompiler],
+      classOf[ScalaPresentationCompiler].getName(),
+      classloader
+    )
+  }
+
+  private def serviceLoader[T](
+      cls: Class[T],
+      className: String,
+      classloader: URLClassLoader
+  ): T = {
+    val services = ServiceLoader.load(cls, classloader).iterator()
     if (services.hasNext) services.next()
     else {
-      // NOTE(olafur): ServiceLoader doesn't find the presentation compiler service
-      // on Appveyor for some reason, I'm unable to reproduce on my computer. Here below
-      // we fallback to manual classloading.
-      val cls =
-        classloader.loadClass(classOf[ScalaPresentationCompiler].getName)
+      // NOTE(olafur): ServiceLoader doesn't find the service on Appveyor for
+      // some reason, I'm unable to reproduce on my computer. Here below we
+      // fallback to manual classloading.
+      val cls = classloader.loadClass(className)
       val ctor = cls.getDeclaredConstructor()
       ctor.setAccessible(true)
-      ctor.newInstance().asInstanceOf[PresentationCompiler]
+      ctor.newInstance().asInstanceOf[T]
     }
+
   }
 }
 
@@ -93,6 +122,19 @@ object Embedded {
           )
         )
       )
+
+  def newMdocClassLoader(info: ScalaBuildTarget): URLClassLoader = {
+    val mdoc = new Dependency(
+      "org.scalameta",
+      s"mdoc_${info.getScalaBinaryVersion()}",
+      BuildInfo.mdocVersion
+    )
+    val settings = downloadSettings(mdoc, info.getScalaVersion())
+    val jars = CoursierSmall.fetch(settings)
+    val parent =
+      new MdocClassLoader(this.getClass.getClassLoader)
+    new URLClassLoader(jars.map(_.toUri().toURL()).toArray, parent)
+  }
 
   def newPresentationCompilerClassLoader(
       info: ScalaBuildTarget,
--- metals/src/main/scala/scala/meta/internal/metals/ScalaTarget.scala
+++ metals/src/main/scala/scala/meta/internal/metals/ScalaTarget.scala
@@ -4,18 +4,16 @@ import ch.epfl.scala.bsp4j.BuildTarget
 import ch.epfl.scala.bsp4j.ScalacOptionsItem
 import scala.meta.internal.metals.MetalsEnrichments._
 import scala.meta.io.AbsolutePath
+import java.{util => ju}
+import java.nio.file.Path
 
 case class ScalaTarget(info: BuildTarget, scalac: ScalacOptionsItem) {
   def isSemanticdbEnabled: Boolean = scalac.isSemanticdbEnabled
 
   def isScalaTarget: Boolean = info.getLanguageIds().contains("scala")
 
-  def fullClasspath: List[AbsolutePath] = {
-    scalac
-      .getClasspath()
-      .asScala
-      .toList
-      .map(_.toAbsolutePath)
+  def fullClasspath: ju.List[Path] = {
+    scalac.getClasspath().map(_.toAbsolutePath.toNIO)
   }
   def jarClasspath: List[AbsolutePath] = {
     scalac
