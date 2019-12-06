e11ef3ba50e2a8bdea61dd6b54eeb9c7e63c0ec2
Olafur Pall Geirsson <lgeirsson@twitter.com>
2019-12-06 10:24:02 +0000

    Upgrade Metals recommended Scalafmt version to v2.3.1

--- metals/src/main/scala/scala/meta/internal/metals/EmptyScalafmtReporter.scala
+++ metals/src/main/scala/scala/meta/internal/metals/EmptyScalafmtReporter.scala
@@ -4,6 +4,7 @@ import org.scalafmt.interfaces.ScalafmtReporter
 import java.io.PrintWriter
 import java.io.OutputStream
 import java.nio.file.Path
+import java.io.OutputStreamWriter
 
 /**
  * A Scalafmt reporter that ignores all messages
@@ -13,6 +14,10 @@ object EmptyScalafmtReporter extends ScalafmtReporter {
   def error(file: Path, e: Throwable): Unit = ()
   def excluded(file: Path): Unit = ()
   def parsedConfig(config: Path, scalafmtVersion: String): Unit = ()
+  def downloadOutputStreamWriter(): OutputStreamWriter =
+    new OutputStreamWriter(new OutputStream() {
+      def write(b: Int): Unit = ()
+    })
   def downloadWriter(): PrintWriter =
     new PrintWriter(new OutputStream() {
       def write(b: Int): Unit = ()
--- tests/unit/src/test/scala/tests/FormattingLspSuite.scala
+++ tests/unit/src/test/scala/tests/FormattingLspSuite.scala
@@ -316,10 +316,10 @@ object FormattingLspSuite extends BaseLspSuite("formatting") {
         )
         assertNoDiff(
           client.workspaceDiagnostics,
-          """|.scalafmt.conf:1:1: error: missing setting 'version'. To fix this problem, add the following line to .scalafmt.conf: 'version=2.0.1'.
-             |maxColumn=40
-             |^^^^^^^^^^^^
-             |""".stripMargin
+          s"""|.scalafmt.conf:1:1: error: missing setting 'version'. To fix this problem, add the following line to .scalafmt.conf: 'version=${V.scalafmtVersion}'.
+              |maxColumn=40
+              |^^^^^^^^^^^^
+              |""".stripMargin
         )
       }
     } yield ()
