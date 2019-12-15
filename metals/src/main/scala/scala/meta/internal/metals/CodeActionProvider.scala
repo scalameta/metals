8ec0ec6d082197d894e6a242c589889e11dbddc3
Chris Birchall <chris.birchall@gmail.com>
2019-12-15 16:40:12 +0000

    Only calculate the kinds of action requested by the client

--- metals/src/main/scala/scala/meta/internal/metals/CodeActionProvider.scala
+++ metals/src/main/scala/scala/meta/internal/metals/CodeActionProvider.scala
@@ -5,6 +5,7 @@ import scala.meta.pc.CancelToken
 import scala.meta.internal.mtags.Semanticdbs
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
+import scala.collection.JavaConverters._
 
 final class CodeActionProvider(
     compilers: Compilers,
@@ -19,26 +20,40 @@ final class CodeActionProvider(
       token: CancelToken
   )(implicit ec: ExecutionContext): Future[Seq[l.CodeAction]] = {
 
-    for {
-      quickFixes <- Future
-        .sequence(
-          List(QuickFix.ImportMissingSymbol)
-            .map(_.contribute(params, compilers, token)) ++
-            List(Refactoring.UseNamedArguments)
-              .map(
-                _.contribute(
-                  params,
-                  trees,
-                  buffers,
-                  semanticdbs,
-                  symbolSearch,
-                  token
-                )
-              )
-        )
-        .map(_.flatten)
-    } yield quickFixes
+    def quickfixes =
+      List(QuickFix.ImportMissingSymbol)
+        .map(_.contribute(params, compilers, token))
 
+    def refactorings =
+      List(Refactoring.UseNamedArguments)
+        .map(
+          _.contribute(
+            params,
+            trees,
+            buffers,
+            semanticdbs,
+            symbolSearch,
+            token
+          )
+        )
+
+    // when we have more refactorings, we should separate them according to the LSP "code action kind",
+    // e.g. refactoring inline, refactoring extraction, refactoring rewrite
+
+    val requestedKinds = Option(params.getContext.getOnly) match {
+      case Some(only) => only.asScala.toList
+      case None =>
+        // if client did not specify, return all actions by default
+        List(l.CodeActionKind.QuickFix, l.CodeActionKind.Refactor)
+    }
+
+    val actions = requestedKinds flatMap {
+      case l.CodeActionKind.QuickFix => quickfixes
+      case l.CodeActionKind.Refactor => refactorings
+      case _ => Nil
+    }
+
+    Future.sequence(actions).map(_.flatten)
   }
 
 }
