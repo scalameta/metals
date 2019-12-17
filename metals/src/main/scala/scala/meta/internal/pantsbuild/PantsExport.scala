a62a7286ea12d0305114c31eac096b77f76a4d12
Olafur Pall Geirsson <lgeirsson@twitter.com>
2019-12-17 14:54:11 +0000

    Handle empty `scala_platform` key in export output

--- metals/src/main/scala/scala/meta/internal/pantsbuild/PantsExport.scala
+++ metals/src/main/scala/scala/meta/internal/pantsbuild/PantsExport.scala
@@ -69,17 +69,9 @@ object PantsExport {
         })
     }.toMap
 
-    val scalaCompilerClasspath = output
-      .obj(PantsKeys.scalaPlatform)
-      .obj(PantsKeys.compilerClasspath)
-      .arr
-      .map(path => Paths.get(path.str))
-    val scalaPlatform = PantsScalaPlatform(
-      output.obj(PantsKeys.scalaPlatform).obj(PantsKeys.scalaVersion).str,
-      scalaCompilerClasspath
-    )
-
     val cycles = Cycles.findConnectedComponents(output)
+
+    val scalaPlatform = PantsScalaPlatform.fromJson(output)
 
     PantsExport(
       targets = targets,
@@ -88,4 +80,5 @@ object PantsExport {
       cycles = cycles
     )
   }
+
 }
--- metals/src/main/scala/scala/meta/internal/pantsbuild/PantsScalaPlatform.scala
+++ metals/src/main/scala/scala/meta/internal/pantsbuild/PantsScalaPlatform.scala
@@ -1,8 +1,42 @@
 package scala.meta.internal.pantsbuild
 
+import scala.collection.JavaConverters._
 import java.nio.file.Path
+import ujson.Value
+import java.nio.file.Paths
+import scala.meta.internal.metals.BuildInfo
+import coursierapi.Dependency
+import coursierapi.Fetch
 
 case class PantsScalaPlatform(
     scalaBinaryVersion: String,
     compilerClasspath: Seq[Path]
 )
+
+object PantsScalaPlatform {
+  def fromJson(output: Value): PantsScalaPlatform = {
+    val (scalaVersion, compilerClasspath) =
+      output.obj.get(PantsKeys.scalaPlatform) match {
+        case Some(scalaPlatform) =>
+          scalaPlatform.obj(PantsKeys.scalaVersion).str ->
+            scalaPlatform
+              .obj(PantsKeys.compilerClasspath)
+              .arr
+              .map(path => Paths.get(path.str))
+        case None =>
+          "2.12" ->
+            fetchScalaCompilerClasspath(BuildInfo.scala212)
+      }
+    PantsScalaPlatform(scalaVersion, compilerClasspath)
+  }
+  private def fetchScalaCompilerClasspath(scalaVersion: String): Seq[Path] =
+    Fetch
+      .create()
+      .withDependencies(
+        Dependency.of("org.scala-lang", "scala-compiler", scalaVersion)
+      )
+      .fetch()
+      .asScala
+      .map(_.toPath())
+      .toList
+}
