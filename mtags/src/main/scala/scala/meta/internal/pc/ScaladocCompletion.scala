1582cd8bd190244153e0d6ca2d2192d420ee1c41
tanishiking24 <rikiriki1238@gmail.com>
2020-01-11 15:40:18 +0900

    Change FIXME comment into NOTE and add some insights on comment.
    https://github.com/scalameta/metals/pull/1250#discussion_r365503290

--- mtags/src/main/scala/scala/meta/internal/pc/ScaladocCompletion.scala
+++ mtags/src/main/scala/scala/meta/internal/pc/ScaladocCompletion.scala
@@ -117,18 +117,25 @@ trait ScaladocCompletion { this: MetalsGlobal =>
       memberDef match {
         case defdef: DefDef =>
           defdef.symbol.paramss.flatten
-            .filter(!_.isSynthetic)
+            .filterNot(_.isSynthetic)
             .map(_.name.toString())
         case clazz: ClassDef =>
           // If the associated def is a class definition,
           // retrieve the constructor from the class, and caluculate the lines
           // from the constructor definition instead.
           clazz.symbol.primaryConstructor.paramss.flatten
-            .filter(sym =>
-              // FIXME: All synthetic params should be filtered out by checking SYNTHETIC flag.
-              // evidence params for classs constructor look like they don't have SYNTHETIC flag.
-              !sym.isSynthetic &&
-                !sym.name.startsWith(nme.EVIDENCE_PARAM_PREFIX)
+            .filterNot(sym =>
+              // NOTE: We check the symbol name not starts with `EVIDENCE_PARAM_PREFIX`
+              // to avoid line that `* @param evidence$1` in case the class constructor requires type class instance, like:
+              // `case class Test[T: Ordering](t: T) = {}`
+              // Those parameters are added by TreeBuilder here
+              // https://github.com/scala/scala/blob/ba9701059216c629410f4f23a2175d20ad62484b/src/compiler/scala/tools/nsc/ast/parser/TreeBuilder.scala#L135
+              //
+              // Though the `SYNTHETIC` modifiers on DefDef survive during type checking,
+              // the modifiers on class constructor attached in TreeBuilder seem not to remain
+              // after type-checking. That's why check if the param name starts with `EVIDENCE_PARAM_PREFIX` for a class constructor.
+              sym.isSynthetic ||
+                sym.name.startsWith(nme.EVIDENCE_PARAM_PREFIX)
             )
             .map(_.name.toString())
         case _ => Nil
