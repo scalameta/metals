c1a842054881c1a96ebf6fc2a6f92aa9b453e6a0
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-03-27 16:05:58 +0100

    Make ImportedBuild a monoid

--- metals/src/main/scala/scala/meta/internal/metals/ImportedBuild.scala
+++ metals/src/main/scala/scala/meta/internal/metals/ImportedBuild.scala
@@ -1,6 +1,10 @@
 package scala.meta.internal.metals
 
+import java.{util => ju}
+
 import ch.epfl.scala.bsp4j._
+
+import scala.collection.JavaConverters._
 
 /**
  * Metadata that we import from the build tool via BSP.
@@ -10,4 +14,35 @@ case class ImportedBuild(
     scalacOptions: ScalacOptionsResult,
     sources: SourcesResult,
     dependencySources: DependencySourcesResult
-)
+) {
+  def ++(other: ImportedBuild): ImportedBuild = {
+    val updatedBuildTargets = new WorkspaceBuildTargetsResult(
+      (workspaceBuildTargets.getTargets.asScala ++ other.workspaceBuildTargets.getTargets.asScala).asJava
+    )
+    val updatedScalacOptions = new ScalacOptionsResult(
+      (scalacOptions.getItems.asScala ++ other.scalacOptions.getItems.asScala).asJava
+    )
+    val updatedSources = new SourcesResult(
+      (sources.getItems.asScala ++ other.sources.getItems.asScala).asJava
+    )
+    val updatedDependencySources = new DependencySourcesResult(
+      (dependencySources.getItems.asScala ++ other.dependencySources.getItems.asScala).asJava
+    )
+    ImportedBuild(
+      updatedBuildTargets,
+      updatedScalacOptions,
+      updatedSources,
+      updatedDependencySources
+    )
+  }
+}
+
+object ImportedBuild {
+  def empty: ImportedBuild =
+    ImportedBuild(
+      new WorkspaceBuildTargetsResult(ju.Collections.emptyList()),
+      new ScalacOptionsResult(ju.Collections.emptyList()),
+      new SourcesResult(ju.Collections.emptyList()),
+      new DependencySourcesResult(ju.Collections.emptyList())
+    )
+}
