f2f7a24921d9453a8fa8126b06417f77ef07284f
Gabriele Petronella <gabriele@buildo.io>
2020-04-04 12:55:50 +0200

    Add VersionedDocsModifier

---
+++ metals-docs/src/main/scala/docs/VersionedDocsModifier.scala
@@ -0,0 +1,25 @@
+package docs
+
+import mdoc.Reporter
+import mdoc.StringModifier
+import scala.meta.inputs.Input
+import scala.meta.internal.metals.BuildInfo
+import scala.meta.internal.semver.SemVer.Version
+
+class VersionedDocsModifier extends StringModifier {
+  val name = "since"
+
+  override def process(
+      version: String,
+      code: Input,
+      reporter: Reporter
+  ): String = {
+    val unreleased =
+      Version.fromString(version) >= Version.fromString(BuildInfo.metalsVersion)
+
+    if (unreleased) {
+      s"> 🚧 This documentation section describes a feature that is currently unreleased. The expected release version is $version\n"
+    } else ""
+
+  }
+}
