9c11dfb2b9538ab94570401f1c827a98006e1162
kpbochenek <kpbochenek@gmail.com>
2020-05-18 18:47:42 +0200

    Fix filtering to not consider access modifiers

--- mtags/src/main/scala-2/scala/meta/internal/pc/completions/OverrideCompletions.scala
+++ mtags/src/main/scala-2/scala/meta/internal/pc/completions/OverrideCompletions.scala
@@ -209,11 +209,16 @@ trait OverrideCompletions { this: MetalsGlobal =>
         // https://github.com/scalameta/metals/issues/565#issuecomment-472761240
         else ""
 
+      val lzy: String =
+        if (sym.isLazy) "lazy "
+        else ""
+
       val _modifs = sym.flagString
         .replace("package ", "")
         .replace("object ", "")
         .replace("trait ", "")
         .replace("class ", "")
+
       val modifs =
         if (_modifs.isEmpty) ""
         else _modifs + " "
@@ -237,7 +242,9 @@ trait OverrideCompletions { this: MetalsGlobal =>
 
       val name: String = Identifier(sym.name)
 
-      val filterText: String = s"${overrideKeyword}${modifs}${keyword}${name}"
+      val filterText: String = s"${overrideKeyword}${lzy}${keyword}${name}"
+      val insertText: String =
+        s"${overrideKeyword}${modifs}${keyword}${name}$signature"
 
       // if we had no val or def then filter will be empty
       def toMember = new OverrideDefMember(
@@ -261,9 +268,9 @@ trait OverrideCompletions { this: MetalsGlobal =>
       private def edit = new l.TextEdit(
         range,
         if (clientSupportsSnippets && shouldMoveCursor) {
-          s"$filterText$signature = $${0:???}"
+          s"$insertText = $${0:???}"
         } else {
-          s"$filterText$signature = ???"
+          s"$insertText = ???"
         }
       )
     }
