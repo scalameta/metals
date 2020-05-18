48c155158d1652e83ee4bf1cf2c98c3b1823b32f
tgodzik <tgodzik@virtuslab.com>
2020-05-18 18:47:47 +0200

    Support java files inside java modules for go to definition

--- tests/unit/src/test/scala/tests/JdkSourcesSuite.scala
+++ tests/unit/src/test/scala/tests/JdkSourcesSuite.scala
@@ -1,9 +1,26 @@
 package tests
 
 import scala.meta.internal.metals.JdkSources
+import scala.meta.internal.mtags.OnDemandSymbolIndex
+import scala.meta.internal.mtags.Symbol
 
 class JdkSourcesSuite extends BaseSuite {
   test("src.zip") {
     JdkSources.getOrThrow()
+  }
+
+  test("index-src.zip") {
+    val jdk = JdkSources.getOrThrow()
+    val symbolIndex = OnDemandSymbolIndex()
+
+    symbolIndex.addSourceJar(jdk)
+
+    val pathsDef = symbolIndex.definition(Symbol("java/nio/file/Paths#"))
+    assert(pathsDef.isDefined, "Cannot find java/nio/file/Paths#")
+
+    val swingBoxDef =
+      symbolIndex.definition(Symbol("javax/swing/Box."))
+    assert(swingBoxDef.isDefined, "Cannot find javax/swing/Box.")
+
   }
 }
