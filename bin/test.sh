d6939e77b5a85a2e8ddc4fa76efad23601fc7b2b
tgodzik <tgodzik@virtuslab.com>
2020-08-12 12:59:25 +0200

    Add debug to check if Bloop is properly using hte memory

--- bin/test.sh
+++ bin/test.sh
@@ -12,5 +12,15 @@ echo "-Xmx1G"  >> ~/.bloop/.jvmopts
 curl -Lo coursier https://git.io/coursier-cli && chmod +x coursier
 ./coursier launch ch.epfl.scala:bloopgun-core_2.12:$(bloop_version) -- about
 
+rm .jvmopts
+touch .jvmopts
+echo "-Xss4m" >> .jvmopts 
+echo "-Xmx1G"  >> .jvmopts 
+echo "-XX:ReservedCodeCacheSize=1024m" >> .jvmopts
+echo "-XX:+TieredCompilation" >> .jvmopts
+echo "-XX:+CMSClassUnloadingEnabled" >> .jvmopts
+echo "-Dfile.encoding=UTF-8" >> .jvmopts
+
 sbt "$1"
+
 # sbt must be the last command - its exit code signals if tests passed or not
