9e2a418b210518567fe51483167658466c9e256c
Krzysiek Bochenek <kpbochenek@gmail.com>
2020-09-24 18:08:20 +0200

    Add documentation for new parameter in GotoLocation

--- metals/src/main/scala/scala/meta/internal/metals/ClientCommands.scala
+++ metals/src/main/scala/scala/meta/internal/metals/ClientCommands.scala
@@ -109,16 +109,21 @@ object ClientCommands {
     "metals-goto-location",
     "Goto location",
     "Move the cursor focus to the provided location",
-    """|A LSP `Location` object with `uri` and `range` fields.
+    """|First required parameter is LSP `Location` object with `uri` and `range` fields.
+       |Second parameter is optional and has signature `otherWindow: Boolean`. 
+       |It gives a hint to client that if possible it would be good to open location in
+       |another buffer/window.
        |Example: 
        |```json
-       |{
+       |[{
        |  "uri": "file://path/to/Definition.scala",
        |  "range": {
        |    "start": {"line": 194, "character": 0},
        |    "end":   {"line": 194, "character": 1}
        |  }
-       |}
+       |},
+       |  false
+       |]
        |```
        |""".stripMargin
   )
