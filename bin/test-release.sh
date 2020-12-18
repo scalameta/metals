3c6bf02e8b79dd048805557ca15afd837bef7bf3
Tomasz Godzik <tomek.godzik@gmail.com>
2020-12-18 20:31:20 +0100

    Add release notes for 0.9.8

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -6,9 +6,10 @@ argumentsRest=${@:2}
 suffix=${argumentsRest:-}
 
 coursier fetch \
-  org.scalameta:mtags_0.26.0:$version \
   org.scalameta:mtags_0.27.0-RC1:$version \
   org.scalameta:mtags_3.0.0-M1:$version \
+  org.scalameta:mtags_3.0.0-M2:$version \
+  org.scalameta:mtags_3.0.0-M3:$version \
   org.scalameta:metals_2.12:$version \
   org.scalameta:mtags_2.13.0:$version \
   org.scalameta:mtags_2.13.1:$version \
