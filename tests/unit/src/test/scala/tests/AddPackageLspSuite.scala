ddcb8424c1632c4a0f423bfc77a13c741f9cda88
Tomasz Godzik <tomek.godzik@gmail.com>
2021-02-13 08:03:36 +0100

    Refactor AddPackageSuite to use a check method
    Previously we would repreat the same workflow in each test in AddPackageLspSuite. I changed it to only use a check method with a name, a file and and expected contents parameters.

--- tests/unit/src/test/scala/tests/AddPackageLspSuite.scala
+++ tests/unit/src/test/scala/tests/AddPackageLspSuite.scala
@@ -4,177 +4,84 @@ import java.nio.file.Files
 
 import scala.meta.internal.metals.MetalsEnrichments._
 import scala.meta.internal.metals.RecursivelyDelete
+import munit.TestOptions
+import java.nio.file.Paths
 
 class AddPackageLspSuite extends BaseLspSuite("add-package") {
 
-  test("single-level") {
-    cleanCompileCache("a")
-    RecursivelyDelete(workspace.resolve("a"))
-    Files.createDirectories(
-      workspace.resolve("a/src/main/scala/a").toNIO
-    )
-    for {
-      _ <- server.initialize(
-        """|/metals.json
-           |{
-           |  "a": { }
-           |}
-        """.stripMargin
-      )
-      _ =
-        workspace
-          .resolve("a/src/main/scala/a/Main.scala")
-          .toFile
-          .createNewFile()
-      _ <- server.didOpen("a/src/main/scala/a/Main.scala")
-      _ = assertNoDiff(
-        workspace.resolve("a/src/main/scala/a/Main.scala").readText,
-        """
-          |package a
-        """.stripMargin
-      )
-    } yield ()
-  }
+  check("single-level")(
+    "a/src/main/scala/a/Main.scala",
+    """|
+       |package a
+       |""".stripMargin
+  )
 
-  test("package-file") {
-    cleanCompileCache("a")
-    RecursivelyDelete(workspace.resolve("a"))
-    Files.createDirectories(
-      workspace.resolve("a/src/main/scala/a").toNIO
-    )
-    for {
-      _ <- server.initialize(
-        """|/metals.json
-           |{
-           |  "a": { }
-           |}
-        """.stripMargin
-      )
-      _ =
-        workspace
-          .resolve("a/src/main/scala/a/package.scala")
-          .toFile
-          .createNewFile()
-      _ <- server.didOpen("a/src/main/scala/a/package.scala")
-      _ = assertNoDiff(
-        workspace.resolve("a/src/main/scala/a/package.scala").readText,
-        "package object a {\n  \n}\n"
-      )
-    } yield ()
-  }
+  check("package-file")(
+    "a/src/main/scala/a/package.scala",
+    """|
+       |package object a {
+       |  
+       |}
+       |
+       |""".stripMargin
+  )
 
-  test("package-file-multi") {
-    cleanCompileCache("a")
-    RecursivelyDelete(workspace.resolve("a"))
-    Files.createDirectories(
-      workspace.resolve("a/src/main/scala/a/b/c").toNIO
-    )
-    for {
-      _ <- server.initialize(
-        """|/metals.json
-           |{
-           |  "a": { }
-           |}
-        """.stripMargin
-      )
-      _ =
-        workspace
-          .resolve("a/src/main/scala/a/b/c/package.scala")
-          .toFile
-          .createNewFile()
-      _ <- server.didOpen("a/src/main/scala/a/b/c/package.scala")
-      _ = assertNoDiff(
-        workspace.resolve("a/src/main/scala/a/b/c/package.scala").readText,
-        """|package a.b
-           |
-           |package object c {
-           |  
-           |}
-           |""".stripMargin
-      )
-    } yield ()
-  }
+  check("package-file-multi")(
+    "a/src/main/scala/a/b/c/package.scala",
+    """|package a.b
+       |
+       |package object c {
+       |  
+       |}
+       |
+       |""".stripMargin
+  )
 
-  test("multilevel") {
-    cleanCompileCache("a")
-    RecursivelyDelete(workspace.resolve("a"))
-    Files.createDirectories(
-      workspace.resolve("a/src/main/scala/a/b/c").toNIO
-    )
-    for {
-      _ <- server.initialize(
-        """|/metals.json
-           |{
-           |  "a": { }
-           |}
+  check("multilevel")(
+    "a/src/main/scala/a/b/c/Main.scala",
+    """
+      |package a.b.c
         """.stripMargin
-      )
-      _ =
-        workspace
-          .resolve("a/src/main/scala/a/b/c/Main.scala")
-          .toFile
-          .createNewFile()
-      _ <- server.didOpen("a/src/main/scala/a/b/c/Main.scala")
-      _ = assertNoDiff(
-        workspace.resolve("a/src/main/scala/a/b/c/Main.scala").readText,
-        """
-          |package a.b.c
-        """.stripMargin
-      )
-    } yield ()
-  }
+  )
 
-  test("no-package") {
-    cleanCompileCache("a")
-    RecursivelyDelete(workspace.resolve("a"))
-    Files.createDirectories(
-      workspace.resolve("a/src/main/scala").toNIO
-    )
-    for {
-      _ <- server.initialize(
-        """|/metals.json
-           |{
-           |  "a": { }
-           |}
-        """.stripMargin
-      )
-      _ =
-        workspace
-          .resolve("a/src/main/scala/Main.scala")
-          .toFile
-          .createNewFile()
-      _ <- server.didOpen("a/src/main/scala/Main.scala")
-      _ = assertNoDiff(
-        workspace.resolve("a/src/main/scala/Main.scala").readText,
-        ""
-      )
-    } yield ()
-  }
+  check("no-package")(
+    "a/src/main/scala/Main.scala",
+    ""
+  )
 
-  test("java-file") {
-    cleanCompileCache("a")
-    RecursivelyDelete(workspace.resolve("a"))
-    Files.createDirectories(
-      workspace.resolve("a/src/main/java/a").toNIO
-    )
-    for {
-      _ <- server.initialize(
-        """|/metals.json
-           |{
-           |  "a": { }
-           |}
+  check("java-file")(
+    "a/src/main/scala/Main.java",
+    ""
+  )
+
+  def check(name: TestOptions)(
+      fileToCreate: String,
+      expectedContent: String
+  ) = {
+    test(name) {
+      val parent = Paths.get(fileToCreate).getParent()
+      cleanCompileCache("a")
+      RecursivelyDelete(workspace.resolve("a"))
+      Files.createDirectories(workspace.toNIO.resolve(parent))
+      for {
+        _ <- server.initialize(
+          """|/metals.json
+             |{
+             |  "a": { }
+             |}
         """.stripMargin
-      )
-      _ =
-        workspace
-          .resolve("a/src/main/java/a/Main.java")
-          .toFile
-          .createNewFile()
-      _ <- server.didOpen("a/src/main/java/a/Main.java")
-      _ = assertNoDiff(
-        workspace.resolve("a/src/main/java/a/Main.java").readText,
-        ""
-      )
-    } yield ()
+        )
+        _ =
+          workspace
+            .resolve(fileToCreate)
+            .toFile
+            .createNewFile()
+        _ <- server.didOpen(fileToCreate)
+        _ = assertNoDiff(
+          workspace.resolve(fileToCreate).readText,
+          expectedContent
+        )
+      } yield ()
+    }
   }
 }
