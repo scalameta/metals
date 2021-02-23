1102cc3d87c0f1c37fe25032bb7682348c7b6e0b
Vadim Chelyshov <qtankle@gmail.com>
2021-02-23 12:51:41 +0300

    [Scala3] FoldingRange -  add tests for Scala3

---
+++ tests/unit/src/test/resources/foldingRange-scala3/expect/Givens.scala
@@ -0,0 +1,28 @@
+trait Foo[A]:
+  def foo: String
+
+given Foo[String]>>region>> with 
+  def a: String =
+    "f" +
+      "o" +
+      "o"<<region<<
+
+given Foo[String]>>region>> with 
+  def a: String = 
+    "f" +
+      "o" +
+      "o"<<region<<
+
+given x: AnyRef>>region>> with
+  extension (y: String)
+    def a (y: Int): String =
+      "f" +
+        "o" +
+        "o"<<region<<
+
+given stringV: String =
+  >>region>>val a = "a"
+  val b = "b"
+  val c = "c"
+  val d = "d"
+  a + b + c + d<<region<<
