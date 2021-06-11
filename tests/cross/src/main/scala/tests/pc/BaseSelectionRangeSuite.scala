536ac72d9f6f9ffdc724fd380a770f826661635f
ckipp01 <ckipp@pm.me>
2021-06-11 22:34:16 +0200

    Add framework in for selectionRanges in cross tests

---
+++ tests/cross/src/main/scala/tests/pc/BaseSelectionRangeSuite.scala
@@ -0,0 +1,88 @@
+package tests.pc
+
+import java.nio.file.Paths
+import java.{util => ju}
+
+import scala.meta.inputs.Input
+import scala.meta.internal.jdk.CollectionConverters._
+import scala.meta.internal.metals.CompilerOffsetParams
+import scala.meta.internal.metals.EmptyCancelToken
+import scala.meta.internal.mtags.MtagsEnrichments._
+import scala.meta.pc.OffsetParams
+
+import munit.TestOptions
+import org.eclipse.{lsp4j => l}
+import tests.Assertions
+import tests.BasePCSuite
+
+abstract class BaseSelectionRangeSuite extends BasePCSuite {
+
+  def check(
+      name: TestOptions,
+      original: String,
+      expectedRanges: List[String],
+      compat: Map[String, String] = Map.empty
+  ): Unit = {
+    test(name) {
+      val (code, offset) = params(original)
+      val offsetParams: ju.List[OffsetParams] = List[OffsetParams](
+        CompilerOffsetParams(
+          Paths.get("SelectionRange.scala").toUri(),
+          code,
+          offset,
+          EmptyCancelToken
+        )
+      ).asJava
+
+      val selectionRanges = presentationCompiler
+        .selectionRange(offsetParams)
+        .get()
+        .asScala
+        .toList
+
+      // Note that this takes some liberty since the spec isn't the clearest on
+      // the way this is done and different LSP clients seems to impliment this
+      // differently. Mainly, we mimic what VS Code does and what other servers
+      // like jdtls do and we send in a single position. Once that positions is
+      // recieved we check the Selection range and all the parents in that range
+      // to ensure it contains the positions we'd expect it to contain. In the
+      // case of VS Code, they never make a second request, and instead they
+      // just rely on the tree to continue selecting. So we mimic that here in
+      // the tests.
+      assertSelectionRanges(
+        selectionRanges.headOption,
+        expectedRanges,
+        code
+      )
+    }
+  }
+
+  private def assertSelectionRanges(
+      range: Option[l.SelectionRange],
+      expected: List[String],
+      original: String
+  ): Unit = {
+    assert(range.nonEmpty)
+    expected.headOption.foreach { expectedRange =>
+      val withRange = applyRanges(original, range.get)
+      Assertions.assertNoDiff(withRange, expectedRange)
+      assertSelectionRanges(range.map(_.getParent()), expected.tail, original)
+    }
+  }
+
+  private def applyRanges(
+      text: String,
+      selectionRange: l.SelectionRange
+  ): String = {
+    val input = Input.String(text)
+
+    val pos = selectionRange.getRange().toMeta(input)
+    val out = new java.lang.StringBuilder()
+    out.append(text, 0, pos.start)
+    out.append(">>region>>")
+    out.append(text, pos.start, pos.end)
+    out.append("<<region<<")
+    out.append(text, pos.end, text.length)
+    out.toString
+  }
+}
---
+++ tests/cross/src/test/scala/tests/pc/SelectionRangeSuite.scala
@@ -0,0 +1,108 @@
+package tests.pc
+
+class SelectionRangeSuite extends BaseSelectionRangeSuite {
+
+  check(
+    "match".tag(IgnoreScala3),
+    """|object Main extends App {
+       |  Option("chris") match {
+       |    case Some(n@@ame) => println("Hello! " + name)
+       |    case None =>
+       |  }
+       |}""".stripMargin,
+    List(
+      """|object Main extends App {
+         |  Option("chris") match {
+         |    case Some(>>region>>name<<region<<) => println("Hello! " + name)
+         |    case None =>
+         |  }
+         |}""".stripMargin,
+      """|object Main extends App {
+         |  Option("chris") match {
+         |    case >>region>>Some(name)<<region<< => println("Hello! " + name)
+         |    case None =>
+         |  }
+         |}""".stripMargin,
+      """|object Main extends App {
+         |  Option("chris") match {
+         |    case >>region>>Some(name) => println("Hello! " + name)<<region<<
+         |    case None =>
+         |  }
+         |}""".stripMargin,
+      """|object Main extends App {
+         |  >>region>>Option("chris") match {
+         |    case Some(name) => println("Hello! " + name)
+         |    case None =>
+         |  }<<region<<
+         |}""".stripMargin,
+      """|object Main >>region>>extends App {
+         |  Option("chris") match {
+         |    case Some(name) => println("Hello! " + name)
+         |    case None =>
+         |  }
+         |}<<region<<""".stripMargin,
+      """|>>region>>object Main extends App {
+         |  Option("chris") match {
+         |    case Some(name) => println("Hello! " + name)
+         |    case None =>
+         |  }
+         |}<<region<<""".stripMargin
+    )
+  )
+
+  check(
+    "for".tag(IgnoreScala3),
+    """|object Main extends App {
+       |  val total = for {
+       |    a <- S@@ome(1)
+       |    b <- Some(2)
+       |  } yield a + b
+       |}""".stripMargin,
+    List(
+      """|object Main extends App {
+         |  val total = for {
+         |    a <- >>region>>Some<<region<<(1)
+         |    b <- Some(2)
+         |  } yield a + b
+         |}""".stripMargin,
+      """|object Main extends App {
+         |  val total = for {
+         |    a <- >>region>>Some(1)<<region<<
+         |    b <- Some(2)
+         |  } yield a + b
+         |}""".stripMargin,
+      // TODO we have a duplicate here, figure out why
+      """|object Main extends App {
+         |  val total = for {
+         |    a <- >>region>>Some(1)<<region<<
+         |    b <- Some(2)
+         |  } yield a + b
+         |}""".stripMargin,
+      """|object Main extends App {
+         |  val total = >>region>>for {
+         |    a <- Some(1)
+         |    b <- Some(2)
+         |  } yield a + b<<region<<
+         |}""".stripMargin,
+      """|object Main extends App {
+         |  >>region>>val total = for {
+         |    a <- Some(1)
+         |    b <- Some(2)
+         |  } yield a + b<<region<<
+         |}""".stripMargin,
+      """|object Main >>region>>extends App {
+         |  val total = for {
+         |    a <- Some(1)
+         |    b <- Some(2)
+         |  } yield a + b
+         |}<<region<<""".stripMargin,
+      """|>>region>>object Main extends App {
+         |  val total = for {
+         |    a <- Some(1)
+         |    b <- Some(2)
+         |  } yield a + b
+         |}<<region<<""".stripMargin
+    )
+  )
+
+}
