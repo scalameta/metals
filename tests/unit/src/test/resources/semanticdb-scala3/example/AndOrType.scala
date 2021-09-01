9472631dcad3b3bff78bf118882d02a9b0b383b3
Tomasz Godzik <tomek.godzik@gmail.com>
2021-09-01 16:50:18 +0200

    Add support for Scala 3.0.2

--- tests/unit/src/test/resources/semanticdb-scala3/example/AndOrType.scala
+++ tests/unit/src/test/resources/semanticdb-scala3/example/AndOrType.scala
@@ -3,6 +3,6 @@ package example
 trait Cancelable/*example.Cancelable#*/ 
 trait Movable/*example.Movable#*/ 
 
-/*example.AndOrType$package.*/type Y/*example.AndOrType$package.Y#*/ = (Cancelable/*example.Cancelable#*/ &/*scala.`&`#*/ Movable/*example.Movable#*/)
+type Y/*example.AndOrType$package.Y#*/ = (Cancelable/*example.Cancelable#*/ &/*scala.`&`#*/ Movable/*example.Movable#*/)
 
 type X/*example.AndOrType$package.X#*/ = String/*scala.Predef.String#*/ |/*scala.`|`#*/ Int/*scala.Int#*/
