80c42beeaf860f84081ec2bcbf83684aacffc9d2
Adrien Piquerez <adrien.piquerez@gmail.com>
2021-09-23 14:55:53 +0200

    Fix StcakFrameDapSuites
    scala-debug-adapter 2.x now shows the memory address of classes.

--- tests/slow/src/test/scala/tests/sbt/SbtStackFrameDapSuite.scala
+++ tests/slow/src/test/scala/tests/sbt/SbtStackFrameDapSuite.scala
@@ -1,9 +1,5 @@
 package tests.sbt
 
-import scala.meta.internal.metals.debug.Scope
-import scala.meta.internal.metals.debug.Variable
-import scala.meta.internal.metals.debug.Variables
-
 import tests.SbtBuildLayout
 import tests.SbtServerInitializer
 import tests.debug.BaseStackFrameDapSuite
@@ -13,55 +9,4 @@ class SbtStackFrameDapSuite
       "sbt-debug-stack-frame",
       SbtServerInitializer,
       SbtBuildLayout
-    ) {
-
-  assertStackFrame("foreach")(
-    source = """|a/src/main/scala/Main.scala
-                |object Main {
-                |  def main(args: Array[String]) = {
-                |    List(1, 2).foreach { value =>
-                |>>      println(value)
-                |    }
-                |    System.exit(0)
-                |  }
-                |}""".stripMargin,
-    expectedFrames = List(
-      Variables(
-        Scope.local(Variable("MODULE$: Main$"), Variable("value: int = 1"))
-      ),
-      Variables(
-        Scope.local(Variable("MODULE$: Main$"), Variable("value: int = 2"))
-      )
     )
-  )
-
-  assertStackFrame("for-comprehension")(
-    source = """|a/src/main/scala/Main.scala
-                |object Main {
-                |  def main(args: Array[String]): Unit = {
-                |    for {
-                |      x <- List(1)
-                |>>    z = x + 2
-                |    } println(z)
-                |    System.exit(0)
-                |  }
-                |}
-                |""".stripMargin,
-    expectedFrames = List(
-      Variables( // before calculating `z`
-        Scope.local(Variable("MODULE$: Main$"), Variable("x: int = 1"))
-      ),
-      Variables( // after calculating `z`
-        Scope.local(
-          Variable("MODULE$: Main$"),
-          Variable("x: int = 1"),
-          Variable("z: int = 3")
-        )
-      ),
-      Variables(
-        Scope.local(Variable("MODULE$: Main$"), Variable("x$1: Tuple2$mcII$sp"))
-      )
-    )
-  )
-
-}
--- tests/unit/src/test/scala/tests/debug/StackFrameDapSuite.scala
+++ tests/unit/src/test/scala/tests/debug/StackFrameDapSuite.scala
@@ -1,9 +1,5 @@
 package tests.debug
 
-import scala.meta.internal.metals.debug.Scope
-import scala.meta.internal.metals.debug.Variable
-import scala.meta.internal.metals.debug.Variables
-
 import tests.QuickBuildInitializer
 import tests.QuickBuildLayout
 
@@ -12,54 +8,4 @@ class StackFrameDapSuite
       "debug-stack-frame",
       QuickBuildInitializer,
       QuickBuildLayout
-    ) {
-
-  assertStackFrame("foreach")(
-    source = """|a/src/main/scala/Main.scala
-                |object Main {
-                |  def main(args: Array[String]) = {
-                |    List(1, 2).foreach { value =>
-                |>>      println(value)
-                |    }
-                |    System.exit(0)
-                |  }
-                |}""".stripMargin,
-    expectedFrames = List(
-      Variables(
-        Scope.local(Variable("MODULE$: Main$"), Variable("value: int = 1"))
-      ),
-      Variables(
-        Scope.local(Variable("MODULE$: Main$"), Variable("value: int = 2"))
-      )
     )
-  )
-
-  assertStackFrame("for-comprehension")(
-    source = """|a/src/main/scala/Main.scala
-                |object Main {
-                |  def main(args: Array[String]): Unit = {
-                |    for {
-                |      x <- List(1)
-                |>>    z = x + 2
-                |    } println(z)
-                |    System.exit(0)
-                |  }
-                |}
-                |""".stripMargin,
-    expectedFrames = List(
-      Variables( // before calculating `z`
-        Scope.local(Variable("MODULE$: Main$"), Variable("x: int = 1"))
-      ),
-      Variables( // after calculating `z`
-        Scope.local(
-          Variable("MODULE$: Main$"),
-          Variable("x: int = 1"),
-          Variable("z: int = 3")
-        )
-      ),
-      Variables(
-        Scope.local(Variable("MODULE$: Main$"), Variable("x$1: Tuple2$mcII$sp"))
-      )
-    )
-  )
-}
