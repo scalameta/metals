f035abdbbbfd12ec6411d5c7be5dce8a826bfb18
Vadim Chelyshov <qtankle@gmail.com>
2021-10-18 16:36:35 +0300

    Add Scala 3.1.0 support

--- tests/unit/src/test/resources/semanticdb-scala3/example/Extension.scala
+++ tests/unit/src/test/resources/semanticdb-scala3/example/Extension.scala
@@ -4,8 +4,8 @@ extension (i/*example.Extension$package.asString().(i)*/: Int/*scala.Int#*/)
   def asString/*example.Extension$package.asString().*/: String/*scala.Predef.String#*/ = i/*example.Extension$package.asString().(i)*/.toString/*scala.Any#toString().*/
 
 extension (s/*example.Extension$package.asInt().(s)*//*example.Extension$package.double().(s)*/: String/*scala.Predef.String#*/) {
-  def asInt/*example.Extension$package.asInt().*/: Int/*scala.Int#*/ = /*scala.Predef.augmentString().*/s/*example.Extension$package.asInt().(s)*/.toInt/*scala.collection.StringOps#toInt().*/
-  def double/*example.Extension$package.double().*/: String/*scala.Predef.String#*/ = /*scala.Predef.augmentString().*/s/*example.Extension$package.double().(s)*/ */*scala.collection.StringOps#`*`().*/ 2
+  def asInt/*example.Extension$package.asInt().*/: Int/*scala.Int#*/ = s/*example.Extension$package.asInt().(s)*/.toInt/*scala.collection.StringOps#toInt().*/
+  def double/*example.Extension$package.double().*/: String/*scala.Predef.String#*/ = s/*example.Extension$package.double().(s)*/ */*scala.collection.StringOps#`*`().*/ 2
 }
 
 trait AbstractExtension/*example.AbstractExtension#*/ {
