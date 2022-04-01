54bd197f0028799d985a48ee7d656ddceb3b9f43
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-04-01 08:37:40 +0200

    Update bloop-config, bloop-launcher to 1.4.13-55-1fc97fae (#3774)

--- project/V.scala
+++ project/V.scala
@@ -12,7 +12,7 @@ object V {
   val ammonite213Version = "2.13.7"
 
   val ammonite = "2.5.2"
-  val bloop = "1.4.13-50-8332ee9c"
+  val bloop = "1.4.13-55-1fc97fae"
   val bloopNightly = bloop
   val bsp = "2.0.0+70-f6e47d42-SNAPSHOT"
   val coursier = "2.1.0-M5"
