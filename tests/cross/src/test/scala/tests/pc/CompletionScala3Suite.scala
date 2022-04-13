55f177b2d4a55d1784b8de36289506722bbc5f67
Rikito Taniguchi <rikiriki1238@gmail.com>
2022-04-13 20:12:07 +0900

    test: Add test for completions of matchtype / higher-kinded type
    https://github.com/scalameta/metals/issues/2810

--- tests/cross/src/test/scala/tests/pc/CompletionScala3Suite.scala
+++ tests/cross/src/test/scala/tests/pc/CompletionScala3Suite.scala
@@ -21,4 +21,25 @@ class CompletionScala3Suite extends BaseCompletionSuite {
        |""".stripMargin,
     topLines = Some(2)
   )
+
+  // https://github.com/scalameta/metals/issues/2810
+  check(
+    "higher-kinded-match-type".tag(IgnoreScalaVersion.forLessThan("3.1.3-RC2")),
+    """|package a
+       |
+       |trait Foo[A] {
+       |  def map[B](f: A => B): Foo[B] = ???
+       |}
+       |case class Bar[F[_]](bar: F[Int])
+       |type M[T] = T match {
+       |  case Int => Foo[Int]
+       |}
+       |object Test:
+       |  val x = Bar[M](new Foo[Int]{})
+       |  x.bar.m@@
+       |""".stripMargin,
+    """|map[B](f: A => B): Foo[B]
+       |""".stripMargin,
+    topLines = Some(1)
+  )
 }
