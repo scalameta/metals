7276573dd443ea45c54d1c19b9eba648899e0660
Kamil Podsiadlo <kpodsiadlo@virtuslab.com>
2022-04-26 16:27:52 +0200

    chore: update server version after release

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -6,10 +6,12 @@ argumentsRest=${@:2}
 suffix=${argumentsRest:-}
 
 coursier fetch \
+  org.scalameta:metals_2.13:$version \
   org.scalameta:mtags_3.0.1:$version \
   org.scalameta:mtags_3.0.2:$version \
   org.scalameta:mtags_3.1.0:$version \
-  org.scalameta:metals_2.12:$version \
+  org.scalameta:mtags_3.1.1:$version \
+  org.scalameta:mtags_3.1.2:$version \
   org.scalameta:mtags_2.13.1:$version \
   org.scalameta:mtags_2.13.2:$version \
   org.scalameta:mtags_2.13.3:$version \
