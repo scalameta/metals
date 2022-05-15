97e4c7e3282425fa4e6c922eebf8161c955477ae
Scala Steward <me@scala-steward.org>
2022-05-15 12:42:33 +0200

    Revert commit(s) 780d1144d

--- project/V.scala
+++ project/V.scala
@@ -33,7 +33,7 @@ object V {
   val sbtBloop = bloop
   val sbtJdiTools = "1.1.1"
   val scalafix = "0.10.0"
-  val scalafmt = "3.5.3"
+  val scalafmt = "3.5.2"
   val scalameta = "4.5.5"
   val scribe = "3.8.2"
   val semanticdb = scalameta
