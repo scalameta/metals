ec0797e3491338b8e206404bd8b7202f114c1db9
Rikito Taniguchi <rikiriki1238@gmail.com>
2022-05-19 17:17:01 +0900

    refactor: remove unnecessary penalty case for Override

--- mtags/src/main/scala-3/scala/meta/internal/pc/completions/Completions.scala
+++ mtags/src/main/scala-3/scala/meta/internal/pc/completions/Completions.scala
@@ -306,11 +306,6 @@ class Completions(
     end symbolRelevance
 
     completion match
-      case ov: CompletionValue.Override =>
-        var penalty = symbolRelevance(ov.symbol)
-        // if (!ov.symbol.is(Deferred)) penalty |= MemberOrdering.IsNotAbstract
-        penalty
-
       case CompletionValue.Workspace(_, sym) =>
         symbolRelevance(sym) | (IsWorkspaceSymbol + sym.name.show.length)
       case sym: CompletionValue.Symbolic =>
