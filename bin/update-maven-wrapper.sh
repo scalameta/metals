b29fd33fce20183b3467d2b45bb777df0fb86908
ckipp01 <ckipp@pm.me>
2022-06-13 09:17:18 +0200

    chore(maven): update the maven wrapper
    Looks like this can potentially cause issues with Maven builds that are
    requiring a more modern version of Maven.
    
    refs #4023

---
+++ bin/update-maven-wrapper.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# NOTE: Run this from the root of metals ./bin/update-maven-wrapper.sh
+
+if ! command -v cs *> /dev/null
+then
+  echo "You must have cs installed to use this script"
+  exit 1
+fi
+
+latest_wrapper=$(cs complete-dep io.takari:maven-wrapper: | tail -1)
+latest_maven=$(cs complete-dep org.apache.maven:apache-maven: | tail -1)
+
+curl https://repo.maven.apache.org/maven2/io/takari/maven-wrapper/${latest_wrapper}/maven-wrapper-${latest_wrapper}.jar > $PWD/metals/src/main/resources/maven-wrapper.jar
+
+sed -i "s/.*apache-maven.*/distributionUrl=https:\/\/repo.maven.apache.org\/maven2\/org\/apache\/maven\/apache-maven\/${latest_maven}\/apache-maven-${latest_maven}-bin.zip/" \
+  $PWD/metals/src/main/resources/maven-wrapper.properties
+sed -i "s/.*takari.*/wrapperUrl=https:\/\/repo.maven.apache.org\/maven2\/io\/takari\/maven-wrapper\/${latest_wrapper}\/maven-wrapper-${latest_wrapper}.jar/" \
+  $PWD/metals/src/main/resources/maven-wrapper.properties
