b3e01fe46ab0204b04470a4e17b8c687290eb27d
Scala Steward <scala_steward@virtuslab.com>
2022-07-08 04:38:31 +0000

    build(deps): Update mill-contrib-testng from 0.10.4 to 0.10.5

--- project/V.scala
+++ project/V.scala
@@ -27,7 +27,7 @@ object V {
   val kindProjector = "0.13.2"
   val lsp4jV = "0.14.0"
   val mavenBloop = bloop
-  val mill = "0.10.4"
+  val mill = "0.10.5"
   val mdoc = "2.3.2"
   val munit = "1.0.0-M6"
   val organizeImportRule = "0.6.0"
