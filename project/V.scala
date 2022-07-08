ada2ef71a8f85deed768da540e0d210e2261d9b2
Scala Steward <scala_steward@virtuslab.com>
2022-07-08 04:39:13 +0000

    build(deps): Update scala3-compiler, scala3-library from 3.2.0-RC1 to 3.2.0-RC2

--- project/V.scala
+++ project/V.scala
@@ -6,7 +6,7 @@ object V {
   val scala212 = "2.12.16"
   val scala213 = "2.13.8"
   val scala3 = "3.1.3"
-  val nextScala3RC = "3.2.0-RC1"
+  val nextScala3RC = "3.2.0-RC2"
   val sbtScala = "2.12.14"
   val ammonite212Version = "2.12.15"
   val ammonite213Version = "2.13.7"
