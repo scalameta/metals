ae60fa388a56d1fb75391c8060f81af23beb9874
Tomasz Godzik <tgodzik@users.noreply.github.com>
2022-07-29 19:54:55 +0200

    chore: Bump Bloop to latest to test out recent changes (#4204)
    chore: Bump Bloop to latest v1.5.3
    
    Also fix the automatic start of the Bloop server at the start of testing.

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function bloop_version {
-  grep "val bloop" build.sbt | sed -n 's/.*"\(.*\)".*/\1/p'
+  grep "val bloop" project/V.scala | sed -n 's/.*"\(.*\)".*/\1/p'
 }
 
 mkdir -p ~/.bloop
