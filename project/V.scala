a7b67164ffe0de09e92e66b69fc2283dae0dda1c
Arthur S <36887373+arixmkii@users.noreply.github.com>
2022-07-29 11:45:59 +0300

    Update debug adapter to 2.2.0 stable (dependency update) (#4203)

--- project/V.scala
+++ project/V.scala
@@ -19,7 +19,7 @@ object V {
   val bsp = "2.1.0-M1"
   val coursier = "2.1.0-M6"
   val coursierInterfaces = "1.0.8"
-  val debugAdapter = "2.2.0-M1"
+  val debugAdapter = "2.2.0"
   val genyVersion = "0.7.1"
   val gradleBloop = bloop
   val java8Compat = "1.0.2"
