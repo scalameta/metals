d04e8204e10ed3e42068c65beb7847d519ff1fc1
Vadim Chelyshov <qtankle@gmail.com>
2022-08-18 22:17:57 +0300

    upgrade bloop to 1.5.3-28-373a64c9 (#4275)

--- project/V.scala
+++ project/V.scala
@@ -14,7 +14,7 @@ object V {
 
   val ammonite = "2.5.4-13-1ebd00a6"
   val betterMonadicFor = "0.3.1"
-  val bloop = "1.5.3"
+  val bloop = "1.5.3-28-373a64c9"
   val bloopNightly = bloop
   val bsp = "2.1.0-M1"
   val coursier = "2.1.0-M6"
