e48b5ad4e32f288f66c66f2142ad4c4405cb8a74
Tomasz Godzik <tgodzik@users.noreply.github.com>
2022-08-31 17:43:32 +0200

    chore: Add support for Scala 3.2.0 (#4315)

--- project/V.scala
+++ project/V.scala
@@ -5,8 +5,7 @@ object V {
   val scala211 = "2.11.12"
   val scala212 = "2.12.16"
   val scala213 = "2.13.8"
-  val scala3 = "3.1.3"
-  val nextScala3RC = "3.2.0-RC4"
+  val scala3 = "3.2.0"
   val sbtScala = "2.12.14"
   val ammonite212Version = "2.12.16"
   val ammonite213Version = "2.13.8"
@@ -94,9 +93,9 @@ object V {
 
   // Scala 3
   def nonDeprecatedScala3Versions =
-    Seq(nextScala3RC, scala3, "3.1.2", "3.1.1", "3.1.0")
+    Seq(scala3, "3.1.3", "3.1.2", "3.1.1")
   def deprecatedScala3Versions =
-    Seq("3.2.0-RC3", "3.2.0-RC2", "3.2.0-RC1", "3.0.2", "3.0.1", "3.0.0")
+    Seq("3.2.0-RC4", "3.2.0-RC3", "3.1.0", "3.0.2", "3.0.1", "3.0.0")
   def scala3Versions = nonDeprecatedScala3Versions ++ deprecatedScala3Versions
 
   lazy val nightlyScala3DottyVersions = {
@@ -123,7 +122,6 @@ object V {
       scala213,
       ammonite213Version,
       scala3,
-      nextScala3RC,
       ammonite3Version,
     ).toList
 }
