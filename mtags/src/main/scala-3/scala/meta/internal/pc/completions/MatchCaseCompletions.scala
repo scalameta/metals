4c40bec0a9a65b8887f5ae528c834d567a606bc2
jkciesluk <72950514+jkciesluk@users.noreply.github.com>
2022-09-06 13:32:29 +0200

    Update mtags/src/main/scala-3/scala/meta/internal/pc/completions/MatchCaseCompletions.scala
    Co-authored-by: Waris Radji <wradji64@gmail.com>

--- mtags/src/main/scala-3/scala/meta/internal/pc/completions/MatchCaseCompletions.scala
+++ mtags/src/main/scala-3/scala/meta/internal/pc/completions/MatchCaseCompletions.scala
@@ -147,9 +147,11 @@ object CaseKeywordCompletion:
         && (sym.is(Case) || sym.is(Flags.Module) || sym.isClass)
         && parents.isSubClass(sym, false)
       indexedContext.scopeSymbols
-        .map(_.info.dealias.typeSymbol)
-        .filter(isValid(_))
-        .foreach(s => visit(s, s.decodedName, Nil))
+        .foreach(s => {
+          val ts  = s.info.dealias.typeSymbol
+          if (isValid(ts))
+            visit(ts, ts.decodedName, Nil)
+         )}
 
       // Step 2: walk through known subclasses of sealed types.
       MetalsSealedDesc.sealedStrictDescendants(selectorSym).foreach { sym =>
