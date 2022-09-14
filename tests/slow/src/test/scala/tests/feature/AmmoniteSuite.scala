1addbf1044ba2f5a37a8664c68a822ad0de026ee
LaurenceWarne <17688577+LaurenceWarne@users.noreply.github.com>
2022-09-14 13:02:57 +0100

    Add Completions for ivy Imports (Ammonite) (#4376)
    Hi, I've been playing around with Ammonite ivy import completions in metals (heavily inspired by https://github.com/com-lihaoyi/Ammonite/pull/957) using the awesome coursier api, demo:
    
    It works very similarly to the existing completions for `$file`s, essentially we just ask `coursierapi.Complete` for completions.  Additionally, when we're at `import $ivy.|` we return a snippet to handle backticks, and when we're at `group:|` we make sure to get scala verion-specific (e.g. `%%`) completions also (hopefully these are both clear from the video).
    
    If you're all happy with this, I'd prefer to work incrementally on the above so as not to make one huge PR :slightly_smiling_face: .  Anyway, let me know what you think, thanks!

--- tests/slow/src/test/scala/tests/feature/AmmoniteSuite.scala
+++ tests/slow/src/test/scala/tests/feature/AmmoniteSuite.scala
@@ -3,7 +3,58 @@ package tests.feature
 import scala.meta.internal.metals.ServerCommands
 import scala.meta.internal.metals.{BuildInfo => V}
 
-class Ammonite213Suite extends tests.BaseAmmoniteSuite(V.ammonite213)
+class Ammonite213Suite extends tests.BaseAmmoniteSuite(V.ammonite213) {
+
+  test("ivy-completion") {
+    for {
+      _ <- initialize(
+        s"""
+           |/metals.json
+           |{
+           |  "a": {
+           |    "scalaVersion": "${V.ammonite213}"
+           |  }
+           |}
+           |/main.sc
+           |import $$ivy.`io.cir`
+           |import $$ivy.`io.circe::circe-ref`
+           |import $$ivy.`io.circe::circe-yaml:0.14`
+           |""".stripMargin
+      )
+      _ <- server.didOpen("main.sc")
+      _ <- server.didSave("main.sc")(identity)
+      _ <- server.executeCommand(ServerCommands.StartAmmoniteBuildServer)
+
+      groupExpectedCompletionList = "io.circe"
+      groupCompletionList <- server.completion(
+        "main.sc",
+        "import $ivy.`io.cir@@`",
+      )
+      _ = assertNoDiff(groupCompletionList, groupExpectedCompletionList)
+
+      artefactExpectedCompletionList =
+        """
+          |circe-refined
+          |circe-refined_sjs0.6
+          |circe-refined_sjs1""".stripMargin
+      artefactCompletionList <- server.completion(
+        "main.sc",
+        "import $ivy.`io.circe::circe-ref@@`",
+      )
+      _ = assertNoDiff(artefactCompletionList, artefactExpectedCompletionList)
+
+      versionExpectedCompletionList =
+        """
+          |0.14.0
+          |0.14.1""".stripMargin
+      versionCompletionList <- server.completion(
+        "main.sc",
+        "import $ivy.`io.circe::circe-yaml:0.14@@`",
+      )
+      _ = assertNoDiff(versionCompletionList, versionExpectedCompletionList)
+    } yield ()
+  }
+}
 
 class Ammonite3Suite extends tests.BaseAmmoniteSuite(V.ammonite3)
 
