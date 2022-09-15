80334760fd3783a0b71d777ac068817c2e39e951
Tomasz Godzik <tgodzik@users.noreply.github.com>
2022-09-15 14:09:43 +0200

    chore: Add support for Scala 2.12.17 (#4382)

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -21,7 +21,6 @@ coursier fetch \
   org.scalameta:mtags_2.13.6:$version \
   org.scalameta:mtags_2.13.7:$version \
   org.scalameta:mtags_2.13.8:$version \
-  org.scalameta:mtags_2.12.8:$version \
   org.scalameta:mtags_2.12.9:$version \
   org.scalameta:mtags_2.12.10:$version \
   org.scalameta:mtags_2.12.11:$version \
@@ -30,4 +29,5 @@ coursier fetch \
   org.scalameta:mtags_2.12.14:$version \
   org.scalameta:mtags_2.12.15:$version \
   org.scalameta:mtags_2.12.16:$version \
+  org.scalameta:mtags_2.12.17:$version \
   org.scalameta:mtags_2.11.12:$version $suffix
