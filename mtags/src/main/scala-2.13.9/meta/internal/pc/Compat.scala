de34e01ef61b28c36250b0f6b2c3cdd4254c6d95
Vadim Chelyshov <qtankle@gmail.com>
2022-09-23 13:23:31 +0300

    Scala 2.13.9 (#4414)
    * Scala 2.13.9 support
    
    Due to https://github.com/scala/scala/pull/9754 it was required to add
    additional compat method to filter out non relevant type completions.

--- mtags/src/main/scala-2.13.9/meta/internal/pc/Compat.scala
+++ mtags/src/main/scala-2.13.9/meta/internal/pc/Compat.scala
@@ -15,5 +15,9 @@ trait Compat { this: MetalsGlobal =>
       case _ => None
     }
 
-  def isAliasCompletion(m: Member): Boolean = false
+  def isAliasCompletion(m: Member): Boolean = m match {
+    case tm: TypeMember => tm.aliasInfo.nonEmpty
+    case sm: ScopeMember => sm.aliasInfo.nonEmpty
+    case _ => false
+  }
 }
