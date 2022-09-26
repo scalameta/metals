f9764f120706e53222afcd7e7de3095f45540b38
Tomasz Godzik <tgodzik@virtuslab.com>
2022-09-26 15:49:21 +0200

    chore: Update Sacal version used by sbt

--- project/V.scala
+++ project/V.scala
@@ -7,7 +7,7 @@ object V {
   val scala213 = "2.13.9"
   val scala3 = "3.2.0"
   val nextScala3RC = "3.2.1-RC2"
-  val sbtScala = "2.12.14"
+  val sbtScala = "2.12.16"
   val ammonite212Version = "2.12.17"
   val ammonite213Version = "2.13.9"
   val ammonite3Version = "3.1.3"
