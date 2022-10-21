d1686def719684b532d6093b31fa61bda9aa4eb7
Jakub Ciesluk <323892@uwr.edu.pl>
2022-10-21 11:35:20 +0200

    fix for imports after closing quote

--- tests/unit/src/main/scala/tests/BaseAmmoniteSuite.scala
+++ tests/unit/src/main/scala/tests/BaseAmmoniteSuite.scala
@@ -631,15 +631,22 @@ abstract class BaseAmmoniteSuite(scalaVersion: String)
       )
       _ = assertNoDiff(artefactCompletionList, artefactExpectedCompletionList)
 
-      versionExpectedCompletionList =
-        """
-          |0.14.1
-          |0.14.0""".stripMargin
-      versionCompletionList <- server.completion(
+      versionExpectedCompletionList = List("0.14.1", "0.14.0")
+      response <- server.completionList(
         "main.sc",
         "import $ivy.`io.circe::circe-yaml:0.14@@`",
       )
-      _ = assertNoDiff(versionCompletionList, versionExpectedCompletionList)
+      versionCompletionList = response
+        .getItems()
+        .asScala
+        .map(_.getLabel())
+        .toList
+      _ = assertEquals(versionCompletionList, versionExpectedCompletionList)
+      noCompletions <- server.completion(
+        "main.sc",
+        "import $ivy.`io.circe::circe-yaml:0.14`@@",
+      )
+      _ = assertNoDiff(noCompletions, "")
     } yield ()
   }
 }
