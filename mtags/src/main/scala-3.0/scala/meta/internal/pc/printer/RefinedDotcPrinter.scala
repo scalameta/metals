b7b73960ff82b20cff4e47a9eed6290bee474b43
Tomasz Godzik <tgodzik@virtuslab.com>
2022-11-14 10:07:52 +0100

    bugfix: Fix compliation issues with 3.3.x
    Previously, toTextRefinement would return `Closed` type and now it returns `Text`, which is causing the compilation issues.
    
    Now, since the printer prints the types for refinments correctly, we can avoid overriding the method altogether.
    
    The type is printer a bit differently, but it does compile, so let's keep it in line with the compiler.

---
+++ mtags/src/main/scala-3.0/scala/meta/internal/pc/printer/RefinedDotcPrinter.scala
@@ -0,0 +1,16 @@
+package scala.meta.internal.pc.printer
+
+import dotty.tools.dotc.printing.RefinedPrinter
+import dotty.tools.dotc.core.Types.*
+import dotty.tools.dotc.core.Contexts.Context
+
+abstract class RefinedDotcPrinter(_ctx: Context) extends RefinedPrinter(_ctx):
+
+  override protected def toTextRefinement(rt: RefinedType) =
+    val keyword = rt.refinedInfo match
+      case _: ExprType | _: MethodOrPoly => "def "
+      case _: TypeBounds => "type "
+      case _: TypeProxy => "val "
+      case _ => ""
+    (keyword ~ refinementNameString(rt) ~ toTextRHS(rt.refinedInfo)).close
+end RefinedDotcPrinter
--- tests/cross/src/test/scala/tests/pc/InsertInferredTypeSuite.scala
+++ tests/cross/src/test/scala/tests/pc/InsertInferredTypeSuite.scala
@@ -646,6 +646,18 @@ class InsertInferredTypeSuite extends BaseCodeActionSuite {
        |  val c: Foo{type T = Int; type G = Long} = new Foo { type T = Int; type G = Long}
        |}
        |""".stripMargin,
+    compat = Map(
+      "3" ->
+        """|object O{
+           |  trait Foo {
+           |    type T
+           |    type G
+           |  }
+           |
+           |  val c: Foo{type T >: Int <: Int; type G >: Long <: Long} = new Foo { type T = Int; type G = Long}
+           |}
+           |""".stripMargin
+    ),
   )
 
   checkEdit(
@@ -666,6 +678,17 @@ class InsertInferredTypeSuite extends BaseCodeActionSuite {
        |  val d: Foo{type T = Int} = c
        |}
        |""".stripMargin,
+    compat = Map(
+      "3" ->
+        """|object O{
+           |  trait Foo {
+           |    type T
+           |  }
+           |  val c = new Foo { type T = Int }
+           |  val d: Foo{type T >: Int <: Int} = c
+           |}
+           |""".stripMargin
+    ),
   )
 
   checkEdit(
@@ -686,6 +709,17 @@ class InsertInferredTypeSuite extends BaseCodeActionSuite {
        |  val c: Foo{type T = Int} = new Foo { type T = Int }
        |}
        |""".stripMargin,
+    compat = Map(
+      "3" ->
+        """|object O{
+           |  trait Foo {
+           |    type T
+           |  }
+           |
+           |  val c: Foo{type T >: Int <: Int} = new Foo { type T = Int }
+           |}
+           |""".stripMargin
+    ),
   )
 
   checkEdit(
@@ -705,7 +739,7 @@ class InsertInferredTypeSuite extends BaseCodeActionSuite {
        |  type T
        |}
        |
-       |val c: Foo{type T = Int; val x: Int; def y: Int; val z: Int; def z_=(x$1: Int): Unit} = new Foo {
+       |val c: Foo{type T >: Int <: Int; val x: Int; def y: Int; val z: Int; def z_=(x$1: Int): Unit} = new Foo {
        |  type T = Int
        |  val x = 0
        |  def y = 0
