50eaf5b8b0a789bb37ca391ddb26536301d3c808
Tomasz Godzik <tgodzik@virtuslab.com>
2022-12-15 11:43:15 +0100

    chore: Bump Scalameta to 4.7.0
    Fixes https://github.com/scalameta/metals/issues/4584

--- tests/unit/src/test/resources/semanticdb/example/MacroAnnotation.scala
+++ tests/unit/src/test/resources/semanticdb/example/MacroAnnotation.scala
@@ -14,6 +14,12 @@ object MacroAnnotations/*example.MacroAnnotations.*/ {
   import scala.meta._
   // IntelliJ has never managed to goto definition for the inner classes from Trees.scala
   // due to the macro annotations.
-  val x/*example.MacroAnnotations.x.*/: Defn/*scala.meta.Defn.*/.Class/*scala.meta.Defn.Class#*/ = Defn/*scala.meta.Defn.*/.Class/*scala.meta.Defn.Class.*/(null, null, null, null, null)
+  val x/*example.MacroAnnotations.x.*/: Defn/*scala.meta.Defn.*/.Class/*scala.meta.Defn.Class#*/ = Defn/*scala.meta.Defn.*/.Class/*scala.meta.Defn.Class.*/(
+    Nil/*scala.package.Nil.*/,
+    Type/*scala.meta.Type.*/.Name/*scala.meta.Type.Name.*/("test"),
+    Nil/*scala.package.Nil.*/,
+    Ctor/*scala.meta.Ctor.*/.Primary/*scala.meta.Ctor.Primary.*/(Nil/*scala.package.Nil.*/, Term/*scala.meta.Term.*/.Name/*scala.meta.Term.Name.*/("this"), Nil/*scala.package.Nil.*/),
+    Template/*scala.meta.Template.*/(Nil/*scala.package.Nil.*/, Nil/*scala.package.Nil.*/, Self/*scala.meta.Self.*/(Name/*scala.meta.Name.*/.Anonymous/*scala.meta.Name.Anonymous.*/(), None/*scala.None.*/), Nil/*scala.package.Nil.*/),
+  )
   val y/*example.MacroAnnotations.y.*/: Mod/*scala.meta.Mod.*/.Final/*scala.meta.Mod.Final#*/ = Mod/*scala.meta.Mod.*/.Final/*scala.meta.Mod.Final.*/()
 }
