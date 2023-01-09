08214ae7e46f54e12db7f314e3afad3c30a120e2
Tomasz Godzik <tgodzik@virtuslab.com>
2023-01-09 17:51:52 +0100

    improvement: Add benchmarks for SemanticHighlight
    They can be run using:
    
    ```
    sbt bench/jmh:run -i 10 -wi 10 -f1 -t1 .*CachedSearchAndCompilerSemanticHighlightBench
    ```
    
    I also reworked the CompletionBenchmark a bit since it needs a lot of the same elements.

---
+++ metals-bench/src/main/scala/bench/SemanticHighlightBench.scala
@@ -0,0 +1,85 @@
+package bench
+
+import java.net.URI
+import java.nio.charset.StandardCharsets
+import java.util.concurrent.TimeUnit
+import java.{util => ju}
+
+import scala.meta.internal.io.FileIO
+import scala.meta.internal.metals.CompilerVirtualFileParams
+import scala.meta.internal.metals.EmptyCancelToken
+import scala.meta.io.AbsolutePath
+
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.BenchmarkMode
+import org.openjdk.jmh.annotations.Mode
+import org.openjdk.jmh.annotations.OutputTimeUnit
+import org.openjdk.jmh.annotations.Param
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.State
+
+@State(Scope.Benchmark)
+class SemanticHighlightBench extends PcBenchmark {
+  var highlightRequests: Map[String, String] = Map.empty
+
+  private def fromZipPath(zip: AbsolutePath, path: String) = {
+    FileIO.withJarFileSystem(zip, create = false, close = true)(root =>
+      FileIO.slurp(root.resolve(path), StandardCharsets.UTF_8)
+    )
+  }
+
+  def beforeAll(): Unit = {
+    val akka = Corpus.akka()
+    val replicator =
+      "akka-2.5.19/akka-cluster/src/main/scala/akka/cluster/ClusterDaemon.scala"
+    val scala = Corpus.scala()
+    val typers =
+      s"scala-${bench.BuildInfo.scalaVersion}/src/compiler/scala/tools/nsc/typechecker/Typers.scala"
+    val fastparse = Corpus.fastparse()
+    val exprs =
+      "fastparse-2.1.0/scalaparse/src/scalaparse/Exprs.scala"
+
+    highlightRequests = Map(
+      "A.scala" ->
+        """
+          |import Java
+          |import scala.collection.mutable
+          |        """.stripMargin,
+      "ClusterDaemon.scala" -> fromZipPath(
+        akka,
+        replicator,
+      ),
+      "Typers.scala" -> fromZipPath(
+        scala,
+        typers,
+      ),
+      "Exprs.scala" -> fromZipPath(
+        fastparse,
+        exprs,
+      ),
+    )
+  }
+
+  @Param(
+    Array("A.scala", "ClusterDaemon.scala", "Typers.scala", "Exprs.scala")
+  )
+  var currentHighlightRequest: String = _
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.SingleShotTime))
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  def semanticHighlight(): ju.List[Integer] = {
+    val pc = presentationCompiler()
+    val text = currentHighlight
+    val vFile = CompilerVirtualFileParams(
+      URI.create(s"file://${currentHighlightRequest}"),
+      text,
+      EmptyCancelToken,
+    )
+
+    pc.semanticTokens(vFile).get()
+  }
+
+  def currentHighlight: String = highlightRequests(currentHighlightRequest)
+
+}
