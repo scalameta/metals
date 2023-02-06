19c2219112aa45d0aefe8b82d4883b4c5169f089
Tomasz Godzik <tgodzik@virtuslab.com>
2023-02-06 10:13:35 +0100

    chore: Fix expected line in FindTextInDependencyJarsSuite
    This looks like JDK being slowly updated on different machines, so this started failing only after some time on all sytems.

--- tests/unit/src/test/scala/tests/FindTextInDependencyJarsSuite.scala
+++ tests/unit/src/test/scala/tests/FindTextInDependencyJarsSuite.scala
@@ -67,8 +67,7 @@ class FindTextInDependencyJarsSuite
       assertLocations(
         jdkLocations, {
           val line =
-            if (isJavaAtLeast17 && isWindows) 1445
-            else if (isJavaAtLeast17) 1447
+            if (isJavaAtLeast17) 1445
             else if (isJavaAtLeast9) 626
             else 578
 
