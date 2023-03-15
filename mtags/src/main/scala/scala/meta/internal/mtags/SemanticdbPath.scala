94473b67728a2a7148d4c2df424f51c41b495a8c
Kamil Podsiadlo <kamilpodsiadlo44@gmail.com>
2023-03-15 19:34:42 +0100

    add some docs

--- mtags/src/main/scala/scala/meta/internal/mtags/SemanticdbPath.scala
+++ mtags/src/main/scala/scala/meta/internal/mtags/SemanticdbPath.scala
@@ -5,6 +5,9 @@ import java.nio.file.Path
 import scala.meta.internal.mtags.MtagsEnrichments._
 import scala.meta.io.AbsolutePath
 
+/**
+ * Wrapper over a path which is semanticDB file.
+ */
 final case class SemanticdbPath(val absolutePath: AbsolutePath) extends AnyVal {
   def semanticdbRoot: Option[Path] = absolutePath.toNIO.semanticdbRoot
   def toNIO: Path = absolutePath.toNIO
