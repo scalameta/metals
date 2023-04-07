912aa99a885a7d6cbbe4c8df77212631d5c4c720
Jakub Ciesluk <323892@uwr.edu.pl>
2023-04-07 09:42:51 +0200

    fix: Semantic highlight on for-comp with `=` line
    In Scala 2, when for-comprehension includes `=` line, we get `scala.x$#` symbols on variables.
    We don't want to highlight them as a `scala` package, so `namespace` needs to have lower priority that `variable`

--- mtags-shared/src/main/scala/scala/meta/internal/pc/SemanticTokens.scala
+++ mtags-shared/src/main/scala/scala/meta/internal/pc/SemanticTokens.scala
@@ -48,15 +48,18 @@ object SemanticTokens {
   val getTypePriority: Map[Int, Int] = Map(
     // In .sbt on `List` are symbols `scala`, `collection`, `immutable`, `List`,
     // We want to pick `List`, so `namespace` has lower priority than `class`
-    TokenTypes.indexOf(SemanticTokenTypes.Namespace) -> 7,
+    TokenTypes.indexOf(SemanticTokenTypes.Namespace) -> 4,
     TokenTypes.indexOf(SemanticTokenTypes.Type) -> 10,
     TokenTypes.indexOf(SemanticTokenTypes.Class) -> 8,
     // On parametrized enums like enum Foo(bar: Int): ...
     // We have both `class` and `enum`, so enum has higher priority than class
     TokenTypes.indexOf(SemanticTokenTypes.Enum) -> 9,
-    TokenTypes.indexOf(SemanticTokenTypes.Interface) -> 6,
-    TokenTypes.indexOf(SemanticTokenTypes.TypeParameter) -> 5,
-    TokenTypes.indexOf(SemanticTokenTypes.Variable) -> 4,
+    TokenTypes.indexOf(SemanticTokenTypes.Interface) -> 7,
+    TokenTypes.indexOf(SemanticTokenTypes.TypeParameter) -> 6,
+    // In scala 2 for comprehensions including lines with `=`, we get `scala.x$1` symbols,
+    // we don't want to choose `scala` package`, so `variable` has higher priority than `namespace`
+    // See test `for-comprehension` in SemanticTokensSuite
+    TokenTypes.indexOf(SemanticTokenTypes.Variable) -> 5,
     TokenTypes.indexOf(SemanticTokenTypes.EnumMember) -> 3,
     TokenTypes.indexOf(SemanticTokenTypes.Method) -> 2,
     // In `case class A(a: Int)` we have class variable `a` and `apply` and `copy` parameter `a`
