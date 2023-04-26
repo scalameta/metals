4b56fe72d7dc30089577c2b14a32b8974db465a9
Tomasz Godzik <tgodzik@virtuslab.com>
2023-04-26 18:06:40 +0200

    improvement: Small improvement to the selection renage comments logic

--- tests/cross/src/test/scala/tests/pc/SelectionRangeCommentSuite.scala
+++ tests/cross/src/test/scala/tests/pc/SelectionRangeCommentSuite.scala
@@ -1,8 +1,8 @@
 package tests.pc
 
 class SelectionRangeCommentSuite extends BaseSelectionRangeSuite {
-
-// WIP! tests for selection expansion in comments not ready yet
+  
+  override def ignoreScalaVersion: Option[IgnoreScalaVersion] = Some(IgnoreScala2)
 
   check(
     "match",
@@ -15,19 +15,20 @@ class SelectionRangeCommentSuite extends BaseSelectionRangeSuite {
        |}""".stripMargin,
     List(
       """|object Main extends App {
-         |  /*>>region>>comment<<region<<*/
-         |  val total = for {
-         |    a <- Some(1)
-         |    b <- Some(2)
-         |  } yield a + b
-         |}""".stripMargin,
-      """|object Main extends App {
          |  >>region>>/*comment*/<<region<<
          |  val total = for {
          |    a <- Some(1)
          |    b <- Some(2)
          |  } yield a + b
          |}""".stripMargin,
+      """|object Main extends >>region>>App {
+         |  /*comment*/
+         |  val total = for {
+         |    a <- Some(1)
+         |    b <- Some(2)
+         |  } yield a + b<<region<<
+         |}
+         |""".stripMargin,
     ),
   )
 
