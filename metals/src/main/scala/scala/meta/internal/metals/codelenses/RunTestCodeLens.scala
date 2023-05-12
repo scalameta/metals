91506fdeaa2026adc058307c06de08c3857bfbcf
Jan Chyb <jan.chyb.99@gmail.com>
2023-05-12 10:07:02 +0200

    feat: Add Best Effort compilation support for Scala 3
    This PR is meant to add support for Best Effort compilation in metals,
    with a few additional considerations:
    * Some unnecessary options are replaced from bloop and set for PC
    * Previously, any project could be run from metals if semanticdb was
    able to be generated. This caused a somewhat weird UX for best effort.
    Now the build directory is explicitly checked for the existence of non
    META-INF files.
    * Previously, in metals doctor, compilation would be set as correct if
    a compilation did not produce any errors. However, with best effort
    compilation, the compiler may not find any additional errors, beyond
    the ones from its dependencies (meaning that a project that cannot
    actually be compiled, could show up as on that was compiling). A note
    about that was added in the doctor.

--- metals/src/main/scala/scala/meta/internal/metals/codelenses/RunTestCodeLens.scala
+++ metals/src/main/scala/scala/meta/internal/metals/codelenses/RunTestCodeLens.scala
@@ -36,6 +36,10 @@ import ch.epfl.scala.bsp4j.BuildTargetIdentifier
 import ch.epfl.scala.{bsp4j => b}
 import com.google.gson.JsonElement
 import org.eclipse.{lsp4j => l}
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.Path
+import java.nio.file.attribute.BasicFileAttributes
 
 /**
  * Class to generate the Run and Test code lenses to trigger debugging.
@@ -92,37 +96,68 @@ final class RunTestCodeLens(
       // although hasDebug is already available in BSP capabilities
       // see https://github.com/build-server-protocol/build-server-protocol/pull/161
       // most of the bsp servers such as bloop and sbt might not support it.
-    } yield requestJvmEnvironment(buildTargetId, isJVM).map { _ =>
-      val classes = buildTargetClasses.classesOf(buildTargetId)
+    } yield isNonMetaFileInClasspath(buildTargetId).flatMap {
+      (isNonMeta: Boolean) =>
+        if (isNonMeta) requestJvmEnvironment(buildTargetId, isJVM).map { _ =>
+          val classes = buildTargetClasses.classesOf(buildTargetId)
 
-      // sbt doesn't declare debugging provider
-      def buildServerCanDebug =
-        connection.isDebuggingProvider || connection.isSbt
+          // sbt doesn't declare debugging provider
+          def buildServerCanDebug =
+            connection.isDebuggingProvider || connection.isSbt
 
-      if (connection.isScalaCLI && path.isAmmoniteScript) {
-        scalaCliCodeLenses(
-          textDocument,
-          buildTargetId,
-          classes,
-          distance,
-          buildServerCanDebug,
-          isJVM,
-        )
-      } else if (buildServerCanDebug || clientConfig.isRunProvider()) {
-        codeLenses(
-          textDocument,
-          buildTargetId,
-          classes,
-          distance,
-          path,
-          buildServerCanDebug,
-          isJVM,
-        )
-      } else { Nil }
-
+          if (connection.isScalaCLI && path.isAmmoniteScript) {
+            scalaCliCodeLenses(
+              textDocument,
+              buildTargetId,
+              classes,
+              distance,
+              buildServerCanDebug,
+              isJVM,
+            )
+          } else if (buildServerCanDebug || clientConfig.isRunProvider()) {
+            codeLenses(
+              textDocument,
+              buildTargetId,
+              classes,
+              distance,
+              path,
+              buildServerCanDebug,
+              isJVM,
+            )
+          } else { Nil }
+        }
+        else Future.successful(Nil)
     }
 
     lenses.getOrElse(Future.successful(Nil))
+  }
+
+  /**
+   * Checks if there exist any files outside of the META-INF directory of build target classpath.
+   *
+   * When using Scala 3 Best Effort compilation, the compilation may fail,
+   * but semanticDB and betasty (and thus the META_INF directory) may still be produced.
+   * We want to avoid creating run/test/debug code lens in those situations.
+   */
+  private def isNonMetaFileInClasspath(
+      buildTargetId: BuildTargetIdentifier
+  ): Future[Boolean] = {
+    import scala.jdk.FunctionConverters._
+    buildTargetClasses
+      .jvmRunEnvironment(buildTargetId)
+      .map(
+        _.map(_.getClasspath().asScala.map(_.toAbsolutePath.toString).toList)
+          .map { classpath =>
+            classpath.exists { dir =>
+              val rootPath = Paths.get(dir)
+              val predicate = { (path: Path, _: BasicFileAttributes) =>
+                !path.endsWith("META-INF") && path != rootPath
+              }.asJavaBiPredicate
+              Files.find(rootPath, 1, predicate).findAny().asScala.isDefined
+            }
+          }
+          .getOrElse(true)
+      )
   }
 
   /**
