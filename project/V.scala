bb6d1f38c88749da1ab5738c2348717532c14c89
Brice Jaglin <bjaglin@gmail.com>
2023-06-03 15:21:16 +0200

    Scalafix 0.11.0

--- project/V.scala
+++ project/V.scala
@@ -36,7 +36,7 @@ object V {
   val sbtBloop = bloop
   val sbtJdiTools = "1.1.1"
   val scalaCli = "0.2.1"
-  val scalafix = "0.10.4"
+  val scalafix = "0.11.0"
   val scalafmt = "3.7.4"
   val scalameta = "4.7.8"
   val scribe = "3.11.5"
