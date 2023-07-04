68c38a69b091e9ead33557217861ab4490466de1
Katarzyna Marek <kasia@marek.net>
2023-07-04 09:42:43 +0200

    improvement: keep only last 10 backup log files

---
+++ metals/src/main/scala/scala/meta/internal/metals/utils/LimitedFilesManager.scala
@@ -0,0 +1,41 @@
+package scala.meta.internal.metals.utils
+
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+
+class LimitedFilesManager(
+    directory: Path,
+    fileLimit: Int,
+    prefixPattern: String,
+) {
+  private val fileNameRegex = s"${prefixPattern}([-+]?[0-9]+)".r
+
+  def getAllFiles(): List[TimestampedFile] = {
+    if (Files.exists(directory) && Files.isDirectory(directory)) {
+      directory.toFile.listFiles().flatMap(timestampedFile).toList
+    } else List()
+  }
+
+  def deleteOld(): List[TimestampedFile] = {
+    val files = getAllFiles()
+    if (files.length > fileLimit) {
+      val filesToDelete = files
+        .sortBy(_.timestamp)
+        .slice(0, files.length - fileLimit)
+      filesToDelete.foreach { f => Files.delete(f.toPath) }
+      filesToDelete
+    } else List()
+  }
+
+  private def timestampedFile(file: File): Option[TimestampedFile] = {
+    file.getName() match {
+      case fileNameRegex(time) => Some(TimestampedFile(file, time.toLong))
+      case _: String => None
+    }
+  }
+}
+
+case class TimestampedFile(file: File, timestamp: Long) {
+  def toPath: Path = file.toPath()
+}
