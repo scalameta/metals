ff9b95d57c43613969a3f3c7d91593a20afb943a
Tomasz Godzik <tgodzik@virtuslab.com>
2023-07-11 11:32:23 +0200

    chore: Update scala CLI to 1.0.2

--- project/V.scala
+++ project/V.scala
@@ -34,7 +34,7 @@ object V {
   val pprint = "0.7.3"
   val sbtBloop = bloop
   val sbtJdiTools = "1.1.1"
-  val scalaCli = "1.0.1"
+  val scalaCli = "1.0.2"
   val scalafix = "0.11.0"
   val scalafmt = "3.7.7"
   val scalameta = "4.8.2"
