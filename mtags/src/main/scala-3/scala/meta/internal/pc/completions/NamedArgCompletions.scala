e798ea37834df17cd519349a4cef05a260d3db79
Tomasz Godzik <tgodzik@virtuslab.com>
2023-08-02 17:36:07 +0200

    improvement: Automatically add colon to get better completions for Java
    Previously, if there was anything following the place you were trying to get a completion in, you would not get any suggestions since the following names would be take into account. Now, we add `;` after anything you are trying to complete if the next symbol is a whitespace to separate the current completion from the following statements.
    
    I also noticed that we don't set proper snippet format for the items, so I added that.

--- mtags/src/main/scala-3/scala/meta/internal/pc/completions/NamedArgCompletions.scala
+++ mtags/src/main/scala-3/scala/meta/internal/pc/completions/NamedArgCompletions.scala
@@ -10,7 +10,6 @@ import dotty.tools.dotc.ast.tpd.*
 import dotty.tools.dotc.core.Constants.Constant
 import dotty.tools.dotc.core.ContextOps.localContext
 import dotty.tools.dotc.core.Contexts.Context
-import dotty.tools.dotc.core.Definitions
 import dotty.tools.dotc.core.Flags
 import dotty.tools.dotc.core.Flags.Method
 import dotty.tools.dotc.core.NameKinds.DefaultGetterName
