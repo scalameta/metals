4b0fe4e4157cc13cdd95c4cfc620ff08ec25322a
tgodzik <tgodzik@virtuslab.com>
2023-09-05 23:40:41 +0200

    chore: Add support for Scala 3.3.1
    Also fix a small issue with releasing mtags for new versions of Scala

--- tests/cross/src/test/scala/tests/pc/CompletionScalaCliSuite.scala
+++ tests/cross/src/test/scala/tests/pc/CompletionScalaCliSuite.scala
@@ -100,7 +100,7 @@ class CompletionScalaCliSuite extends BaseCompletionSuite {
   check(
     "plugin".tag(
       IgnoreScalaVersion(version =>
-        Set("2.12.16", "2.12.18", "2.13.11")(version) ||
+        Set("2.12.16", "3.3.1")(version) ||
           version.contains(
             "NIGHTLY"
           ) || version.contains(
