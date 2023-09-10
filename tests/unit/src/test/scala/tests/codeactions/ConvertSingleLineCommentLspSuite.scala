f1e60edd720c4a8530700d34fd3b898f66ce7bb9
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-09-10 20:09:12 +0200

    Remove redundant test

--- tests/unit/src/test/scala/tests/codeactions/ConvertSingleLineCommentLspSuite.scala
+++ tests/unit/src/test/scala/tests/codeactions/ConvertSingleLineCommentLspSuite.scala
@@ -123,13 +123,4 @@ class ConvertSingleLineCommentLspSuite
     """/* comment middle */""",
     fileName = "script.sc",
   )
-
-  checkNoAction(
-    "should not show action for single-line multiline comment",
-    """|val a = 1
-       |<<>>/* comment middle */
-       |val b = 2""".stripMargin,
-    fileName = "script2.sc",
-  )
-
 }
