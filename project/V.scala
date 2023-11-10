908a92bc60cb1636857656ca910cc06d9f0e884e
Tomasz Godzik <tgodzik@virtuslab.com>
2023-11-10 17:29:12 +0100

    bugfix: Pin mdoc versions for different Scala versions and JDK ones

--- project/V.scala
+++ project/V.scala
@@ -33,7 +33,7 @@ object V {
   val lsp4jV = "0.20.1"
   val mavenBloop = "2.0.0"
   val mill = "0.11.5"
-  val mdoc = "2.5.0"
+  val mdoc = "2.5.1"
   val munit = "1.0.0-M10"
   val pprint = "0.7.3"
   val sbtBloop = bloop
