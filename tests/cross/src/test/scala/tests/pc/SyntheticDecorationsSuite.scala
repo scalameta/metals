853e5bfe357684590e5a65bb1f3a7760254a1a77
Jakub Ciesluk <323892@uwr.edu.pl>
2023-11-13 11:24:49 +0100

    ignore synthetic decorations tests for Scala 3 nightly

--- tests/cross/src/test/scala/tests/pc/SyntheticDecorationsSuite.scala
+++ tests/cross/src/test/scala/tests/pc/SyntheticDecorationsSuite.scala
@@ -6,6 +6,10 @@ import tests.BaseSyntheticDecorationsSuite
 
 class SyntheticDecorationsSuite extends BaseSyntheticDecorationsSuite {
 
+  override protected def ignoreScalaVersion: Option[IgnoreScalaVersion] = Some(
+    IgnoreForScala3CompilerPC
+  )
+
   check(
     "type-params",
     """|object Main {
