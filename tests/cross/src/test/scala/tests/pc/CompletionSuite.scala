495333fda5fac22cd98115e3c9858fe9f049abaa
Tomasz Godzik <tgodzik@virtuslab.com>
2023-11-28 19:11:41 +0100

    bugfix: Fix some of the tests in the nightly CI

--- tests/cross/src/test/scala/tests/pc/CompletionSuite.scala
+++ tests/cross/src/test/scala/tests/pc/CompletionSuite.scala
@@ -32,6 +32,14 @@ class CompletionSuite extends BaseCompletionSuite {
            |List - java.util
            |List - scala.collection.immutable
            |List[A](elems: A*): CC[A]
+           |""".stripMargin,
+      // TODO https://github.com/scalameta/metals/issues/5885
+      ">=3.4.0-RC1-bin-20231127-41e7d95-NIGHTLY" ->
+        """|List:  scala.collection.immutable
+           |List - java.awt
+           |List - java.util
+           |List - scala.collection.immutable
+           |List[A](elems: A*): CC[A]
            |""".stripMargin
     ),
     topLines = Some(5)
@@ -1065,7 +1073,14 @@ class CompletionSuite extends BaseCompletionSuite {
     """|None scala
        |NoManifest scala.reflect
        |""".stripMargin,
-    topLines = Some(2)
+    topLines = Some(2),
+    compat = Map(
+      // TODO https://github.com/scalameta/metals/issues/5885
+      ">=3.4.0-RC1-bin-20231127-41e7d95-NIGHTLY" ->
+        """|None scala
+           |NoManifest:  scala.reflect
+           |""".stripMargin
+    )
   )
 
   check(
@@ -1093,8 +1108,14 @@ class CompletionSuite extends BaseCompletionSuite {
            |""".stripMargin,
       "3" ->
         """|Some(value) scala
-           |Seq scala.collection.immutable
-           |Set scala.collection.immutable
+           |Seq: scala.collection.immutable
+           |Set: scala.collection.immutable
+           |""".stripMargin,
+      // TODO https://github.com/scalameta/metals/issues/5885
+      ">=3.4.0-RC1-bin-20231127-41e7d95-NIGHTLY" ->
+        """|Some(value) scala
+           |Seq:  scala.collection.immutable
+           |Set:  scala.collection.immutable
            |""".stripMargin
     )
   )
