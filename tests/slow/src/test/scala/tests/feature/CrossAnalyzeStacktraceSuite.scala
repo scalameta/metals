ccd13bafdb3a2e90fd98815b4f856af2f2e75f75
Tomasz Godzik <tgodzik@virtuslab.com>
2023-12-27 16:19:34 +0100

    bugfix: Fix StacktraceAnalyser in case of empty package

--- tests/slow/src/test/scala/tests/feature/CrossAnalyzeStacktraceSuite.scala
+++ tests/slow/src/test/scala/tests/feature/CrossAnalyzeStacktraceSuite.scala
@@ -29,4 +29,49 @@ class CrossAnalyzeStacktraceSuite
     scalaVersion = V.scala3,
   )
 
+  check(
+    "no-package",
+    """|
+       |def fetch() =
+       |  println("Fetching!")
+       |<<1>>  throw new Exception("")
+       |
+       |@main
+       |def main() = 
+       |<<2>>  fetch()
+       |  
+       |def fetch(a: Int) = a + 2
+       |""".stripMargin,
+    """|Exception in thread "main" java.lang.Exception:
+       |at other$package$.fetch(other.scala:4)
+       |at other$package$.main(other.scala:8)
+       |at main.main(other.scala:6)
+       |""".stripMargin,
+    filename = "other.scala",
+    scalaVersion = V.scala3,
+  )
+
+  check(
+    "no-package-type",
+    """|
+       |
+       |object O:
+       |  def fetch() =
+       |    println("Fetching!")
+       |<<1>>    throw new Exception("")
+       |
+       |@main
+       |def main() = 
+       |<<2>>  fetch()
+       |  
+       |def fetch(a: Int) = a + 2
+       |""".stripMargin,
+    """|Exception in thread "main" java.lang.Exception:
+       |at O$.fetch(Main.scala:6)
+       |at other$package$.main(other.scala:10)
+       |at main.main(other.scala:8)
+       |""".stripMargin,
+    filename = "other.scala",
+    scalaVersion = V.scala3,
+  )
 }
