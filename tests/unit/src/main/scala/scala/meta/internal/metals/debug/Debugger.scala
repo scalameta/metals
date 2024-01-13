057550b6f72510f5d5dbafa60a93774c865ac190
mekpavit <pavit140239@gmail.com>
2024-01-13 22:28:13 +0700

    add test-case to reproduce bug

--- tests/unit/src/main/scala/scala/meta/internal/metals/debug/Debugger.scala
+++ tests/unit/src/main/scala/scala/meta/internal/metals/debug/Debugger.scala
@@ -107,11 +107,13 @@ final class Debugger(server: RemoteServer)(implicit ec: ExecutionContext) {
           .asScala
           .map(callback)
           .flatMap(_ => step(threadId, nextStep))
-      case DebugStep.Complete(expression, frameId, callback, line, character) =>
+      case DebugStep.Complete(expression, frameId, callback, line, character, isLineNullable) =>
         val args = new CompletionsArguments()
         args.setFrameId(frameId)
         args.setText(expression)
-        args.setLine(line)
+        if (!isLineNullable) {
+          args.setLine(line)
+        }
         args.setColumn(character)
         server.completions(args).asScala.flatMap { completions =>
           callback(completions)
