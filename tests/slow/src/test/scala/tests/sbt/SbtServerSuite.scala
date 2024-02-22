ff18eab354ced1ff642b41703a88c5b366e2fab5
Tomasz Godzik <tgodzik@virtuslab.com>
2024-02-22 15:56:51 +0100

    chore: Add support for Scala 2.12.19

--- tests/slow/src/test/scala/tests/sbt/SbtServerSuite.scala
+++ tests/slow/src/test/scala/tests/sbt/SbtServerSuite.scala
@@ -34,7 +34,7 @@ class SbtServerSuite
     with ScriptsAssertions {
 
   val preBspVersion = "1.3.13"
-  val supportedMetaBuildVersion = "1.6.0-M1"
+  val supportedMetaBuildVersion = "1.7.0"
   val supportedBspVersion = V.sbtVersion
   val scalaVersion = V.scala213
   val buildTool: SbtBuildTool = SbtBuildTool(None, workspace, () => userConfig)
