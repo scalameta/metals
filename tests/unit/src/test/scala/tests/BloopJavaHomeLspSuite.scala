acdfcb2640ef26a0775be2c94b9f4a2a42f8a67d
Katarzyna Marek <kasia@marek.net>
2024-04-22 09:28:14 +0200

    improvement: don't override bloop java home if version greater or equal to metals java version

--- tests/unit/src/test/scala/tests/BloopJavaHomeLspSuite.scala
+++ tests/unit/src/test/scala/tests/BloopJavaHomeLspSuite.scala
@@ -4,6 +4,8 @@ import scala.meta.internal.metals.BloopServers
 import scala.meta.internal.metals.MetalsEnrichments._
 import scala.meta.io.AbsolutePath
 
+import coursierapi.JvmManager
+
 class BloopJavaHomeLspSuite extends BaseLspSuite("java-home") {
 
   val jsonFilePath: Option[AbsolutePath] =
@@ -12,6 +14,7 @@ class BloopJavaHomeLspSuite extends BaseLspSuite("java-home") {
   val contents: Option[String] = jsonFile.map(_.readText)
 
   val javaHome: String = sys.props.get("java.home").getOrElse("")
+  val pathToJava21: String = JvmManager.create().get("21").toString()
 
   def stashBloopJson(): Unit = {
     jsonFile.foreach(_.delete())
@@ -75,6 +78,47 @@ class BloopJavaHomeLspSuite extends BaseLspSuite("java-home") {
         ujson.Obj
           .apply("javaHome" -> javaHome)
           .toString(),
+      )
+    } yield ()
+  }
+
+  test("correct-home") {
+    cleanWorkspace()
+    val bloopJsonContent =
+      ujson.Obj
+        .apply("javaHome" -> pathToJava21)
+        .toString()
+
+    jsonFilePath.foreach(_.writeText(bloopJsonContent))
+    for {
+      _ <- initialize(
+        """/metals.json
+          |{
+          |  "a": {}
+          |}
+          |/a/src/main/scala/a/A.scala
+          |package a
+          |object A { val x : String = 1 }
+          |""".stripMargin
+      )
+      _ <- server.didOpen("a/src/main/scala/a/A.scala")
+      _ = assertNoDiff(
+        client.workspaceDiagnostics,
+        """|a/src/main/scala/a/A.scala:2:29: error: type mismatch;
+           | found   : Int(1)
+           | required: String
+           |object A { val x : String = 1 }
+           |                            ^
+           |""".stripMargin,
+      )
+      bloopJsonText = jsonFilePath.map(_.readText).getOrElse("")
+      _ = assert(
+        server.client.messageRequests.isEmpty(),
+        "No message should show up",
+      )
+      _ = assertNoDiff(
+        bloopJsonText,
+        bloopJsonContent,
       )
     } yield ()
   }
