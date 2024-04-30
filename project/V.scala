cdd60ab18421df0dcd56e3d1786fb69493d5e3a0
Tomasz Godzik <tgodzik@virtuslab.com>
2024-04-30 11:18:09 +0200

    chore: Add support for Scala 2.13.14

--- project/V.scala
+++ project/V.scala
@@ -4,7 +4,7 @@ object V {
   val scala210 = "2.10.7"
   val scala211 = "2.11.12"
   val scala212 = "2.12.19"
-  val scala213 = "2.13.13"
+  val scala213 = "2.13.14"
   val scala3 = "3.3.3"
   val firstScala3PCVersion = "3.3.4"
   val wrapperMetalsVersion = "3.4.0-RC1-bin-20231127-41e7d95-NIGHTLY"
@@ -80,10 +80,10 @@ object V {
     "2.12.13",
     "2.12.14",
     "2.12.15",
-    "2.13.6",
     "2.13.7",
     "2.13.8",
     "2.13.9",
+    "2.13.10",
   )
 
   def nonDeprecatedScala2Versions = Seq(
@@ -92,9 +92,9 @@ object V {
     "2.12.18",
     "2.12.17",
     "2.12.16",
-    "2.13.10",
     "2.13.11",
     "2.13.12",
+    "2.13.13",
   )
 
   def minimumSupportedSbtVersion = {
