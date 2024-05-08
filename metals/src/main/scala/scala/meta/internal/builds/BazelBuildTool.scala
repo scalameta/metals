addef6d5b4ffff2b006a37a511f78b5fb8ca2e20
Tomasz Godzik <tgodzik@virtuslab.com>
2024-05-08 12:06:45 +0200

    chore: Bump Bazel BSP to nightly with recent fixes

--- metals/src/main/scala/scala/meta/internal/builds/BazelBuildTool.scala
+++ metals/src/main/scala/scala/meta/internal/builds/BazelBuildTool.scala
@@ -65,7 +65,7 @@ case class BazelBuildTool(
 object BazelBuildTool {
   val name: String = "bazel"
   val bspName: String = "bazelbsp"
-  val version: String = "3.1.0-20240328-e62f321-NIGHTLY"
+  val version: String = "3.2.0-20240508-f3a81e7-NIGHTLY"
 
   val mainClass = "org.jetbrains.bsp.bazel.install.Install"
 
