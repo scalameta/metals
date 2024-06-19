43dd377d64b1c1adc20941a66bffff8ca473d644
Tomasz Godzik <tgodzik@virtuslab.com>
2024-06-19 17:45:10 +0200

    chore: Update mdoc to 2.5.4 to fix reproted issues

--- project/V.scala
+++ project/V.scala
@@ -36,7 +36,7 @@ object V {
   val lsp4jV = "0.23.1"
   val mavenBloop = "2.0.0"
   val mill = "0.11.8"
-  val mdoc = "2.5.2"
+  val mdoc = "2.5.4"
   val munit = "1.0.0"
   val pprint = "0.7.3"
   val sbtBloop = bloop
