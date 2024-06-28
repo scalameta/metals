e44488767a1a80859f24c3c24e960a94d42875c4
Tomasz Godzik <tgodzik@virtuslab.com>
2024-06-28 12:01:58 +0200

    improvement: Don't query database on each format

--- metals/src/main/scala/scala/meta/internal/metals/ChosenBuildTool.scala
+++ metals/src/main/scala/scala/meta/internal/metals/ChosenBuildTool.scala
@@ -1,19 +1,34 @@
 package scala.meta.internal.metals
 
 import java.sql.Connection
+import java.util.concurrent.atomic.AtomicReference
 
 import scala.meta.internal.metals.JdbcEnrichments._
 
 class ChosenBuildTool(conn: () => Connection) {
+
+  private val currentTool: AtomicReference[Option[String]] =
+    new AtomicReference(
+      None
+    )
+
   def selectedBuildTool(): Option[String] = {
-    conn()
-      .query(
-        "select * from chosen_build_tool LIMIT 1;"
-      )(_ => ()) { _.getString("build_tool") }
-      .headOption
+    val selected = currentTool
+      .get()
+      .orElse(
+        conn()
+          .query(
+            "select * from chosen_build_tool LIMIT 1;"
+          )(_ => ()) { _.getString("build_tool") }
+          .headOption
+      )
+    selected.foreach(toolName => currentTool.set(Some(toolName)))
+    selected
   }
+
   def chooseBuildTool(buildTool: String): Int = synchronized {
     reset()
+    currentTool.set(Some(buildTool))
     conn().update {
       "insert into chosen_build_tool values (?);"
     } { stmt => stmt.setString(1, buildTool) }
