beb33a73170fef99f94febc23272594d57e38d33
Katarzyna Marek <kasia@marek.net>
2024-07-02 15:07:26 +0200

    fix: change project ref setting to support VSCode workspace folder reload

--- metals/src/main/scala/scala/meta/internal/metals/DelegateSetting.scala
+++ metals/src/main/scala/scala/meta/internal/metals/DelegateSetting.scala
@@ -1,28 +1,51 @@
 package scala.meta.internal.metals
 
+import scala.util.Try
+
 import scala.meta.internal.metals.MetalsEnrichments._
 import scala.meta.io.AbsolutePath
 
+import ujson.Value
+
 object DelegateSetting {
-  private val settingName = "delegate"
+  val delegateSetting = "delegate"
+  private val projectRefSetting = "project-ref"
 
-  def writeDeleteSetting(
+  def writeProjectRef(
       folder: AbsolutePath,
-      servicePath: AbsolutePath,
+      projectRefs: List[AbsolutePath],
   ): Unit = {
-    val relPath = servicePath.toRelative(folder)
-    val jsonText = ujson.Obj(settingName -> relPath.toString()).toString()
+    val relPath = projectRefs.map(_.toRelative(folder))
+    val jsonText =
+      ujson.Obj(projectRefSetting -> relPath.map(_.toString())).toString()
     folder.resolve(Directories.metalsSettings).writeText(jsonText)
-
   }
 
-  def readDeleteSetting(folder: AbsolutePath): Option[AbsolutePath] =
+  def readProjectRefs(folder: AbsolutePath): List[AbsolutePath] = {
     for {
-      text <- folder.resolve(Directories.metalsSettings).readTextOpt
-      json = ujson.read(text)
-      relPath <- json(settingName).strOpt
-      path = folder.resolve(relPath).dealias
+      setting <- getSetting(folder, projectRefSetting).toList
+      ref <- setting.arrOpt.getOrElse(Nil)
+      pathStr <- ref.strOpt
+      path = folder.resolve(pathStr).dealias
       if path.exists
     } yield path
+  }
+
+  def readDeleteSetting(root: AbsolutePath): Option[AbsolutePath] =
+    for {
+      setting <- getSetting(root, delegateSetting)
+      relPath <- setting.strOpt
+      path = root.resolve(relPath).dealias
+      if path.exists
+    } yield path
+
+  private def getSetting(
+      root: AbsolutePath,
+      settingName: String,
+  ): Option[Value] =
+    for {
+      text <- root.resolve(Directories.metalsSettings).readTextOpt
+      setting <- Try(ujson.read(text)(settingName)).toOption
+    } yield setting
 
 }
