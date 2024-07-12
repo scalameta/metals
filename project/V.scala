8a8078c237564cec6579d758b2f9f9b4b01eb17a
Tomasz Godzik <tgodzik@virtuslab.com>
2024-07-12 16:44:12 +0200

    chore: Add support for 3.3.4-RC1

--- project/V.scala
+++ project/V.scala
@@ -8,7 +8,7 @@ object V {
   val scala3 = "3.3.3"
 
   // When you can add to removedScalaVersions in MtagsResolver.scala with the last released version
-  val scala3RC: Option[String] = None
+  val scala3RC: Option[String] = Some("3.3.4-RC1")
   val sbtScala = "2.12.18"
   val ammonite212Version = "2.12.19"
   val ammonite213Version = "2.13.14"
