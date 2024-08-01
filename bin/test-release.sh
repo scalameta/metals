6136010f0d2ea041d323c5df9b5d48c03196c6d6
Tomasz Godzik <tgodzik@virtuslab.com>
2024-08-01 12:40:59 +0200

    chore: Add release notes for Metals 1.3.5
    I want to get the release out since it looks like betasty is breaking code for some people.

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -8,21 +8,12 @@ suffix=${argumentsRest:-}
 coursier fetch \
   org.scalameta:metals_2.13:$version \
   org.scalameta:mtags_3.2.2:$version \
-  org.scalameta:mtags_3.3.0:$version \
-  org.scalameta:mtags_3.3.1:$version \
+  org.scalameta:mtags_3.3.2:$version \
   org.scalameta:mtags_3.3.3:$version \
-  org.scalameta:mtags_2.13.7:$version \
-  org.scalameta:mtags_2.13.8:$version \
-  org.scalameta:mtags_2.13.9:$version \
-  org.scalameta:mtags_2.13.10:$version \
   org.scalameta:mtags_2.13.11:$version \
   org.scalameta:mtags_2.13.12:$version \
   org.scalameta:mtags_2.13.13:$version \
   org.scalameta:mtags_2.13.14:$version \
-  org.scalameta:mtags_2.12.12:$version \
-  org.scalameta:mtags_2.12.13:$version \
-  org.scalameta:mtags_2.12.14:$version \
-  org.scalameta:mtags_2.12.15:$version \
   org.scalameta:mtags_2.12.16:$version \
   org.scalameta:mtags_2.12.17:$version \
   org.scalameta:mtags_2.12.18:$version \
