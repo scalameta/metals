655724031994b7b37b6da160ed7ca6e7713faaaf
Yarosλaβ . <untainsyd@gmail.com>
2024-09-27 01:26:03 +0300

    bugfix: Metals SqlSharedIndices.scala XDG path violation

--- metals/src/main/scala/scala/meta/internal/metals/SqlSharedIndices.scala
+++ metals/src/main/scala/scala/meta/internal/metals/SqlSharedIndices.scala
@@ -2,12 +2,25 @@ package scala.meta.internal.metals
 
 import java.nio.file.Paths
 
+import scala.util.Properties
+
 import scala.meta.io.AbsolutePath
+
+object MetalsDirectories {
+  def getMetalsDirectory: AbsolutePath = {
+    if (Properties.isLinux) {
+      Option(System.getenv("XDG_DATA_HOME"))
+        .map(xdg => Paths.get(xdg, "metals"))
+        .getOrElse(Paths.get(sys.props("user.home"), ".local", "share", "metals"))
+    } else {
+      Paths.get(sys.props("user.home"), ".metals")
+    }
+  }.toAbsolutePath
+}
 
 class SqlSharedIndices
     extends H2ConnectionProvider(
-      directory =
-        AbsolutePath(Paths.get(sys.props("user.home"))).resolve(".metals"),
+      directory = MetalsDirectories.getMetalsDirectory,
       name = "metals-shared",
       migrations = "/shared-db/migration",
     ) {
