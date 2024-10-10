6160121ea47178f9c7cdd285d5b7e5935db29c53
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2024-10-10 21:42:14 +0200

    Stop considering all mill files Ammonite scripts

--- metals/src/main/scala/scala/meta/internal/parsing/Trees.scala
+++ metals/src/main/scala/scala/meta/internal/parsing/Trees.scala
@@ -174,7 +174,7 @@ final class Trees(
       val skipFistShebang =
         if (text.startsWith("#!")) text.replaceFirst("#!", "//") else text
       val input = Input.VirtualFile(path.toURI.toString(), skipFistShebang)
-      if (path.isAmmoniteScript || path.isMill) {
+      if (path.isAmmoniteScript) {
         val ammoniteInput = Input.Ammonite(input)
         ammoniteInput.safeParse[MultiSource](dialect)
       } else {
