aba8be848a25233e9505c8413f65d187f095d750
Tomasz Godzik <tgodzik@virtuslab.com>
2025-03-22 18:49:52 +0100

    improvement: Add scalafix and Scala 3 PC to DownloadDependencies
    Also add an option to copy those jars

--- metals/src/main/scala/scala/meta/metals/DownloadDependencies.scala
+++ metals/src/main/scala/scala/meta/metals/DownloadDependencies.scala
@@ -1,7 +1,9 @@
 package scala.meta.metals
 
+import java.net.URI
 import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.Paths
 
 import scala.meta.internal.builds.BazelBuildTool
 import scala.meta.internal.metals.BloopServers
@@ -10,13 +12,23 @@ import scala.meta.internal.metals.Embedded
 import scala.meta.internal.metals.FormattingProvider
 import scala.meta.internal.metals.ScalaVersions
 import scala.meta.internal.metals.logging.MetalsLogger
+import scala.meta.internal.mtags.CoursierComplete
+import scala.meta.io.AbsolutePath
 
+import coursier.LocalRepositories
+import coursier.paths.CoursierPaths
 import coursierapi.Dependency
+import scalafix.interfaces.Scalafix
 
 object DownloadDependencies {
 
   private val metalsBinaryVersion =
     ScalaVersions.scalaBinaryVersionFromFullVersion(BuildInfo.scala213)
+
+  private val complete = new CoursierComplete("3")
+  private lazy val allSupportedScala3Versions = complete
+    .complete("org.scala-lang:scala3-presentation-compiler_3:")
+    .filterNot(_.contains("RC"))
 
   /**
    * A main class that populates the Coursier download cache with Metals dependencies.
@@ -30,17 +42,79 @@ object DownloadDependencies {
    * @param args ignored.
    */
   def main(args: Array[String]): Unit = {
+
+    val filterVersions: String => Boolean =
+      args.indexOf("--scala-versions") match {
+        case -1 => (_: String) => true
+        case index =>
+          val versions =
+            args.drop(index + 1).takeWhile(!_.startsWith("-")).toSet
+          println("Filtering versions: " + versions.mkString(", "))
+          (s: String) => versions(s)
+      }
     MetalsLogger.updateDefaultFormat()
-    val allPaths = downloadMdoc() ++
+    val allPaths = downloadMdoc(filterVersions) ++
       downloadScalafmt() ++
-      downloadMtags() ++
-      downloadSemanticDBScalac() ++
+      downloadMtags(filterVersions) ++
+      downloadSemanticDBScalac(filterVersions) ++
       downloadSemanticDBJavac() ++
       downloadScala() ++
       downloadBloop() ++
-      downloadBazelBsp()
+      downloadBazelBsp() ++
+      downloadScala3PresentationCompiler(filterVersions) ++
+      downloadAllScalafixVersions(filterVersions)
 
-    allPaths.distinct.foreach(println)
+    val distinctFiles = allPaths.distinct
+    val copyToDest = args.indexOf("--copy-to") match {
+      case -1 => None
+      case index =>
+        Some(args(index + 1))
+    }
+
+    copyToDest.foreach { path =>
+      copyToDestination(distinctFiles, path)
+    }
+    distinctFiles.foreach(println)
+  }
+
+  def copyToDestination(
+      distinctFiles: Seq[Path],
+      destinationString: String,
+  ): Unit = {
+    val destinationPath = AbsolutePath(
+      Paths.get(destinationString).toAbsolutePath()
+    )
+    val ivyDirectoryPath = LocalRepositories.ivy2Local.pattern.chunks
+      .takeWhile {
+        case _: coursier.ivy.Pattern.Chunk.Const => true
+        case _ => false
+      }
+      .map(_.string)
+      .mkString
+
+    val coursierCacheLocation = AbsolutePath(
+      CoursierPaths.cacheDirectory().toPath()
+    )
+    val ivyDirectory = AbsolutePath(Paths.get(URI.create(ivyDirectoryPath)))
+    println(s"Copying artifacts to ${destinationPath}")
+    distinctFiles.foreach { p =>
+      val relative =
+        if (p.startsWith(coursierCacheLocation.toNIO))
+          AbsolutePath(p).toRelative(coursierCacheLocation)
+        else if (p.startsWith(ivyDirectory.toNIO))
+          AbsolutePath(p).toRelative(ivyDirectory)
+        else throw new Exception(s"Unexpected cache path: $p")
+      val target = destinationPath.resolve(relative)
+      if (!Files.exists(target.toNIO.getParent))
+        Files.createDirectories(target.toNIO.getParent)
+      if (Files.exists(target.toNIO)) {
+        println(s"Skipping $p, already exists at $target")
+      } else {
+        println(s"Copied $p to $target")
+        Files.copy(p, target.toNIO)
+      }
+    }
+
   }
 
   def downloadScala(): Seq[Path] = {
@@ -50,13 +124,14 @@ object DownloadDependencies {
     }
   }
 
-  def downloadMdoc(): Seq[Path] = {
+  def downloadMdoc(filterVersions: String => Boolean): Seq[Path] = {
     scribe.info("Downloading mdoc")
-    BuildInfo.supportedScala2Versions.flatMap { scalaVersion =>
-      Embedded.downloadMdoc(
-        ScalaVersions.scalaBinaryVersionFromFullVersion(scalaVersion),
-        scalaVersion = None,
-      )
+    BuildInfo.supportedScala2Versions.filter(filterVersions).flatMap {
+      scalaVersion =>
+        Embedded.downloadMdoc(
+          ScalaVersions.scalaBinaryVersionFromFullVersion(scalaVersion),
+          scalaVersion = None,
+        )
     }
   }
 
@@ -83,17 +158,30 @@ object DownloadDependencies {
     )
   }
 
-  def downloadMtags(): Seq[Path] = {
+  def downloadMtags(filterVersions: String => Boolean): Seq[Path] = {
     scribe.info("Downloading mtags")
-    BuildInfo.supportedScalaVersions.flatMap { scalaVersion =>
-      Embedded.downloadMtags(scalaVersion, BuildInfo.metalsVersion)
+    BuildInfo.supportedScalaVersions.filter(filterVersions).flatMap {
+      scalaVersion =>
+        Embedded.downloadMtags(scalaVersion, BuildInfo.metalsVersion)
     }
   }
 
-  def downloadSemanticDBScalac(): Seq[Path] = {
+  def downloadScala3PresentationCompiler(
+      filterVersions: String => Boolean
+  ): Seq[Path] = {
+    scribe.info("Downloading Scala 3 presentation compiler")
+    allSupportedScala3Versions.filter(filterVersions).flatMap { scalaVersion =>
+      Embedded.downloadScala3PresentationCompiler(
+        scalaVersion
+      ) ++ Embedded.downloadScala3Sources(scalaVersion)
+    }
+  }
+
+  def downloadSemanticDBScalac(filterVersions: String => Boolean): Seq[Path] = {
     scribe.info("Downloading semanticdb-scalac")
-    BuildInfo.supportedScala2Versions.flatMap { scalaVersion =>
-      Embedded.downloadSemanticdbScalac(scalaVersion)
+    BuildInfo.supportedScala2Versions.filter(filterVersions).flatMap {
+      scalaVersion =>
+        Embedded.downloadSemanticdbScalac(scalaVersion)
     }
   }
 
@@ -108,6 +196,34 @@ object DownloadDependencies {
       BazelBuildTool.dependency,
       None,
     )
+  }
+
+  def downloadAllScalafixVersions(
+      filterVersions: String => Boolean
+  ): Seq[Path] = {
+    scribe.info("Downloading all Scalafix versions")
+    val allScalaVersions =
+      (allSupportedScala3Versions ++ BuildInfo.supportedScala2Versions).filter(
+        filterVersions
+      )
+    val allToDownload = allScalaVersions
+      .map(_.split('.').take(2).mkString("."))
+      .distinct
+      .filter(_ != "2.11")
+    allToDownload.flatMap(downloadScalafix)
+  }
+
+  def downloadScalafix(scalaMinorVersion: String): Array[Path] = {
+    scribe.info(s"Downloading Scalafix for $scalaMinorVersion")
+    Scalafix
+      .fetchAndClassloadInstance(scalaMinorVersion)
+      .getClass()
+      .getClassLoader() match {
+      case cl: java.net.URLClassLoader =>
+        cl.getURLs().map(_.toURI()).map(Paths.get(_))
+      case cl =>
+        throw new Exception(s"Unexpected classloader: $cl")
+    }
   }
 
   def downloadBloop(): Seq[Path] = {
