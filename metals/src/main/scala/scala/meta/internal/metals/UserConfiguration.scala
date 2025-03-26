657d7328deae7d818cc97c89bdb688fa82d42344
ajafri2001 <abdullahjafri2001@gmail.com>
2025-03-27 01:02:54 +0530

    remove log statements and add comments

--- metals/src/main/scala/scala/meta/internal/metals/UserConfiguration.scala
+++ metals/src/main/scala/scala/meta/internal/metals/UserConfiguration.scala
@@ -686,9 +686,6 @@ object UserConfiguration {
       getStringKey("bloop-version")
     val defaultShell =
       getStringKey("default-shell")
-    println(
-      s"Hello from UserConfiguration.scala line:654 printing defaultShell => ${defaultShell}"
-    )
     val bloopJvmProperties = getStringListKey("bloop-jvm-properties")
     val superMethodLensesEnabled =
       getBooleanKey("super-method-lenses-enabled").getOrElse(false)
