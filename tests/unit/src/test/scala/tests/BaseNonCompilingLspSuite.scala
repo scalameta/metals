7d56e0d16a6968c6dff42476ca148bcec575ee62
amit.miran <amit.miran@unit.co>
2025-04-23 13:49:47 +0300

    tests and scalafix
    (cherry picked from commit 697a99265cbec8e0b6888f1966a4a9b9521006bf)

--- tests/unit/src/test/scala/tests/BaseNonCompilingLspSuite.scala
+++ tests/unit/src/test/scala/tests/BaseNonCompilingLspSuite.scala
@@ -5,6 +5,7 @@ import scala.concurrent.Future
 import scala.meta.internal.metals.codeactions.CreateNewSymbol
 import scala.meta.internal.metals.codeactions.ImportMissingSymbol
 import scala.meta.internal.metals.codeactions.ImportMissingSymbolQuickFix
+import scala.meta.internal.metals.codeactions.SourceAddMissingImports
 
 abstract class BaseNonCompilingLspSuite(name: String)
     extends BaseCompletionLspSuite(name) {
@@ -116,6 +117,7 @@ abstract class BaseNonCompilingLspSuite(name: String)
             "a/src/main/scala/a/A.scala",
             newText,
             s"""|${ImportMissingSymbol.title("UniqueObjectOther", "b")}
+                |${SourceAddMissingImports.title}
                 |${CreateNewSymbol.title("UniqueObjectOther")}
                 |""".stripMargin,
             kind = List(ImportMissingSymbolQuickFix.kind),
