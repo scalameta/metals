d8a0d3d82c24eeec846fca020d0ce276ad5c5942
Alec Theriault <alec.theriault@gmail.com>
2025-05-20 14:48:45 -0400

    fix: indexer bug around type members (#7489)
    When a `type A` member was immediately followed by a closing brace
    (closing a surrounding scope), the indexer would swallow the closing
    brace while scanning the member. This would cause the rest of the file
    to be interpreted as though that closing brace was missing.
    
    Fixes #7475

---
+++ tests/unit/src/test/resources/documentSymbol/example/TypeMembers.scala
@@ -0,0 +1,5 @@
+/*example(Package):5*/package example
+
+/*example.TypeMembers(Interface):3*/trait TypeMembers { /*example.TypeMembers#TypeMember(TypeParameter):3*/type TypeMember }
+
+/*example.AfterTrait(Module):5*/object AfterTrait
