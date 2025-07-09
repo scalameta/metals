ac617ee057b5e1de68be52182267a203091a1295
ajafri2001 <abdullahjafri2001@gmail.com>
2025-07-09 19:53:33 +0530

    Wip commit

--- metals/src/main/scala/scala/meta/internal/metals/TwirlAdjustments.scala
+++ metals/src/main/scala/scala/meta/internal/metals/TwirlAdjustments.scala
@@ -4,6 +4,7 @@ import java.io.File
 import scala.meta.inputs.Input.VirtualFile
 import scala.io.Source
 import org.eclipse.lsp4j.Position
+import scala.collection.immutable.ListMap
 
 object TwirlAdjustments {
 
@@ -13,6 +14,10 @@ object TwirlAdjustments {
   ): (VirtualFile, Position => Position, AdjustLspData) = {
 
     val base = rawScalaVersion.split('-')(0)
+
+    type AnchorLength = Int
+
+    val originalTwirl = twirlFile.value
 
     val scalaVersion = base match {
       case v if v.startsWith("2") => v.split('.').take(2).mkString(".")
@@ -27,60 +32,99 @@ object TwirlAdjustments {
       )
       .replace(".scala.html", ".template.scala")
 
-    println(compiledTwirlPath)
-
     val compiledTwirl = Source.fromFile(new File(compiledTwirlPath)).mkString
 
     val newVirtualFile = twirlFile.copy(value = compiledTwirl)
 
+    val anchorRegex = raw"/\*\d+\.\d+\*/".r
     val pattern = """(\d+)->(\d+)""".r
     val number_matching =
       pattern.findAllIn(compiledTwirl).toList
 
+    val lengthOfAnchors: Iterator[Int] =
+      anchorRegex.findAllIn(compiledTwirl).map(_.length)
+
     val chars = number_matching.take(number_matching.length / 2)
     val lines = number_matching.drop(number_matching.length / 2)
 
-    // Map[Int, Int](1 -> 15, 2 -> 20, 3 -> 21)  SourceFile -> CompiledFile
-    val lineMap: Map[Int, Int] = lines.map { s =>
-      val parts = s.split("->")
-      val a = parts(0).toInt
-      val b = parts(1).toInt
-      b - 1 -> a
-    }.toMap
+    def getCharAtPosition(pos: Position): Char = {
+      val lines = compiledTwirl.split("\n", -1) // preserve trailing empty lines
+      val line = pos.getLine()
+      val char = pos.getCharacter()
 
-    val charMap: Map[Int, Int] = chars.map { s =>
-      val parts = s.split("->")
-      val a = parts(0).toInt
-      val b = parts(1).toInt
-      b -> a
-    }.toMap
+      lines(line).charAt(char)
+    }
 
-    def findCharPosition(char_pos: Int): Int = {
+    def findCharPosition(char_pos: Int, sourceFile: String): Int = {
       var index = 0
       var pos = 0
-      while (index <= char_pos) {
-        if (compiledTwirl(index) == '\n') {
-          pos = 0
-        }
+      while (index < char_pos) {
         index += 1
         pos += 1
+        if (sourceFile(index) == '\n') {
+          pos = 0
+        }
       }
       pos
     }
 
+    // Map[Int, Int](1 -> 15, 2 -> 20, 3 -> 21)  SourceFile -> CompiledFile
+    val lineMap: ListMap[Int, Int] = ListMap.from {
+      lines.map { s =>
+        val parts = s.split("->")
+        val a = parts(0).toInt
+        val b = parts(1).toInt
+        (b - 1) -> (a - 1)
+      }
+    }
+
+    val charMap: ListMap[Int, (Int, AnchorLength)] = ListMap.from {
+      chars.map { s =>
+        val parts = s.split("->")
+        val a = findCharPosition(parts(0).toInt, compiledTwirl)
+        val b = findCharPosition(parts(1).toInt, originalTwirl)
+        b -> (a - 1, lengthOfAnchors.next)
+      }
+    }
+
+    println(charMap)
+
+    def positionMapping(pos: Position): Position = {
+      val charPos = pos.getCharacter()
+      val linePos = pos.getLine()
+
+      val newPosition =
+        new Position(
+          lineMap(linePos),
+          charMap(charPos)._1,
+        )
+
+      pprint.log("Old Position => " + pos + "\n New Position => " + newPosition)
+
+      pprint.log(getCharAtPosition(newPosition))
+
+      println(compiledTwirl(charMap(charPos)._1))
+
+      newPosition
+    }
+
+    def adjustLSPMapping(pos: Position): Position = {
+      val charPos = pos.getCharacter()
+      val linePos = pos.getLine()
+
+      val adjustedPosition =
+        // TODO - This needs to be changed from here
+        new Position(
+          lineMap(linePos),
+          charMap(charPos)._1,
+        )
+      adjustedPosition
+    }
+
     (
       newVirtualFile,
-      pos =>
-        new Position(
-          lineMap(pos.getLine()) - 1,
-          findCharPosition(charMap(pos.getCharacter())),
-        ),
-      AdjustedLspData.create(pos =>
-        new Position(
-          lineMap(pos.getLine()) - 1,
-          findCharPosition(charMap(pos.getCharacter())),
-        )
-      ),
+      positionMapping,
+      AdjustedLspData.create(adjustLSPMapping),
     )
   }
 }
