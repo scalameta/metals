66f515b7d239b7b5af3c5e2e1e6d1db48f4b8f2d
Chris Kipp <ckipp@pm.me>
2025-07-25 17:44:03 +0200

    feat: introduce a format-file MCP tool
    The specific use-case I have for this is that when using claude code
    you're allowed to define tools as hooks. So when you make edits to a
    file you can run a hook after it. Often this is something like
    formatting. While we can call scalafmt directly I thought it'd be nice
    to just add this as another tool as everything is mostly there already.

--- metals/src/main/scala/scala/meta/internal/metals/mcp/MetalsMcpServer.scala
+++ metals/src/main/scala/scala/meta/internal/metals/mcp/MetalsMcpServer.scala
@@ -19,6 +19,7 @@ import scala.meta.internal.metals.Cancelable
 import scala.meta.internal.metals.Compilations
 import scala.meta.internal.metals.ConnectionProvider
 import scala.meta.internal.metals.Diagnostics
+import scala.meta.internal.metals.FormattingProvider
 import scala.meta.internal.metals.JsonParser.XtensionSerializableToJson
 import scala.meta.internal.metals.MetalsEnrichments._
 import scala.meta.internal.metals.MutableCancelable
@@ -66,6 +67,7 @@ class MetalsMcpServer(
     languageClient: LanguageClient,
     connectionProvider: ConnectionProvider,
     scalaVersionSelector: ScalaVersionSelector,
+    formattingProvider: FormattingProvider,
 )(implicit
     ec: ExecutionContext
 ) extends Cancelable {
@@ -123,6 +125,7 @@ class MetalsMcpServer(
     asyncServer.addTool(importBuildTool()).subscribe()
     asyncServer.addTool(createFindDepTool()).subscribe()
     asyncServer.addTool(createListModulesTool()).subscribe()
+    asyncServer.addTool(createFormatTool()).subscribe()
 
     // Log server initialization
     asyncServer.loggingNotification(
@@ -776,6 +779,67 @@ class MetalsMcpServer(
             false,
           )
         }.toMono
+      },
+    )
+  }
+
+  private def createFormatTool(): AsyncToolSpecification = {
+    val schema =
+      """|{
+         |  "type": "object",
+         |  "properties": {
+         |    "fileInFocus": {
+         |      "type": "string",
+         |      "description": "The file to format, if empty we will try to detect file in focus"
+         |    }
+         |  }
+         |}""".stripMargin
+    new AsyncToolSpecification(
+      new Tool(
+        "format-file",
+        "Format a Scala file and return the formatted text",
+        schema,
+      ),
+      withErrorHandling { (_, arguments) =>
+        val path = arguments.getFileInFocus
+        if (path.exists && path.isScalaFilename) {
+          val cancelChecker = new org.eclipse.lsp4j.jsonrpc.CancelChecker {
+            override def isCanceled(): Boolean = false
+            override def checkCanceled(): Unit = ()
+          }
+
+          formattingProvider
+            .formatForMcp(path, projectPath, cancelChecker)
+            .map {
+              case Left(errorMessage) =>
+                new CallToolResult(
+                  createContent(errorMessage),
+                  true,
+                )
+              case Right(None) =>
+                new CallToolResult(
+                  createContent("File is already properly formatted."),
+                  false,
+                )
+              case Right(Some(formattedText)) =>
+                new CallToolResult(
+                  createContent(formattedText),
+                  false,
+                )
+            }
+            .toMono
+        } else {
+          Future
+            .successful(
+              new CallToolResult(
+                createContent(
+                  s"Error: File not found or not a Scala file: $path"
+                ),
+                true,
+              )
+            )
+            .toMono
+        }
       },
     )
   }
