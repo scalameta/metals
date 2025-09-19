6b3debc77e9bef47d1bb77bb2eeb877a7e3afb06
warcholjakub <warcholljakub@gmail.com>
2025-09-19 14:44:20 +0200

    fix: reorganize imports

--- metals/src/main/scala/scala/meta/internal/metals/WorkspaceSymbolProvider.scala
+++ metals/src/main/scala/scala/meta/internal/metals/WorkspaceSymbolProvider.scala
@@ -9,9 +9,9 @@ import scala.util.control.NonFatal
 
 import scala.meta.internal.metals.MetalsEnrichments._
 import scala.meta.internal.mtags.GlobalSymbolIndex
-import scala.meta.internal.semanticdb.SymbolInformation
 import scala.meta.internal.mtags.ToplevelMember
 import scala.meta.internal.pc.InterruptException
+import scala.meta.internal.semanticdb.SymbolInformation
 import scala.meta.io.AbsolutePath
 import scala.meta.pc.CancelToken
 import scala.meta.pc.SymbolSearch
