e665821a81fbe2dfab26ce0e50826b30eb3a8936
Tomasz Godzik <tgodzik@users.noreply.github.com>
2025-10-03 14:58:55 +0200

    bugfix: Don't use bsp version as Bazel fallback version (#7817)

--- metals/src/main/scala/scala/meta/internal/builds/BazelBuildTool.scala
+++ metals/src/main/scala/scala/meta/internal/builds/BazelBuildTool.scala
@@ -72,14 +72,15 @@ object BazelBuildTool {
   val name: String = "bazel"
   val bspName: String = "bazelbsp"
   val bspVersion: String = "4.0.1"
+  val defaultBazelVersion = "8.2.1"
 
   def resolveBazelVersion(projectRoot: AbsolutePath): String = {
     val bazelVersionFile = projectRoot.resolve(".bazelversion")
     if (bazelVersionFile.exists && bazelVersionFile.isFile) {
       val version = bazelVersionFile.readText.trim()
-      if (version.nonEmpty) version else bspVersion
+      if (version.nonEmpty) version else defaultBazelVersion
     } else {
-      bspVersion
+      defaultBazelVersion
     }
   }
 
