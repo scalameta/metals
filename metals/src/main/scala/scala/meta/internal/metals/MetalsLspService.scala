2bb574ed6d4f07ced14c3dbb46692b93714a2710
Mateusz Piękos <mateuszpiekos@gmail.com>
2025-10-07 12:57:11 +0200

    Added afterCompilationFinished to debugDiscovery and runClosest

--- metals/src/main/scala/scala/meta/internal/metals/MetalsLspService.scala
+++ metals/src/main/scala/scala/meta/internal/metals/MetalsLspService.scala
@@ -1511,14 +1511,34 @@ abstract class MetalsLspService(
   )
   buildClient.registerLogForwarder(debugProvider)
 
+  private def afterCompilationFinished[T](
+      params: DebugDiscoveryParams
+  )(action: DebugDiscoveryParams => Future[T]): Future[T] = {
+    val path = Option(params.path).map(_.toAbsolutePath)
+    val buildTarget = path
+      .flatMap(buildTargets.inverseSources(_))
+      .orElse {
+        Option(params.buildTarget)
+          .flatMap(buildTargets.findByDisplayName)
+          .map(_.getId())
+      }
+
+    buildTarget match {
+      case Some(target) =>
+        compilations
+          .compilationFinished(Seq(target), compileInverseDependencies = false)
+          .flatMap(_ => action(params))
+      case None =>
+        action(params)
+    }
+  }
+
   def debugDiscovery(params: DebugDiscoveryParams): Future[DebugSession] =
-    debugDiscovery
-      .debugDiscovery(params)
+    afterCompilationFinished(params)(debugDiscovery.debugDiscovery)
       .flatMap(debugProvider.asSession)
 
   def runClosest(params: DebugDiscoveryParams): Future[DebugSession] =
-    debugDiscovery
-      .debugDiscovery(params)
+    afterCompilationFinished(params)(debugDiscovery.debugDiscovery)
       .flatMap(debugProvider.asSession)
 
   def createDebugSession(
@@ -1550,25 +1570,8 @@ abstract class MetalsLspService(
 
   def discoverMainClasses(
       unresolvedParams: DebugDiscoveryParams
-  ): Future[b.DebugSessionParams] = {
-    val path = Option(unresolvedParams.path).map(_.toAbsolutePath)
-    val buildTarget = path
-      .flatMap(buildTargets.inverseSources(_))
-      .orElse {
-        Option(unresolvedParams.buildTarget)
-          .flatMap(buildTargets.findByDisplayName)
-          .map(_.getId())
-      }
-
-    buildTarget match {
-      case Some(target) =>
-        compilations
-          .compilationFinished(Seq(target), compileInverseDependencies = false)
-          .flatMap(_ => debugDiscovery.runCommandDiscovery(unresolvedParams))
-      case None =>
-        debugDiscovery.runCommandDiscovery(unresolvedParams)
-    }
-  }
+  ): Future[b.DebugSessionParams] =
+    afterCompilationFinished(unresolvedParams)(debugDiscovery.runCommandDiscovery)
 
   def supportsBuildTarget(
       target: b.BuildTargetIdentifier
