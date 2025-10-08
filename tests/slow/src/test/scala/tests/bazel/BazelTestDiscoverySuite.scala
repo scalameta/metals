e48a47a73cfd335b6556676dccdfac5e1464cd28
Mateusz Piękos <mateuszpiekos@gmail.com>
2025-10-08 09:26:08 +0200

    trying just single test in BazelTestDiscoverySuite

---
+++ tests/slow/src/test/scala/tests/bazel/BazelTestDiscoverySuite.scala
@@ -0,0 +1,94 @@
+package tests.bazel
+
+import tests.{BaseImportSuite, BazelModuleLayout, BazelServerInitializer}
+
+import scala.jdk.CollectionConverters.*
+import scala.meta.internal.builds.{BazelBuildTool, BazelDigest}
+import scala.meta.internal.metals.BuildInfo as V
+import scala.meta.io.AbsolutePath
+
+class BazelTestDiscoverySuite
+    extends BaseImportSuite("bazel-test-discovery", BazelServerInitializer) {
+
+  val bazelVersion = "8.2.1"
+
+  val buildTool: BazelBuildTool = BazelBuildTool(() => userConfig, workspace)
+
+  def buildFileWithToolchain(): String =
+    s"""|/BUILD
+        |load("@rules_scala//scala:scala_toolchain.bzl", "scala_toolchain")
+        |load("@rules_scala//scala:scala.bzl", "scala_test", "scala_junit_test")
+        |
+        |scala_toolchain(
+        |    name = "semanticdb_toolchain_impl",
+        |    enable_semanticdb = True,
+        |    strict_deps_mode = "error",
+        |    unused_dependency_checker_mode = "warn",
+        |)
+        |
+        |toolchain(
+        |    name = "semanticdb_toolchain",
+        |    toolchain = ":semanticdb_toolchain_impl",
+        |    toolchain_type = "@rules_scala//scala:toolchain_type",
+        |    visibility = ["//visibility:public"],
+        |)
+        |
+        |""".stripMargin
+
+  override def currentDigest(
+      workspace: AbsolutePath
+  ): Option[String] = BazelDigest.current(workspace)
+
+  test("simple-scalatest-discovery") {
+    cleanWorkspace()
+    val testLayout =
+      s"""|${buildFileWithToolchain()}
+          |scala_test(
+          |    name = "simple_test",
+          |    srcs = ["SimpleTest.scala"],
+          |)
+          |
+          |/SimpleTest.scala
+          |import org.scalatest.funsuite.AnyFunSuite
+          |
+          |class SimpleTest extends AnyFunSuite {
+          |  test("simple test case") {
+          |    assert(1 + 1 == 2)
+          |  }
+          |}
+          |
+          |""".stripMargin
+
+    for {
+      _ <- initialize(
+        BazelModuleLayout(
+          testLayout,
+          V.scala3,
+          bazelVersion,
+          enableToolChainRegistration = true,
+        )
+      )
+      _ <- server.didOpen("SimpleTest.scala")
+      _ <- server.didSave("SimpleTest.scala")
+
+      _ <- server.waitFor(java.util.concurrent.TimeUnit.SECONDS.toMillis(10))
+
+      testSuites <- server.discoverTestSuites(List("SimpleTest.scala"))
+
+    } yield {
+      val testEvents = testSuites.flatMap(_.events.asScala.toList)
+      assert(
+        testEvents.nonEmpty,
+        s"Expected to find test events, but got empty list. TestSuites: $testSuites",
+      )
+
+      val testClasses = testEvents.collect {
+        case event if event.toString.contains("SimpleTest") => "SimpleTest"
+      }
+      assert(
+        testClasses.nonEmpty,
+        s"Expected to find 'SimpleTest' in discovered classes: ${testEvents.mkString(", ")}",
+      )
+    }
+  }
+}
