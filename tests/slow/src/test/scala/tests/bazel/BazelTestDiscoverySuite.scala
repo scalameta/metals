f76f54b87da4d6073f420fa08bfbf3eb537b4806
Mateusz Piękos <mateuszpiekos@gmail.com>
2025-10-10 14:08:15 +0200

    testing shutting down bazel after each test

--- tests/slow/src/test/scala/tests/bazel/BazelTestDiscoverySuite.scala
+++ tests/slow/src/test/scala/tests/bazel/BazelTestDiscoverySuite.scala
@@ -6,6 +6,7 @@ import scala.jdk.CollectionConverters._
 
 import scala.meta.internal.builds.BazelBuildTool
 import scala.meta.internal.builds.BazelDigest
+import scala.meta.internal.builds.ShellRunner
 import scala.meta.internal.metals.DebugDiscoveryParams
 import scala.meta.internal.metals.JsonParser._
 import scala.meta.internal.metals.{BuildInfo => V}
@@ -46,6 +47,142 @@ class BazelTestDiscoverySuite
   override def currentDigest(
       workspace: AbsolutePath
   ): Option[String] = BazelDigest.current(workspace)
+
+  override def afterEach(context: AfterEach): Unit = {
+    try {
+      // Shutdown Bazel server after each test
+      ShellRunner.runSync(
+        List("bazel", "shutdown"),
+        workspace,
+        redirectErrorOutput = false
+      ) match {
+        case Some(output) => 
+          scribe.info(s"Bazel shutdown completed: $output")
+        case None => 
+          scribe.warn("Bazel shutdown command failed or produced no output")
+      }
+    } catch {
+      case e: Exception =>
+        scribe.warn(s"Failed to shutdown Bazel server: ${e.getMessage}")
+    } finally {
+      super.afterEach(context)
+    }
+  }
+
+  test("simple-scalatest-discovery") {
+    cleanWorkspace()
+    val testLayout =
+      s"""|${buildFileWithToolchain()}
+          |scala_test(
+          |    name = "simple_test",
+          |    srcs = ["SimpleTest.scala"],
+          |)
+          |
+          |/SimpleTest.scala
+          |import org.scalatest.funsuite.AnyFunSuite
+          |
+          |class SimpleTest extends AnyFunSuite {
+          |  test("simple test case") {
+          |    assert(1 + 1 == 2)
+          |  }
+          |}
+          |
+          |""".stripMargin
+
+    for {
+      _ <- initialize(
+        BazelModuleLayout(
+          testLayout,
+          V.scala3,
+          bazelVersion,
+          enableToolChainRegistration = true,
+        )
+      )
+      _ <- server.didOpen("SimpleTest.scala")
+      _ <- server.didSave("SimpleTest.scala")
+
+      _ <- server.waitFor(java.util.concurrent.TimeUnit.SECONDS.toMillis(10))
+
+      testSuites <- server.discoverTestSuites(List("SimpleTest.scala"))
+
+    } yield {
+      val testEvents = testSuites.flatMap(_.events.asScala.toList)
+      assert(
+        testEvents.nonEmpty,
+        s"Expected to find test events, but got empty list. TestSuites: $testSuites",
+      )
+
+      val testClasses = testEvents.collect {
+        case event if event.toString.contains("SimpleTest") => "SimpleTest"
+      }
+      assert(
+        testClasses.nonEmpty,
+        s"Expected to find 'SimpleTest' in discovered classes: ${testEvents.mkString(", ")}",
+      )
+    }
+  }
+
+  test("scalatest-with-multiple-tests") {
+    cleanWorkspace()
+    val testLayout =
+      s"""|${buildFileWithToolchain()}
+          |scala_test(
+          |    name = "multi_test",
+          |    srcs = ["MultiTest.scala"],
+          |)
+          |
+          |/MultiTest.scala
+          |import org.scalatest.funsuite.AnyFunSuite
+          |
+          |class MultiTest extends AnyFunSuite {
+          |  test("first test") {
+          |    assert(1 + 1 == 2)
+          |  }
+          |
+          |  test("second test") {
+          |    assert(2 * 2 == 4)
+          |  }
+          |
+          |  test("third test") {
+          |    assert(3 * 3 == 9)
+          |  }
+          |}
+          |
+          |""".stripMargin
+
+    for {
+      _ <- initialize(
+        BazelModuleLayout(
+          testLayout,
+          V.scala3,
+          bazelVersion,
+          enableToolChainRegistration = true,
+        )
+      )
+
+      _ <- server.didOpen("MultiTest.scala")
+      _ <- server.didSave("MultiTest.scala")
+
+      _ <- server.waitFor(java.util.concurrent.TimeUnit.SECONDS.toMillis(10))
+
+      testSuites <- server.discoverTestSuites(List("MultiTest.scala"))
+
+    } yield {
+      val testEvents = testSuites.flatMap(_.events.asScala.toList)
+      assert(
+        testEvents.nonEmpty,
+        s"Expected to find test events, but got empty list",
+      )
+
+      val testClasses = testEvents.collect {
+        case event if event.toString.contains("MultiTest") => "MultiTest"
+      }
+      assert(
+        testClasses.nonEmpty,
+        s"Expected to find 'MultiTest' in discovered classes: ${testEvents.mkString(", ")}",
+      )
+    }
+  }
 
   test("junit-test-discovery") {
     cleanWorkspace()
@@ -104,6 +241,68 @@ class BazelTestDiscoverySuite
       assert(
         testClasses.nonEmpty,
         s"Expected to find 'JUnitTest' in discovered classes: ${testEvents.mkString(", ")}",
+      )
+    }
+  }
+
+  test("scalatest-debug-mode") {
+    cleanWorkspace()
+    val testLayout =
+      s"""|${buildFileWithToolchain()}
+          |scala_test(
+          |    name = "debug_test",
+          |    srcs = ["DebugTest.scala"],
+          |)
+          |
+          |/DebugTest.scala
+          |import org.scalatest.funsuite.AnyFunSuite
+          |
+          |class DebugTest extends AnyFunSuite {
+          |  test("debug test case") {
+          |    val result = 2 + 2
+          |    assert(result == 4, s"Expected 4, got $$result")
+          |  }
+          |
+          |  test("another debug test") {
+          |    val message = "Hello from Bazel debug test"
+          |    assert(message.nonEmpty)
+          |  }
+          |}
+          |
+          |""".stripMargin
+
+    for {
+      _ <- initialize(
+        BazelModuleLayout(
+          testLayout,
+          V.scala3,
+          bazelVersion,
+          enableToolChainRegistration = true,
+        )
+      )
+      _ <- server.didOpen("DebugTest.scala")
+      _ <- server.didSave("DebugTest.scala")
+
+      _ <- server.waitFor(TimeUnit.SECONDS.toMillis(15))
+
+      debugger <- server.startDebuggingUnresolved(
+        new DebugDiscoveryParams(
+          server.toPath("DebugTest.scala").toURI.toString,
+          "testFile",
+        ).toJson
+      )
+      _ <- debugger.initialize
+      _ <- debugger.launch
+      _ <- debugger.configurationDone
+      _ <- debugger.shutdown
+      output <- debugger.allOutput
+
+    } yield {
+      assert(
+        output.contains("All tests in DebugTest passed") || output.contains(
+          "test completed"
+        ),
+        s"Expected test execution success message in debug output, but got: $output",
       )
     }
   }
