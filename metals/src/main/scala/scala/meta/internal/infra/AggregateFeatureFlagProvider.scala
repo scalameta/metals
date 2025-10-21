343def200bd83fb265aa2c277923b70b28e3d9ec
Ólafur Páll Geirsson <olaf.geirsson@databricks.com>
2025-10-21 20:35:49 +0200

    Feature Flags: fix bug that causes 100% rollout for all users (#144)
    Previously, all feature flags were enabled at 100% of all users because
    of a bug in how we test username matches.
    ```scala
    private object UserLabel {
      def unapply(value: String): Boolean =
        value == value // <-- always true
    }
    ```
    This PR fixes that bug so that feature flags actually allow the safe
    rollout of features. After fixing this bug, I spotted other issues with
    the logic:
    
    - The percentage check always threw a `MatchError`
    - It returned `Optional.empty()` instead of `Optional.of(false)` for
    known feature flags. It should only return `Optional.empty()` for
    feature flags we don't know about.
    
    
    To help troubleshoot issues like this in the future, this PR adds
    support to impersonate users for manual testing. . Now, you can add the
    following system properties to impersonate user or a team:
    
    - `-Dmetals.impersonate-user=USER`
    - `-Dmetals.impersonate-team=TEAM`

--- metals/src/main/scala/scala/meta/internal/infra/AggregateFeatureFlagProvider.scala
+++ metals/src/main/scala/scala/meta/internal/infra/AggregateFeatureFlagProvider.scala
@@ -3,6 +3,8 @@ package scala.meta.internal.infra
 import java.util.Optional
 import java.util.ServiceLoader
 
+import scala.util.control.NonFatal
+
 import scala.meta.infra.FeatureFlag
 import scala.meta.infra.FeatureFlagProvider
 import scala.meta.internal.jdk.CollectionConverters._
@@ -13,7 +15,17 @@ class AggregateFeatureFlagProvider(val underlying: List[FeatureFlagProvider])
   override def readBoolean(flag: FeatureFlag): Optional[java.lang.Boolean] = {
     underlying.foldLeft(Optional.empty[java.lang.Boolean])((acc, provider) => {
       if (acc.isPresent) acc // Return first non-empty result
-      else provider.readBoolean(flag)
+      else {
+        try provider.readBoolean(flag)
+        catch {
+          case NonFatal(e) =>
+            scribe.error(
+              s"error reading feature flag $flag from provider ${provider.getClass.getName}",
+              e,
+            )
+            Optional.empty()
+        }
+      }
     })
   }
 }
