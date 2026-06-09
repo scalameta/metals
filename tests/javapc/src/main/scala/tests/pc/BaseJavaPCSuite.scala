18c420b20505c563476d1f39d99feed967cb8316
Snjezana Peco <snjeza.peco@gmail.com>
2026-06-10 00:24:33 +0200

    Package-private Class Proposed for Annotation

--- tests/javapc/src/main/scala/tests/pc/BaseJavaPCSuite.scala
+++ tests/javapc/src/main/scala/tests/pc/BaseJavaPCSuite.scala
@@ -1,9 +1,19 @@
 package tests.pc
 
+import java.io.File
+import java.net.URI
+import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.Paths
+import javax.tools.ForwardingJavaFileManager
+import javax.tools.JavaFileManager
+import javax.tools.JavaFileObject
+import javax.tools.SimpleJavaFileObject
+import javax.tools.StandardLocation
 
 import scala.jdk.CollectionConverters._
+import scala.util.Using
 
 import scala.meta.Dialect
 import scala.meta.dialects
@@ -12,11 +22,13 @@ import scala.meta.internal.jpc.JavaPresentationCompiler
 import scala.meta.internal.pc.PresentationCompilerConfigImpl
 import scala.meta.io.AbsolutePath
 import scala.meta.pc.EmbeddedClient
+import scala.meta.pc.JavaFileManagerFactory
 
 import coursierapi.Dependency
 import org.slf4j.LoggerFactory
 import tests.BaseSuite
 import tests.PCSuite
+
 class TestingEmbeddedClient(val tmp: AbsolutePath) extends EmbeddedClient {
   override def javaHeaderCompilerPluginJarPath(): Path = {
     val bytes = InputStreamIO.readBytes(
@@ -42,8 +54,82 @@ abstract class BaseJavaPCSuite extends BaseSuite with PCSuite {
   protected lazy val presentationCompiler: JavaPresentationCompiler = {
     val fetch = createFetch()
     extraDependencies.foreach(fetch.addDependencies(_))
-
     val myclasspath: Seq[Path] = extraLibraries(fetch)
+
+    val sourcePath: Option[Path] = getSourcePath()
+    registerExistingJavaFiles(sourcePath)
+    val javaFileManagerFactory: JavaFileManagerFactory =
+      (standardFileManager, classpath) => {
+        if (myclasspath != null) {
+          standardFileManager.setLocationFromPaths(
+            StandardLocation.CLASS_PATH,
+            myclasspath.asJava,
+          )
+        }
+        new ForwardingJavaFileManager[JavaFileManager](standardFileManager) {
+          override def list(
+              location: JavaFileManager.Location,
+              packageName: String,
+              kinds: java.util.Set[JavaFileObject.Kind],
+              recurse: Boolean,
+          ): java.lang.Iterable[JavaFileObject] = {
+
+            val originalResult =
+              super.list(location, packageName, kinds, recurse).asScala.toList
+            if (kinds.contains(JavaFileObject.Kind.SOURCE)) {
+              val matchingExternal = ExternalSourceStorage.fileObjects.filter {
+                f =>
+                  val uriStr = f.toUri.toString
+                  val pkgPath = packageName.replace('.', '/')
+                  if (packageName.isEmpty) true
+                  else if (recurse) uriStr.contains(pkgPath)
+                  else uriStr.contains("/" + pkgPath + "/")
+              }
+              (originalResult ++ matchingExternal).asJava
+            } else {
+              originalResult.asJava
+            }
+          }
+          override def getJavaFileForInput(
+              location: JavaFileManager.Location,
+              className: String,
+              kind: JavaFileObject.Kind,
+          ): JavaFileObject = {
+            if (kind == JavaFileObject.Kind.SOURCE) {
+              ExternalSourceStorage.fileObjects
+                .find { f =>
+                  val uriStr = f.toUri.toString
+                  val expectedEnd = className.replace('.', '/') + ".java"
+                  uriStr.endsWith(expectedEnd)
+                }
+                .getOrElse(super.getJavaFileForInput(location, className, kind))
+            } else {
+              super.getJavaFileForInput(location, className, kind)
+            }
+          }
+
+          override def inferBinaryName(
+              location: JavaFileManager.Location,
+              file: JavaFileObject,
+          ): String = {
+            file match {
+              case inMemory: InMemoryJavaFile =>
+                val uriStr = inMemory.toUri.toString
+                val cleanPath = uriStr.replace(".java", "")
+                val srcIndex = cleanPath.indexOf("/src/")
+                if (srcIndex != -1) {
+                  cleanPath.substring(srcIndex + 5).replace('/', '.')
+                } else {
+                  val lastSlash = cleanPath.lastIndexOf('/')
+                  if (lastSlash != -1) cleanPath.substring(lastSlash + 1)
+                  else cleanPath
+                }
+              case _ => super.inferBinaryName(location, file)
+            }
+          }
+        }
+      }
+
     val dependencySources = fetch.withClassifiers(Set("sources").asJava).fetch()
     dependencySources.asScala.foreach { jar =>
       index.addSourceJar(AbsolutePath(jar), dialect)
@@ -63,6 +149,7 @@ abstract class BaseJavaPCSuite extends BaseSuite with PCSuite {
       .withLogger(LoggerFactory.getLogger("java.metals"))
       // TODO: we need a real instance of the embedded client here.
       .withEmbeddedClient(new TestingEmbeddedClient(tmp))
+      .withJavaFileManagerFactory(javaFileManagerFactory)
       .newInstance("", myclasspath.asJava, Nil.asJava, () => Nil.asJava)
       .asInstanceOf[JavaPresentationCompiler]
   }
@@ -79,4 +166,66 @@ abstract class BaseJavaPCSuite extends BaseSuite with PCSuite {
 
   protected def extraDependencies: Seq[Dependency] =
     Seq.empty
+
+  private def getSourcePath(): Option[Path] = {
+    val classLocation =
+      getClass.getProtectionDomain.getCodeSource.getLocation.toURI
+    val testClassesPath = Paths.get(classLocation).toAbsolutePath.toString
+    val sep = File.separator
+    val targetMarker = s"${sep}target${sep}"
+    if (testClassesPath.contains(targetMarker)) {
+      val projectRootStr =
+        testClassesPath.substring(0, testClassesPath.lastIndexOf(targetMarker))
+      val projectRoot = Paths.get(projectRootStr)
+      val srcDir = projectRoot.resolve("testsources")
+      if (srcDir.toFile.exists() && srcDir.toFile.isDirectory) {
+        Option(srcDir)
+      } else {
+        None
+      }
+    } else {
+      None
+    }
+  }
+
+  class InMemoryJavaFile(uri: URI, val code: String)
+      extends SimpleJavaFileObject(uri, JavaFileObject.Kind.SOURCE) {
+    override def getCharContent(ignoreEncodingErrors: Boolean): CharSequence =
+      code
+  }
+
+  object ExternalSourceStorage {
+    var fileObjects: List[JavaFileObject] = Nil
+  }
+
+  def registerExistingJavaFiles(sourcePath: Option[Path]): Unit = {
+    sourcePath.foreach { rootPath =>
+      if (Files.exists(rootPath) && Files.isDirectory(rootPath)) {
+        Using(Files.walk(rootPath)) { stream =>
+          val files = stream
+            .iterator()
+            .asScala
+            .filter(p => Files.isRegularFile(p) && p.toString.endsWith(".java"))
+            .toList
+
+          ExternalSourceStorage.fileObjects = files.map { javaFile =>
+            val code = Files.readString(javaFile, StandardCharsets.UTF_8)
+            val absolutePath =
+              scala.meta.io.AbsolutePath(javaFile.toAbsolutePath)
+            val rootAbsolutePath =
+              scala.meta.io.AbsolutePath(rootPath.toAbsolutePath)
+
+            index.addSourceFile(absolutePath, Some(rootAbsolutePath), dialect)
+            workspace.inputs(absolutePath.toURI.toString()) = (code, dialect)
+
+            new InMemoryJavaFile(absolutePath.toURI, code)
+          }
+          scribe.info(
+            s"Successfully pre-registered ${ExternalSourceStorage.fileObjects.size} Java file objects."
+          )
+        }.get
+      }
+    }
+  }
+
 }
---
+++ tests/javapc/testsources/test/annon/AnnotationPrivate.java
@@ -0,0 +1,7 @@
+package test.annon;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@interface AnnotationPrivate {}
