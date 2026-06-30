02f3d392312a4ca7820f69b60a5413cd1c357452
Snjezana Peco <snjeza.peco@gmail.com>
2026-06-30 23:21:47 +0200

    Invalid import declaration

--- mtags-java/src/main/scala/scala/meta/internal/jpc/JavaAutoImportProvider.scala
+++ mtags-java/src/main/scala/scala/meta/internal/jpc/JavaAutoImportProvider.scala
@@ -6,6 +6,8 @@ import scala.meta.internal.pc.AutoImportsResultImpl
 import scala.meta.pc.AutoImportsResult
 import scala.meta.pc.OffsetParams
 
+import com.sun.source.util.Trees
+
 class JavaAutoImportProvider(
     val compiler: JavaMetalsCompiler,
     val params: OffsetParams,
@@ -17,11 +19,21 @@ class JavaAutoImportProvider(
       case NameStartRegex(name) => name
       case _ => toImportName
     }
+    params.checkCanceled()
+    val compile = compiler.compilationTask(params).withAnalyzePhase()
+    params.checkCanceled()
+    val trees = Trees.instance(compile.task)
+    val task = compile.task
+    val cu = compile.cu
+    val scanner = new JavaTreeScanner(compiler.logger, task, cu)
+    val position =
+      CursorPosition(params.offset(), params.offset(), params.offset())
+    val path = compiler.compilerTreeNode(scanner, position)
     for {
       fqn <- compiler.doSearch(query).iterator
       // Only suggest imports for exact matches
       if fqn.endsWith(s".$toImportName")
-      edit = new JavaAutoImportEditor(params.text(), fqn).textEdit()
+      edit = new JavaAutoImportEditor(path.get, trees, fqn).textEdit()
       pkg = fqn.split('.').dropRight(1).mkString(".")
     } yield AutoImportsResultImpl(
       pkg,
