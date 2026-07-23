4c682159ef74b8cd34cc698e89a985bbbbec7744
Tomasz Godzik <tgodzik@virtuslab.com>
2026-07-23 15:43:44 +0200

    chore: Update expected files

--- tests/unit/src/test/resources/semanticdb/example/ImplicitClasses.scala
+++ tests/unit/src/test/resources/semanticdb/example/ImplicitClasses.scala
@@ -1,10 +1,10 @@
 package example
 
 object ImplicitClasses/*example.ImplicitClasses.*/ {
-  implicit class Xtension/*example.ImplicitClasses.Xtension#*/(number/*example.ImplicitClasses.Xtension#number.*/: Int/*scala.Int#*/) {
+  implicit class Xtension/*example.ImplicitClasses.Xtension#*/(number/*example.ImplicitClasses.Xtension#number.*//*example.ImplicitClasses.Xtension#`<init>`().(number)*/: Int/*scala.Int#*/) {
     def increment/*example.ImplicitClasses.Xtension#increment().*/: Int/*scala.Int#*/ = number/*example.ImplicitClasses.Xtension#number.*/ +/*scala.Int#`+`(+4).*/ 1
   }
-  implicit class XtensionAnyVal/*example.ImplicitClasses.XtensionAnyVal#*/(private val number/*example.ImplicitClasses.XtensionAnyVal#number.*/: Int/*scala.Int#*/) extends AnyVal/*scala.AnyVal#*/ /*scala.AnyVal#`<init>`().*/{
+  implicit class XtensionAnyVal/*example.ImplicitClasses.XtensionAnyVal#*/(private val number/*example.ImplicitClasses.XtensionAnyVal#number.*//*example.ImplicitClasses.XtensionAnyVal#`<init>`().(number)*/: Int/*scala.Int#*/) extends AnyVal/*scala.AnyVal#*/ /*scala.AnyVal#`<init>`().*/{
     def double/*example.ImplicitClasses.XtensionAnyVal#double().*/: Int/*scala.Int#*/ = number/*example.ImplicitClasses.XtensionAnyVal#number.*/ */*scala.Int#`*`(+3).*/ 2
   }
 }
