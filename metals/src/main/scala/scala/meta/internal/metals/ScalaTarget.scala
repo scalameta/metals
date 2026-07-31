be0f4ff640147c40d435a7c87b120ab100b53a4d
Russ White <356303+russwyte@users.noreply.github.com>
2026-07-30 23:21:40 -0500

    bugfix: Use Scala 3 dialect for sbt 2.x *.sbt files
    sbt 2 compiles build.sbt with Scala 3, so significant-indentation
    syntax was falsely flagged when Metals always used dialects.Sbt.

--- metals/src/main/scala/scala/meta/internal/metals/ScalaTarget.scala
+++ metals/src/main/scala/scala/meta/internal/metals/ScalaTarget.scala
@@ -43,7 +43,8 @@ case class ScalaTarget(
     )
 
   def dialect(path: AbsolutePath): Dialect =
-    if (info.isSbtBuild && path.isSbt) Sbt
+    if (info.isSbtBuild && path.isSbt)
+      ScalaVersions.dialectForSbtVersion(sbtVersion)
     else scalaDialect
 
   private def scalaDialect: Dialect = {
--- tests/unit/src/test/scala/tests/SbtVersionSuite.scala
+++ tests/unit/src/test/scala/tests/SbtVersionSuite.scala
@@ -1,6 +1,10 @@
 package tests
 
+import scala.meta.dialects
 import scala.meta.internal.builds.SbtBuildTool
+import scala.meta.internal.metals.BuildTargets
+import scala.meta.internal.metals.ScalaVersionSelector
+import scala.meta.internal.metals.ScalaVersions
 import scala.meta.internal.metals.UserConfiguration
 
 import munit.Location
@@ -35,5 +39,74 @@ class SbtVersionSuite extends BaseSuite {
     """.stripMargin,
     "1.1.3",
   )
+
+  test("loadVersionForPath") {
+    val root = FileLayout.fromString(
+      """|/project/build.properties
+         |sbt.version=2.0.4
+         |/build.sbt
+         |scalaVersion := "3.3.4"
+         |/project/plugins.sbt
+         |addSbtPlugin("com.example" % "example" % "1.0")
+         |""".stripMargin
+    )
+    assertEquals(
+      SbtBuildTool.loadVersionForPath(root.resolve("build.sbt")),
+      Some("2.0.4"),
+    )
+    assertEquals(
+      SbtBuildTool.loadVersionForPath(root.resolve("project/plugins.sbt")),
+      Some("2.0.4"),
+    )
+  }
+
+  test("dialectForSbtVersion") {
+    assertEquals(
+      ScalaVersions.dialectForSbtVersion(Some("1.11.2")),
+      dialects.Sbt,
+    )
+    assertEquals(
+      ScalaVersions.dialectForSbtVersion(None),
+      dialects.Sbt,
+    )
+    val sbt2 = ScalaVersions.dialectForSbtVersion(Some("2.0.4"))
+    assert(sbt2.allowSignificantIndentation)
+    assert(sbt2.allowToplevelTerms)
+  }
+
+  test("getDialect-sbt2") {
+    val root = FileLayout.fromString(
+      """|/project/build.properties
+         |sbt.version=2.0.4
+         |/build.sbt
+         |val x = 1
+         |""".stripMargin
+    )
+    val selector = new ScalaVersionSelector(
+      () => UserConfiguration(),
+      BuildTargets.empty,
+    )
+    val dialect = selector.getDialect(root.resolve("build.sbt"))
+    assert(dialect.allowSignificantIndentation)
+    assert(dialect.allowToplevelTerms)
+  }
+
+  test("getDialect-sbt1") {
+    val root = FileLayout.fromString(
+      """|/project/build.properties
+         |sbt.version=1.11.2
+         |/build.sbt
+         |val x = 1
+         |""".stripMargin
+    )
+    val selector = new ScalaVersionSelector(
+      () => UserConfiguration(),
+      BuildTargets.empty,
+    )
+    assertEquals(
+      selector.getDialect(root.resolve("build.sbt")),
+      dialects.Sbt,
+    )
+  }
 
 }
