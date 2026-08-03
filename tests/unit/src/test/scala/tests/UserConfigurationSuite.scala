9a880fb23d50032ef93a492b3d82e13369229e78
Tomasz Godzik <tgodzik@virtuslab.com>
2026-08-03 16:12:44 +0200

    improvement: Save semanticdb to disk under setting
    Also allow to specify the maximum cache size for the text document cache.

--- tests/unit/src/test/scala/tests/UserConfigurationSuite.scala
+++ tests/unit/src/test/scala/tests/UserConfigurationSuite.scala
@@ -15,6 +15,7 @@ import scala.meta.internal.metals.Configs.BatchSemanticdbConfig
 import scala.meta.internal.metals.Configs.FallbackClasspathConfig
 import scala.meta.internal.metals.Configs.FallbackSourcepathConfig
 import scala.meta.internal.metals.Configs.JavacServicesOverrides
+import scala.meta.internal.metals.Configs.MbtConfig
 import scala.meta.internal.metals.Configs.TurbineRecompileDelayConfig
 import scala.meta.internal.metals.Configs.WorkspaceSymbolProviderConfig
 import scala.meta.internal.metals.InlayHintsOption
@@ -411,6 +412,7 @@ class UserConfigurationSuite extends BaseSuite {
       scalaImportsPlacement = ScalaImportsPlacement.SMART,
       batchSemanticdbCompilerInstances = BatchSemanticdbConfig(4),
       promptBuildImport = true,
+      mbtConfig = MbtConfig(false, true, 1000),
     )
 
     val json = nonDefault.toString()
@@ -524,7 +526,11 @@ class UserConfigurationSuite extends BaseSuite {
   },
   "enableBestEffort": false,
   "startMcpServer": false,
-  "importGeneratedSourcesMbt": false
+  "mbt": {
+    "importGeneratedSources": false,
+    "semanticdbCacheEnabled": true,
+    "semanticdbCacheMaxSize": 1000
+  }
 }""",
     )
     val roundtripJson = UserConfiguration.parse(json)
@@ -607,7 +613,9 @@ class UserConfigurationSuite extends BaseSuite {
           |default-shell                                string                         ""              Full path to the shell executable to be used as the default
           |start-mcp-server                             boolean                        false           Start MCP server
           |mcp-client                                   string                         ""              MCP Client Name
-          |import-generated-sources-mbt                 boolean                        false           Import Generated Sources In MBT Builds""".stripMargin
+          |mbt.import-generated-sources                 boolean                        false           Import Generated Sources In MBT Builds
+          |mbt.semanticdb-cache-enabled                 boolean                        false           Enable MBT Semanticdb Cache
+          |mbt.semanticdb-cache-max-size                string                         ${Int.MaxValue.toString.padTo(15, ' ')} MBT Semanticdb In-Memory Cache Size Limit""".stripMargin
     assertNoDiff(obtained, expected)
   }
 
@@ -700,5 +708,37 @@ class UserConfigurationSuite extends BaseSuite {
     """.stripMargin,
   ) { obtained =>
     assert(obtained.targetBuildTool == Some("sbt"))
+  }
+
+  checkOK(
+    "mbt-config-old-format",
+    """
+      |{
+      | "import-generated-sources-mbt": true,
+      | "mbt-semanticdb-cache": true,
+      | "mbt-semanticdb-cache-max-size": "500"
+      |}
+    """.stripMargin,
+  ) { obtained =>
+    assert(obtained.mbtConfig.importGeneratedSources == true)
+    assert(obtained.mbtConfig.semanticdbCacheEnabled == true)
+    assert(obtained.mbtConfig.semanticdbCacheMaxSize == 500)
+  }
+
+  checkOK(
+    "mbt-config-new-format",
+    """
+      |{
+      | "mbt": {
+      |   "import-generated-sources": true,
+      |   "semanticdb-cache-enabled": true,
+      |   "semanticdb-cache-max-size": "500"
+      | }
+      |}
+    """.stripMargin,
+  ) { obtained =>
+    assert(obtained.mbtConfig.importGeneratedSources == true)
+    assert(obtained.mbtConfig.semanticdbCacheEnabled == true)
+    assert(obtained.mbtConfig.semanticdbCacheMaxSize == 500)
   }
 }
